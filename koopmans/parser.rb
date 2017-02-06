d39d663522a5ab514d94b5cd9e2557208685d9a1
aijkoopmans <arend@Arends-MacBook-Pro.local>
2017-02-06 14:39:48 +0100

    one line

--- koopmans/parser.rb
+++ koopmans/parser.rb
@@ -1,12 +1,20 @@
 require 'parslet'
 
 class Mini < Parslet::Parser
-  rule :string do
-    str('"') >> (str('"').absent? >> any).repeat.as(:question) >> str('"')
-  end
+  # standard
+  rule(:spaces) { match('\s').repeat(1) }
+  rule(:spaces?) { spaces.maybe }
 
-  root :string
+
+  # question
+  rule(:string) { str('"') >> (str('"').absent? >> any).repeat.as(:string) >> str('"') }
+  rule(:variable) { (str(':').absent? >> any).repeat.as(:variable) >> str(':') }
+  rule(:type) { (str('boolean') | str('money')).as(:type) }
+  rule(:field) { string >> spaces? >> variable >> spaces? >> type }
+
+  root :field
 end
+
 
 def parse(str)
   mini = Mini.new
@@ -16,5 +24,10 @@ rescue Parslet::ParseFailed => failure
   puts failure
 end
 
-p parse '"How much is?"' # => "1 + 2 + 3"@0
+# p parse '"How much is?"' # => "1 + 2 + 3"@0
+# p parse 'hasSoldHouse:'
+p parse '"How much is?"
+
+hasSoldHouse:      boolean'
+# p parse 'hasSoldHouse: boolean'
 # p parse "a + 2"      # fails, see below
