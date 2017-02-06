541ccdfc3ebe67e0af369ea141643a76bb90a3ce
Adrian Zborowski <ak.zborowski@gmail.com>
2017-02-06 14:32:53 +0100

    + Changed the structure to try-catch

--- pql/pql/main.py
+++ pql/pql/main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
-import sys
-import io
+import sys, os, io
 
 if __name__ == '__main__':
 
-    if len(sys.argv) >= 2:
-
+    try:
         ql = io.open(sys.argv[1], "r")
+    except IndexError:
+        print(" ".join(["Usage: python pql.py", str(os.path.join("path", "to", "your", "file"))]))
+        sys.exit(0)
+    except FileNotFoundError:
+        print("The given file could not be found.")
+        sys.exit(0)
+
+    print(ql.read())
