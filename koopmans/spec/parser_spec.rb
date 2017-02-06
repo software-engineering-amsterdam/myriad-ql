c9e69524eaa501f3785dacc00b275e3241b1e547
aijkoopmans <arendkoopmans@gmail.com>
2017-02-06 16:55:12 +0100

    property tests

--- koopmans/spec/parser_spec.rb
+++ koopmans/spec/parser_spec.rb
@@ -9,11 +9,17 @@ describe Parser do
     it 'should parse' do
       expect(parser.label).to parse('"How much is?"')
     end
+    it 'should parse into property' do
+      expect(parser.label.parse('"How much is?"')).to eq({label: 'How much is?'})
+    end
   end
 
   context 'variable' do
     it 'should parse' do
       expect(parser.variable).to parse('hasSoldHouse:')
+    end
+    it 'should parse into property' do
+      expect(parser.variable.parse('hasSoldHouse:')).to eq({variable: 'hasSoldHouse'})
     end
   end
 
@@ -21,11 +27,17 @@ describe Parser do
     it 'should parse' do
       expect(parser.type).to parse('boolean')
     end
+    it 'should parse into property' do
+      expect(parser.type.parse('boolean')).to eq({type: 'boolean'})
+    end
   end
 
   context 'question' do
     it 'should parse' do
       expect(parser.question).to parse('"How much is?" hasSoldHouse: boolean')
+    end
+    it 'should parse into property' do
+      expect(parser.question.parse('"How much is?" hasSoldHouse: boolean')).to eq({question: {label: 'How much is?', variable: 'hasSoldHouse', type: 'boolean'}})
     end
   end
 
