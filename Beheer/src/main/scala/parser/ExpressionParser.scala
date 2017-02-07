9d076a197ef940566426db4c98bd4023af9bc328
Jasper Dijt <jasper@jasperdijt.eu>
2017-02-07 14:07:43 +0100

    Beheer :: Expressions to AST nodes, the ugly version.

--- Beheer/src/main/scala/parser/ExpressionParser.scala
+++ Beheer/src/main/scala/parser/ExpressionParser.scala
@@ -1,29 +1,59 @@
 package parser
 
+import parser.ast._
+
+import scala.annotation.tailrec
 import scala.util.parsing.combinator.JavaTokenParsers
 
 /**
   * Created by jasper on 07/02/17.
   */
 trait ExpressionParser extends JavaTokenParsers {
-  def comp: Parser[Any] = addSub ~
-    rep(">" ~ addSub
-      | "<" ~ addSub
-      | ">=" ~ addSub
-      | "<=" ~ addSub
-      | "==" ~ addSub
-      | "!=" ~ addSub
+  def expr: Parser[ExpressionNode] = comp ~ rep("""\&\&|\|\|""" ~ comp) ^^ {
+    case lhs ~ tail => parseList(lhs, tail.map(x => (x._1, x._2)))
+  }
+
+  def comp: Parser[ExpressionNode] = addSub ~ rep(""">|<|>=|<=|!=|==""".r ~ addSub) ^^ {
+    case lhs ~ tail => parseList(lhs, tail.map(x => (x._1, x._2)))
+  }
+
+  def addSub: Parser[ExpressionNode] = term ~ rep("""\+|-""".r ~ term) ^^ {
+    case lhs ~ tail => parseList(lhs, tail.map(x => (x._1, x._2)))
+  }
+
+  def term: Parser[ExpressionNode] = factor ~ rep("""\*|/""".r ~ factor) ^^ {
+    case lhs ~ tail => parseList(lhs, tail.map(x => (x._1, x._2)))
+  }
+
+
+  def factor: Parser[ExpressionNode] = (
+    value
+      | """\+|-|!""".r ~ value ^^ {
+      case op ~ value => PrefixOperation(op, value);
+    }
     )
 
-  def addSub: Parser[Any] = term ~ rep("+" ~ term | "-" ~ term)
+  def value: Parser[ExpressionNode] = (
+    ident ^^ (s => Identifier(s))
+      | "(" ~> expr <~ ")"
+      | integer
+    )
 
-  def term: Parser[Any] = factor ~ rep("*" ~ factor | "/" ~ factor)
+  def integer: Parser[Value] = """\d+""".r ^^ (x => Value(x.toInt))
 
-  def factor: Parser[Any] = value | "+" ~ value | "-" ~ value | "!" ~ value
 
-  def value: Parser[Any] = "(" ~ expr ~ ")" | integer | ident
+  private def parseList(lhs: ExpressionNode, list: List[(String, ExpressionNode)]): ExpressionNode = {
+    list match {
+      case Nil => lhs
+      case head :: tail => InfixOperation(lhs, head._1, parseList(head._2, tail));
+    }
+  }
+}
 
-  def integer: Parser[Any] = "\\d+".r
 
-  def expr: Parser[Any] = comp ~ rep("&&" ~ comp | "||" ~ comp)
+object ExpressionParser extends ExpressionParser{
+  def main(args: Array[String]) = {
+    val input = "(-(+1++1)*22-+50/+300) + BLA"
+    println(parseAll(expr, input))
+  }
 }
