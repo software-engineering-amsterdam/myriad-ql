08cb5a6f137286c18a4482930e95e913da981999
Mats Stijlaart <matstijl@wcw-stud-145-109-52-124.wireless.uva.nl>
2017-02-07 11:25:17 +0100

    [elmos] Add tests for comparison

--- elmos/tests/ExpressionParserTests.elm
+++ elmos/tests/ExpressionParserTests.elm
@@ -9,27 +9,47 @@ import ParserTestUtil exposing (testWithParser)
 all : Test
 all =
     describe "ParserTests"
-        [ expressionTests
-        , arithmeticTests
+        [ arithmeticTests
+        , comparisonTests
+        , atomTests
         ]
 
 
 arithmeticTests : Test
 arithmeticTests =
     testWithParser ExpressionParser.expression
-        "expression"
+        "arithmeticTests"
         [ ( "Should parse simple add", "2+3", Just (PlusExpression (Integer 2) (Integer 3)) )
         , ( "Should parse bigger add", "2+3+4", Just (PlusExpression (PlusExpression (Integer 2) (Integer 3)) (Integer 4)) )
         , ( "Should parse plus and multiplication", "2+3*4", Just (PlusExpression (Integer 2) (MultiplyExpression (Integer 3) (Integer 4))) )
         , ( "Should parse minus and division", "2-3/4", Just (MinusExpression (Integer 2) (DivideExpression (Integer 3) (Integer 4))) )
-        , ( "Should parse variables", "x+y", Just (PlusExpression (Var "x") (Var "y")) )
+        , ( "Should parse variables", "x+y", Just (PlusExpression (Var "x") (Var "z")) )
         ]
 
 
-expressionTests : Test
-expressionTests =
+comparisonTests : Test
+comparisonTests =
     testWithParser ExpressionParser.expression
-        "expression"
+        "comparisonTests"
+        [ ( "Should parse less than comparison", "x < y", Just (LessThanExpression (Var "x") (Var "y")) )
+        , ( "Should parse greater than comparison", "x > y", Just (GreaterThanExpression (Var "x") (Var "y")) )
+        , ( "Should parse less than equal comparison", "x <= y", Just (GreaterThanOrEqualExpression (Var "x") (Var "y")) )
+        , ( "Should parse greater than equal comparison", "x >= y", Just (LessThanOrEqualExpression (Var "x") (Var "y")) )
+        , ( "Should parse comparison with arithmetic"
+          , "x+y < z * a"
+          , Just
+                (LessThanExpression
+                    (PlusExpression (Var "x") (Var "y"))
+                    (MultiplyExpression (Var "z") (Var "a"))
+                )
+          )
+        ]
+
+
+atomTests : Test
+atomTests =
+    testWithParser ExpressionParser.expression
+        "atomTests"
         [ ( "Should parse varName", "someVarName", Just (Var "someVarName") )
-        , ( "Should parse expression between parentheses", "(someVarName)", Just (ParensExpression (Var "someVarName")) )
+        , ( "Should parse int literal", "2", Just (ParensExpression (Integer 2)) )
         ]
