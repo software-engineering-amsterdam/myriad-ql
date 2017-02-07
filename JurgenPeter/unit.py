a808ebf029ff7f28c32d4a08414639fcc34ae591
Peter Verkade <peter.m.verkade@gmail.com>
2017-02-07 10:46:52 +0100

    added unit test class template

---
+++ JurgenPeter/unit.py
@@ -0,0 +1,22 @@
+from unittest import TestCase, main
+
+
+class TestSomething(TestCase):
+
+    def setUp(self):
+        # Optional
+        pass
+
+    def tearDown(self):
+        # Optional
+        pass
+
+    def testSomething(self):
+        # self.assertEqual
+        # self.assertTrue
+        # self.assertFalse
+        # self.assertRaises
+        pass
+
+if __name__ == "__main__":
+    main()
