b7701e660ddf664abf1f9af668c603ced2a91053
Ahmed <ahmedmusaafir@gmail.com>
2017-02-07 16:30:40 +0100

    Parser work in progress

--- AlternativeQL/src/org/ql/parser/Parser.java
+++ AlternativeQL/src/org/ql/parser/Parser.java
@@ -1,12 +1,20 @@
 package org.ql.parser;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.ql.ast.Form;
 
 import java.io.File;
 
 public class Parser {
     public Form parse(String code) {
-
+        QLParserLexer lexer = new QLParserLexer(new ANTLRInputStream(code));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        QLParserParser qlParser = new QLParserParser(tokens);
+        QLParserParser.FormContext formContext = qlParser.form();
+        return new Form();
     }
 
     public Form parse(File sourceFile) {
