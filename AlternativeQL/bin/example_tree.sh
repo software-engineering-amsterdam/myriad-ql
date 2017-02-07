13439e35880956d5392e65d59e9bd1de84865fbe
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2017-02-07 11:27:51 +0100

    Updated the shell script

--- AlternativeQL/bin/example_tree.sh
+++ AlternativeQL/bin/example_tree.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-antlr4 QL.g4 && javac src/*.java && grun QL form ../drafts/*.aql -gui
+antlr4 src/org/uva/sea/ql/parser/QL.g4 -o generated && \
+    javac generated/src/org/uva/sea/ql/parser/*.java -d out && \
+    cd out && \
+    grun QL form ../drafts/*.aql -gui && \
+    cd ..
