63f52d365643534e88325ae743b0efcc38f46ae4
Stan Janssen <stanjanssen@outlook.com>
2017-02-07 16:03:28 +0100

    added todo

--- OffByOne/src/OffByOne.LanguageCore/Ast/Expressions/Expression.cs
+++ OffByOne/src/OffByOne.LanguageCore/Ast/Expressions/Expression.cs
@@ -2,6 +2,7 @@
 {
     using OffByOne.LanguageCore.Ast;
 
+    // TODO: Debate the usefulness of adding a Boolean/ArithmeticExpression abstract class.
     public abstract class Expression : AstNode
     {
     }
