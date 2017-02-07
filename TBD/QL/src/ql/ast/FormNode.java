a4367c8cd7f7aafba6bb6453e64e2c091508a7ca
Erik Landkroon <eriklandkroon@gmail.com>
2017-02-07 10:50:35 +0100

    added jacc and the beginning of ast

---
+++ TBD/QL/src/ql/ast/FormNode.java
@@ -0,0 +1,12 @@
+package ql.ast;
+
+import ql.ast.types.StringNode;
+
+/**
+ * Created by Erik on 6-2-2017.
+ */
+public class FormNode extends ASTNode {
+    public FormNode(StringNode st){
+
+    }
+}
---
+++ TBD/QL/src/ql/ast/IfNode.java
@@ -0,0 +1,7 @@
+package ql.ast;
+
+/**
+ * Created by Erik on 6-2-2017.
+ */
+public class IfNode implements ASTNode, StatementNode {
+}
