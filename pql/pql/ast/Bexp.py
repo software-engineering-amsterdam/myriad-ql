1494e256468604ff31c49ecc28e8d36e03cfd011
Adrian Zborowski <ak.zborowski@gmail.com>
2017-02-07 16:11:48 +0100

    - Cleaned code

--- pql/pql/ast/Bexp.py
+++ pql/pql/ast/Bexp.py
@@ -14,25 +14,6 @@ class RelopBexp(Bexp):
     def __repr__(self):
         return 'RelopBexp(%s, %s, %s)' % (self.op, self.left, self.right)
 
-    def eval(self, env):
-        left_value = self.left.eval(env)
-        right_value = self.right.eval(env)
-        if self.op == '<':
-            value = left_value < right_value
-        elif self.op == '<=':
-            value = left_value <= right_value
-        elif self.op == '>':
-            value = left_value > right_value
-        elif self.op == '>=':
-            value = left_value >= right_value
-        elif self.op == '=':
-            value = left_value == right_value
-        elif self.op == '!=':
-            value = left_value != right_value
-        else:
-            raise RuntimeError('unknown operator: ' + self.op)
-        return value
-
 
 class AndBexp(Bexp):
     def __init__(self, left, right):
@@ -41,11 +22,6 @@ class AndBexp(Bexp):
 
     def __repr__(self):
         return 'AndBexp(%s, %s)' % (self.left, self.right)
-
-    def eval(self, env):
-        left_value = self.left.eval(env)
-        right_value = self.right.eval(env)
-        return left_value and right_value
 
 
 class OrBexp(Bexp):
@@ -56,11 +32,6 @@ class OrBexp(Bexp):
     def __repr__(self):
         return 'OrBexp(%s, %s)' % (self.left, self.right)
 
-    def eval(self, env):
-        left_value = self.left.eval(env)
-        right_value = self.right.eval(env)
-        return left_value or right_value
-
 
 class NotBexp(Bexp):
     def __init__(self, exp):
@@ -68,7 +39,3 @@ class NotBexp(Bexp):
 
     def __repr__(self):
         return 'NotBexp(%s)' % self.exp
-
-    def eval(self, env):
-        value = self.exp.eval(env)
-        return not value
