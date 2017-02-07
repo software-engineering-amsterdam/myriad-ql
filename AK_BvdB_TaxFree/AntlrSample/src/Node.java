6311f4042e9ddf59264ea6180a59432ef0fef3b3
Alex Kok <alexkok08@gmail.com>
2017-02-07 15:29:29 +0100

    AST

--- AK_BvdB_TaxFree/AntlrSample/src/Node.java
+++ AK_BvdB_TaxFree/AntlrSample/src/Node.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Alex on 7-2-2017.
  */
-public class Node<T> {
+public abstract class Node {
 
-    private final Class<T> type;
+    private int mName;
+    private final int mType;
+    private Node mParent;
+    private List<Node> mChildren;
 
-    public Node(Class<T> type) {
-        this.type = type;
+    public Node(int type, Node parent) {
+        mType = type;
+        mParent = parent;
+        mChildren = new ArrayList<>();
     }
 
-    public Class<T> getMyType() {
-        return this.type;
+    public int getMyType() {
+        return mType;
+    }
+
+    public Node getParent() {
+        return mParent;
+    }
+
+    public List<Node> getChildren() {
+        return mChildren;
+    }
+
+    public void addChild(Node child) {
+        mChildren.add(child);
     }
 }
