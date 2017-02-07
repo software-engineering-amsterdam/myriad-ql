7988c80318b937ca29eeb8ce8ea85e11b27e4bc5
Jurgen Baas <jurgen.baas@outlook.com>
2017-02-07 13:22:38 +0100

    Renamed files for consistent naming convention. Finished Parser class. Added ast tostr methods.

---
+++ JurgenPeter/form_ast.py
@@ -0,0 +1,74 @@
+from enum import Enum
+
+Datatype = Enum("Datatype", "boolean string integer date decimal money")
+Operator = Enum("Operator", "+ - * / ! && || < > <= >= == !=")
+
+
+class Form:
+
+    def __init__(self, name, statements):
+        self.name = name
+        self.statements = statements
+
+    def __str__(self):
+        return "form {} [\n{}\n]".format(self.name, "\n".join([str(s) for s in self.statements]))
+
+
+class Conditional:
+
+    def __init__(self, condition, statements):
+        self.condition = condition
+        self.statements = statements
+
+    def __str__(self):
+        return "if {} [\n{}\n]".format(str(self.condition), "\n".join([str(s) for s in self.statements]))
+
+class Question:
+
+    def __init__(self, name, label, datatype, expression=None):
+        self.name = name
+        self.label = label
+        self.datatype = datatype
+        self.expression = expression
+
+    def __str__(self):
+        if self.expression is not None:
+            return "{}: \"{}\" {} = {}".format(self.name, self.label, self.datatype.name, str(self.expression))
+        return "{}: \"{}\" {}".format(self.name, self.label, self.datatype.name)
+
+class UnaryOperator:
+
+    def __init__(self, operator, right):
+        self.operator = operator
+        self.right = right
+
+    def __str__(self):
+        return "{}{}".format(self.operator.name, str(self.right))
+
+
+class BinaryOperator:
+
+    def __init__(self, left, operator, right):
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return "{} {} {}".format(str(self.left), self.operator.name, str(self.right))
+
+class Identifier:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Constant:
+
+    def __init__(self, value, datatype):
+        self.value = value
+        self.datatype = datatype
+
+    def __str__(self):
+        return str(self.value)
--- JurgenPeter/form_parser.py
+++ JurgenPeter/form_parser.py
@@ -1,5 +1,5 @@
 from pyparsing import *
-
+from form_ast import *
 
 class Grammar:
     identifier = Word(alphas)
@@ -37,7 +37,7 @@ class Grammar:
 
     block = Forward()
     question = identifier + semicolon + QuotedString("\"") + datatype + Optional(assignment + expression)
-    conditional = Literal("if") + bool_expr + bracket_open + block + bracket_close
+    conditional = Literal("if") + bool_expr + bracket_open + Group(block) + bracket_close
     conditional.setParseAction(lambda tokens: ["_if"] + tokens[1:])
     statement = Group(question ^ conditional)
     block <<= ZeroOrMore(statement)
@@ -51,31 +51,64 @@ class Parser:
     @staticmethod
     def parse_file(filename):
         # TODO catch errors: print warning line and return None
-        tokens = Grammar.form.parseFilename(filename)
+        tokens = Grammar.form.parseFile(filename)
         return Parser.parse_form(tokens)
 
     @staticmethod
     def parse_form(tokens):
+        if len(tokens) == 3 and tokens[0] == "_form":
+            return Form(tokens[1], [Parser.parse_statement(t) for t in tokens[2]])
         return None
 
     @staticmethod
     def parse_statement(tokens):
-        return None
+        conditional = Parser.parse_conditional(tokens)
+        if conditional is not None:
+            return conditional
+        return Parser.parse_question(tokens)
 
     @staticmethod
     def parse_conditional(tokens):
+        if len(tokens) == 3 and tokens[0] == "_if":
+            return Conditional(Parser.parse_expression(tokens[1]), [Parser.parse_statement(t) for t in tokens[2]])
         return None
 
     @staticmethod
     def parse_question(tokens):
+        if len(tokens) == 3:
+            return Question(tokens[0], tokens[1], Datatype[tokens[2]])
+        elif len(tokens) == 4:
+            return Question(tokens[0], tokens[1], Datatype[tokens[2]], Parser.parse_expression(tokens[3]))
         return None
 
     @staticmethod
     def parse_expression(tokens):
-        return None
+        if type(tokens) == str:
+            if hasattr(Datatype, tokens):
+                return Datatype[tokens]
+            if tokens == "true":
+                return Constant(True, Datatype.boolean)
+            if tokens == "false":
+                return Constant(False, Datatype.boolean)
+            if tokens[0] == "\"" and tokens[-1] == "\"" and len(tokens) >= 2:
+                return Constant(tokens[1:-1], Datatype.string)
+            if tokens.isdigit():
+                return Constant(int(tokens), Datatype.integer)
+            if tokens.isdecimal():
+                return Constant(float(tokens), Datatype.decimal)
+            # TODO: check date
+            return Identifier(tokens)
+        else:
+            tokens = [Parser.parse_expression(t) for t in tokens]
+            if len(tokens) == 2:
+                return UnaryOperator(*tokens[0:2])
+            while len(tokens) >= 3:
+                tokens = [BinaryOperator(*tokens[0:3])] + tokens[3:]
+            return tokens[0]
 
 
 if __name__ == "__main__":
     # TODO remove testcode for unittests
-    result = Grammar.form.parseFile("JurgenPeter/testForm.txt")
-    print(result)
+    filepath = "testForm.txt"
+    # result = Grammar.form.parseFile(filepath)
+    print(Parser.parse_file(filepath))
