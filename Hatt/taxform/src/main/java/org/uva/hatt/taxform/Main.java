6760f51385b86d72663284454aea1503e14dfb6a
Thanus <tthanusijan@gmail.com>
2017-02-07 11:19:03 +0100

    add antlr code from prototype

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/Main.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/Main.java
@@ -1,9 +1,17 @@
 package org.uva.hatt.taxform;
 
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.uva.hatt.taxform.antlr.QLLexer;
+import org.uva.hatt.taxform.antlr.QLParser;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ANTLRStringStream in = new ANTLRStringStream("12*(5-6)");
+        QLLexer lexer = new QLLexer(in);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        QLParser parser = new QLParser(tokens);
     }
 
 }
