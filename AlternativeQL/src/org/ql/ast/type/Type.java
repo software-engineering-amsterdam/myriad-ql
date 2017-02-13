203150173a0be30bbe3633fc623d8209e7e36c1b
Ahmed <ahmedmusaafir@gmail.com>
2017-02-13 13:17:12 +0100

    Type enumeration

--- AlternativeQL/src/org/ql/ast/type/Type.java
+++ AlternativeQL/src/org/ql/ast/type/Type.java
@@ -1,17 +1,5 @@
 package org.ql.ast.type;
 
-public class Type {
-    private String type;
-
-    public Type(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
+public enum Type {
+    BOOLEAN, FLOAT, INTEGER, STRING, MONEY, DATE
 }
