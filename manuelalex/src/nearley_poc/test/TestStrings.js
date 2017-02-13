199ecb801e697cade95ed71a65a7c48d9cf1dc02
Alex van Manen <alexvanmanen@wcw-eduroam-195-169-173-230.wireless.uva.nl>
2017-02-13 15:42:33 +0100

    made test conform grammar

--- manuelalex/src/nearley_poc/test/TestStrings.js
+++ manuelalex/src/nearley_poc/test/TestStrings.js
@@ -4,18 +4,18 @@
 
 
 export let test1 = 'form taxOfficeExample{\n' +
-    'Did you sell a house in 2010?\n' +
+    'question `Did you sell a house in 2010?`\n' +
     'hasSoldHouse: boolean\n' +
-    'Did you buy a house in 2010?\n' +
+    'question `Did you buy a house in 2010?`\n' +
     'hasBoughtHouse: boolean\n' +
-    'Did you enter a loan?\n' +
+    'question `Did you enter a loan?`\n' +
     'hasMaintLoan: boolean\n' +
     'if (hasSoldHouse) {\n' +
-    'What was the selling price?\n' +
+    'question `What was the selling price?`\n' +
     'sellingPrice: money\n' +
-    'What was the private debts for the sold house?\n' +
+    'question `What was the private debts for the sold house?`\n' +
     'privateDebt: money\n' +
-    'Value residue:\n' +
+    'answer `Value residue:`\n' +
     'valueResidue: money = (sellingPrice - privateDebt)\n' +
     '\n}' +
     '\n}';
