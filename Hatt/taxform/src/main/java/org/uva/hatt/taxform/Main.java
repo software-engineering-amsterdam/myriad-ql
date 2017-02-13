969cec98bb24c07db4845c37afbeb9bc808df5c3
Thanus <tthanusijan@gmail.com>
2017-02-13 11:35:16 +0100

    simple parsing

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/Main.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/Main.java
@@ -1,17 +1,26 @@
 package org.uva.hatt.taxform;
 
-import org.antlr.runtime.ANTLRStringStream;
-import org.antlr.runtime.CommonTokenStream;
-import org.uva.hatt.taxform.antlr.QLLexer;
-import org.uva.hatt.taxform.antlr.QLParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.uva.hatt.taxform.grammars.QLLexer;
+import org.uva.hatt.taxform.grammars.QLParser;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ANTLRStringStream in = new ANTLRStringStream("12*(5-6)");
-        QLLexer lexer = new QLLexer(in);
+    public static void main(String[] args) throws Exception {
+//        InputStream inputStream = new ByteArrayInputStream("form taxOfficeExample { \"Did you sell a house in 2010?\" hasSoldHouse: string }".getBytes());
+        InputStream inputStream = new ByteArrayInputStream("form taxOfficeExample { \"Did you sell a house in 2010?\" hasSoldHouse: string if (hasSoldHouse) { \"What was the selling price?\" sellingPrice: money \"What was the selling price?\" sellingPrice: money } }".getBytes());
+        ANTLRInputStream input = new ANTLRInputStream(inputStream);
+        QLLexer lexer = new QLLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         QLParser parser = new QLParser(tokens);
+
+        ParseTree tree = parser.form();
+        System.out.println(tree.toStringTree(parser));
     }
 
 }
