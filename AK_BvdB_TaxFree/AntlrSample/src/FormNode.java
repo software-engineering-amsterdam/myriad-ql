12e1895c6da4d4fe2b00e9c7571bedc2fe4e1470
Alex Kok <alexkok08@gmail.com>
2017-02-13 13:22:52 +0100

    Defined the way how we can generate the AST

--- AK_BvdB_TaxFree/AntlrSample/src/FormNode.java
+++ AK_BvdB_TaxFree/AntlrSample/src/FormNode.java
@@ -1,11 +1,8 @@
-import java.util.List;
-
 /**
  * Created by Alex on 7-2-2017.
  */
 public class FormNode extends Node {
-
-    public FormNode(int type, Node parent, List<Node> children) {
-        super(type, parent, children);
+    public FormNode(String name, Node parent) {
+        super(name, parent);
     }
 }
