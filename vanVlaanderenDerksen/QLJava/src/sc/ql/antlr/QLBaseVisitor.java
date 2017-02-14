da100c6c017b1191814d6f30dafe2b03dbcb44d9
Jona Derksen <jonaderksen@gmail.com>
2017-02-14 13:36:22 +0100

    models and parser update

--- vanVlaanderenDerksen/QLJava/src/sc/ql/antlr/QLBaseVisitor.java
+++ vanVlaanderenDerksen/QLJava/src/sc/ql/antlr/QLBaseVisitor.java
@@ -59,13 +59,6 @@ public class QLBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements QLV
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitNotExpr(QLParser.NotExprContext ctx) { return visitChildren(ctx); }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
 	@Override public T visitOpExpr(QLParser.OpExprContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
@@ -94,14 +87,14 @@ public class QLBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements QLV
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRelExpr(QLParser.RelExprContext ctx) { return visitChildren(ctx); }
+	@Override public T visitBoolExpr(QLParser.BoolExprContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitBoolExpr(QLParser.BoolExprContext ctx) { return visitChildren(ctx); }
+	@Override public T visitRelExpr(QLParser.RelExprContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
--- vanVlaanderenDerksen/QLJava/src/sc/ql/ast/Visitor.java
+++ vanVlaanderenDerksen/QLJava/src/sc/ql/ast/Visitor.java
@@ -23,51 +23,51 @@ public class Visitor extends QLBaseVisitor<Object> {
 
 	@Override 
 	public IfStatement visitIf_statement(QLParser.If_statementContext ctx) {
-		
+		return new IfStatement();
 	}
 
 	@Override 
 	public ConditionalBlock visitConditional_block(QLParser.Conditional_blockContext ctx) {
-		
+		return new ConditionalBlock();
 	}
 
 	@Override 
 	public ID visitIdAtom(QLParser.IdAtomContext ctx) {
-		
+		return new ID();
 	}
 
 	@Override 
 	public OpExpression visitOpExpr(QLParser.OpExprContext ctx) {
-		
+		return new OpExpression();
 	}
 
 	@Override 
 	public IntExpression visitIntAtom(QLParser.IntAtomContext ctx) {
-	
+		return new IntExpression();
 	}
 	
 	@Override 
 	public StrExpression visitStrAtom(QLParser.StrAtomContext ctx) {
-		
+		return new StrExpression);
 	}
 
 	@Override 
 	public BoolExpression visitBoolAtom(QLParser.BoolAtomContext ctx) {
-		
+		return new BoolExpression();
 	}
 
 	@Override 
 	public BoolExpression visitRelExpr(QLParser.RelExprContext ctx) {
-		
+		return new BoolExpression();
 	}
 
 	@Override 
 	public BoolExpression visitBoolExpr(QLParser.BoolExprContext ctx) {
-		
+		return new BoolExpression();
 	}
 
 	@Override 
 	public Expression visitParenExpr(QLParser.ParenExprContext ctx) {
-		
+		return new Expression();
 	}	
 }
