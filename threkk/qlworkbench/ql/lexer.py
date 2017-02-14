94284bce3f1f3ee932bf3a12dd934f0970de94d4
Alberto Mtnz <alberto@threkk.com>
2017-02-14 01:41:19 +0100

    Adds documentation to the QL lexer.

--- threkk/qlworkbench/ql/lexer.py
+++ threkk/qlworkbench/ql/lexer.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+This module defines the parser of the QL language. It uses the PLY library to
+take a string and return instead a stream of tokens. PLY is often used as a
+single script but in this case we use a class for better encapsulation and code
+organisation.
+"""
 import ply.lex as lex
+from ply.lex import TOKEN
 
 
 class QLLexer(object):
+    """The QLLexer parser shaped as a class"""
 
-    # Reserved words
+    # Reserved words. The documentation
+    # (http://www.dabeaz.com/ply/ply.html#ply_nn6) advises to store them
+    # separately in order to substitute common string for reserved words.
     reserved = {
         'boolean': 'BOOLEAN',
         'date': 'DATE',
@@ -40,10 +51,9 @@ class QLLexer(object):
         'QUESTION',  # "A question?"
         'COMMENT',   # // This is a comment
         'WHITESPACE'
-    ) + tuple(reserved.values())
+    ) + tuple(reserved.values())  # Added also the reserved words.
 
-    # Rules that define the tokens.
-    t_FORM = r'form'
+    # Reggular expressions that define the tokens.
     t_LBRACK = r'\{'
     t_RBRACK = r'\}'
     t_LPAREN = r'\('
@@ -62,30 +72,49 @@ class QLLexer(object):
     t_MULT = r'\*'
     t_DIV = r'/'
     t_ASSIGN = r'='
-    t_QUESTION = r'\"[\w\s\.\,\:\?\!\-\+\(\)/]+\"'
+    t_QUESTION = r'\".+\"'
 
+    # Regular expressions for tokens that require special interactions.
+    ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
+    COMMENT = r'\/\/.*'
+    WHITESPACE = r'\s+'
+
+    @TOKEN(ID)
     def t_ID(self, t):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
+        """
+        Identifies the ID tokens. It also identifies all the reserved words by
+        looking them up in a dictionary and substituting the ID type for the
+        reserved word.
+        """
         t.type = self.reserved.get(t.value, 'ID')  # Check for reserved words
         return t
 
+    @TOKEN(COMMENT)
     def t_COMMENT(self, t):
-        r'\/\/.*'
+        """Comments are ignored"""
         pass
 
+    @TOKEN(WHITESPACE)
     def t_WHITESPACE(self, t):
-        r'\s+'
+        """White spaces are ignored"""
         pass
 
-    # Error handling rule
     def t_error(self, t):
+        """
+        Error handler. It logs when a character is not recognised by any token
+        of the lexer.
+        """
+        # Following the recommendations in the Hitchhiker's guide to Python
+        # (http://docs.python-guide.org/en/latest/writing/logging/)
         print('Illegal character "{value}"'.format(value=t.value[0]))
         t.lexer.skip(1)
 
     def __init__(self, **kwargs):
+        """Initialises the lexer. It complies with PLY requirements."""
         self.lexer = lex.lex(module=self, debug=0, **kwargs)
 
     def getLexer(self):
+        """Returns the initialised lexer"""
         return self.lexer
 
 
