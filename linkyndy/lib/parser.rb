20184053dd6db429f504c444a0d2c56f40c14551
Andrei Horak <linkyndy@gmail.com>
2017-02-14 13:36:37 +0100

    Improve types in grammar

--- linkyndy/lib/parser.rb
+++ linkyndy/lib/parser.rb
@@ -4,31 +4,28 @@ class Parser < Parslet::Parser
   rule(:space) { match('\s').repeat(1) }
   rule(:space?) { space.maybe }
 
-  rule(:lparen) { str('\(') }
-  rule(:rparen) { str('\)') }
+  rule(:lparen) { str('(') }
+  rule(:rparen) { str(')') }
   rule(:quote) { str('"') }
   rule(:qmark) { str('?') }
-  rule(:alnum) { match['a-zA-z0-9'].repeat(1) }
-  rule(:num) { match['0-9'].repeat(1) }
+  rule(:string) { match['a-zA-z0-9\s'].repeat(1) }
+  rule(:integer) { match['0-9'].repeat(1) }
+  rule(:boolean) { str('true') | str('false') }
+  rule(:literal) { string | integer | boolean }
+  rule(:operator) { str('+') | str('-') | str('*') | str('/') }
 
-  rule(:comment) { str('#') >> space? >> alnum }
+  rule(:comment) { str('#') >> string.maybe }
 
   rule(:type) { str('text') | str('bool') | str('number') | str('money') }
 
-  rule(:literal) { string | integer | boolean }
-  rule(:string) { quote >> alnum >> quote }
-  rule(:integer) { num }
-  rule(:boolean) { str('true') | str('false') }
-  rule(:operator) { str('+') | str('-') | str('*') | str('/') }
-
-  rule(:identifier) { alnum }
+  rule(:identifier) { match['a-zA-Z'] >> match['a-zA-z0-9'].repeat(1) }
   rule(:expression) { lparen >> space? >> (operation | identifier | literal) >> space? >> rparen }
   rule(:operation) { expression >> space? >> operator >> space? >> expression }
   rule(:block) { ((if_statement | item) >> space).repeat(1) }
 
   rule(:if_statement) { str('if') >> space >> expression >> space >> block >> space >> (str('else') >> space >> block >> space).maybe >> str('end') }
 
-  rule(:question) { quote >> alnum >> qmark >> quote }
+  rule(:question) { quote >> string >> qmark >> quote }
   rule(:answer) { type >> spaces? >> identifier >> (spaces? >> str('=>') >> spaces? >> expression).maybe }
 
   rule(:item) { question >> spaces? >> answer }
