f4def6d67206ae10f19b7fcc26971083cc0e4a5b
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2017-02-14 13:13:06 +0100

    Updated imports of literals after refactoring

--- AlternativeQL/test/org/ql/parser/ParserTest.java
+++ AlternativeQL/test/org/ql/parser/ParserTest.java
@@ -2,6 +2,7 @@ package org.ql.parser;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.ql.ast.expression.literals.BooleanLiteral;
 import org.ql.ast.form.Form;
 import org.ql.ast.statement.If;
 import org.ql.ast.statement.Question;
@@ -35,6 +36,20 @@ public class ParserTest extends Assert {
         assertEquals("hasSoldHouse", ((Question) ast.getStatement(0)).getId().toString());
         assertEquals("Did you sell a house in 2010?", ((Question) ast.getStatement(0)).getQuestionText().toString());
         assertEquals(Type.BOOLEAN, ((Question) ast.getStatement(0)).getType());
+    }
+
+    @Test
+    public void shouldCreateFormWithQuestionStatementsHoldingDefaultValues() {
+        Parser parser = new Parser();
+        String inputCode = "form MyNewForm {" +
+                "    boolean hasSoldHouse: \"Did you sell a house in 2010?\" = true;\n" +
+                "    boolean hasBoughtHouse: \"Did you buy a house in 2010?\" = false;" +
+                "}";
+
+        Form ast = parser.parse(inputCode);
+
+        assertEquals(true, ((BooleanLiteral) ((Question) ast.getStatement(0)).getDefaultValue()).getBooleanLiteral());
+        assertEquals(false, ((BooleanLiteral) ((Question) ast.getStatement(1)).getDefaultValue()).getBooleanLiteral());
     }
 
     @Test
