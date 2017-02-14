84f893b614a126db2548f889b1924b2faa1a2b23
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2017-02-14 13:57:36 +0100

    Introduced StatementTest

--- AlternativeQL/test/org/ql/parser/ExpressionTest.java
+++ AlternativeQL/test/org/ql/parser/ExpressionTest.java
@@ -10,28 +10,6 @@ import static org.junit.Assert.*;
 
 public class ExpressionTest {
     @Test
-    public void shouldCreateFormWithQuestionsInsideIf() {
-        Parser parser = new Parser();
-        String inputCode = "form ExampleForm {\n" +
-                "    boolean hasSoldHouse: \"Did you sell a house in 2010?\";\n" +
-                "    boolean hasBoughtHouse: \"Did you buy a house in 2010?\";\n" +
-                "    boolean hasMainLoan:  \"Did you enter a loan?\";\n" +
-                "\n" +
-                "    if (hasSoldHouse) {\n" +
-                "        money sellingPrice: \"What was the selling price?\";\n" +
-                "        money privateDebt: \"Private debts for the sold house:\";\n" +
-                "    }\n" +
-                "}";
-        int expectedIfStatementLocation = 3;
-        int expectedAmountOfQuestionsInsideIfStatement = 2;
-
-        Form ast = parser.parse(inputCode);
-        If ifStatement = (If) ast.getStatement(expectedIfStatementLocation);
-        assertSame(expectedAmountOfQuestionsInsideIfStatement, ifStatement.getStatements().size());
-        assertEquals("hasSoldHouse", ifStatement.getCondition().toString());
-    }
-
-    @Test
     public void shouldCreateFormWithEqualsExpression() {
         Parser parser = new Parser();
         int expectedLeftHandValue = 5;
