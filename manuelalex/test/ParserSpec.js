30069c27ebc734bdcc63a75c2448c7532988d685
Alex van Manen <alexvanmanen@hotmail.com>
2017-02-16 20:57:17 +0100

    Valid test for parser

--- manuelalex/test/ParserSpec.js
+++ manuelalex/test/ParserSpec.js
@@ -2,33 +2,35 @@
  * Created by alexvanmanen on 06-02-17.
  */
 import chai                         from 'chai';
+import {test3,test2}                      from '../src/test/TestStrings.js';
 import {
     loadDependencies,
     mockDependency
 }                                   from './Bootstrap.js';
 
-let expect = chai.expect;
+import Parser                   from '../src/Parser.js';
+import Form                     from '../src/Form.js';
+
+const expect = chai.expect;
 
 describe('DataSource', () => {
     let imports = {};
 
     before(() => loadDependencies({
-            Parser: System.normalizeSync('./src/Parser.js'),
-            Form: System.normalizeSync('./src/Form.js')
+            // Parser: System.normalizeSync('./src/Parser.js'),
+            // Form: System.normalizeSync('./src/Form.js')
         }
         ).then((importedObjects) => {
             imports = importedObjects;
         })
     );
 
-    describe('Parser.parse', () => {
-        it('returns valid parsed text', () => {
-            let parser = new imports.Parser();
-            let form = new imports.Form("My valid input");
-            //parser.lexer("");
-
-            // TODO make actual input
-            expect(parser.parse('My valid input')._name).to.equal(form._name);
+    describe('Parser.parse(test2)', () => {
+        it('returns valid AST', () => {
+            const parser = new Parser();
+            let f = new Form();
+            f.name="y";
+            expect(parser.parse(test2)).to.deep.equal(f);
         });
     });
 });
