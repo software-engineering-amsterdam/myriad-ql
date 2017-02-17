0b9646a1f4354e81c32f8ee0a2b9c23f12fdc59f
jetsekoopmans <jetsekoopmans@hotmail.com>
2017-02-17 19:40:39 +0100

    refactor a bit

--- koopmans/lib/type_checker/variable_visitor.rb
+++ koopmans/lib/type_checker/variable_visitor.rb
@@ -5,6 +5,10 @@ class VariableVisitor < BaseChecker
     subject.statements.map{|u| visit_statement(u)}.flatten
   end
 
+  def visit_if_statement(subject)
+    subject.block.map { |statement| visit_statement(statement) }
+  end
+
   def visit_question(subject)
     subject.variable.name
   end
