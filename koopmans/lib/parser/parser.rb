77b242094c049c16cefb6ef8d5c3e2de2476328d
aijkoopmans <arendkoopmans@gmail.com>
2017-02-17 11:50:42 +0100

    clean up

--- koopmans/lib/parser/parser.rb
+++ koopmans/lib/parser/parser.rb
@@ -8,6 +8,12 @@ require 'parslet'
 
 # parser for forms
 class Parser < Parslet::Parser
+  include ExpressionRules
+  include TypeRules
+  include LiteralRules
+  include VariableRules
+  include StatementRules
+  include FormRules
 
   rule(:spaces) do
     match('\s').repeat(1)
@@ -16,110 +22,6 @@ class Parser < Parslet::Parser
   rule(:spaces?) do
     spaces.maybe
   end
-  #
-  # def self.symbols(symbols)
-  #   symbols.each do |name, symbol|
-  #     rule(name) { str(symbol) }
-  #   end
-  # end
-
-  # symbols left_brace: '{',
-          # right_brace: '}'
-          # left_parenthesis: '(',
-          # right_parenthesis: ')'
-          # quote: '"'
-          # colon: ':'
-          # assign: '='
-
-
-  # expression
-  # rule(:calculation) do
-  #   variable_or_literal.as(:left) >> operator >> expression.as(:right)
-  # end
-
-  # rule(:operator) do
-  #   (str('-') | str('+') | str('*') | str('/') | str('<=') | str('>=') | str('==') | str('!=') | str('<') | str('>') | str('&&') | str('||') | str('!')).as(:operator) >> spaces?
-  # end
-
-  # rule(:operator) do
-  #   BinaryExpression.descendants.map { |binary_expression| str(binary_expression.to_operator) }.reduce(&:|).as(:operator) >> spaces?
-  # end
-
-  include ExpressionRules
-
-  # rule(:expression) do
-  #   left_parenthesis >> spaces? >> expression.as(:expression) >> spaces? >> right_parenthesis >> spaces? | calculation | variable_or_literal
-  # end
-
-
-  # question
-  # rule(:type) do
-  #   match('[boolean integer').as(:type) >> spaces?
-  #   (str('boolean') | str('integer') | str('date') | str('decimal') | str('string') | str('money')).as(:type) >> spaces?
-  # end
-
-  # rule(:type) do
-  #   Type.descendants.map { |type| str(type.type) }.reduce(&:|).as(:type) >> spaces?
-  # end
-  include TypeRules
-
-  # variables or literals
-  # rule(:variable_or_literal) do
-  #   (boolean_negation? >> boolean_literal | integer_negation? >> integer_literal | string_literal | negation? >> variable) >> spaces?
-  # end
-
-  # rule(:integer_negation?) do
-  #   str('-').as(:integer_negation).maybe
-  # end
-  #
-  # rule(:boolean_negation?) do
-  #   str('!').as(:boolean_negation).maybe
-  # end
-  #
-  # rule(:negation?) do
-  #   (str('!') | str('-')).as(:negation).maybe
-  # end
-
-  # rule(:variable) do
-  #   match('\w+').repeat(1).as(:variable)
-  # end
-
-  include LiteralRules
-  include VariableRules
-
-  # rule(:variable_assignment) do
-  #   variable >> colon >> spaces?
-  # end
-
-  # rule(:assignment?) do
-  #   (assign >> spaces? >> expression).maybe >> spaces?
-  # end
-
-  # rule(:question) do
-  #   (string_literal >> variable_assignment >> type >> assignment?).as(:question) >> spaces?
-  # end
-  #
-  #
-  # # if block
-  # rule(:block) do
-  #   left_brace >> spaces? >> (question | if_statement).repeat.as(:block) >> right_brace >> spaces?
-  # end
-  #
-  # rule(:if_statement) do
-  #   # IfStatement.to_rule
-  #
-  #   (str('if') >> spaces? >> expression >> block).as(:if_statement)
-  # end
-
-  include StatementRules
-
-
-  # form
-  # rule(:form) do
-  #   spaces? >> (str('form') >> spaces? >> variable >> spaces? >> block).as(:form)
-  # end
-
-  include FormRules
 
   root :form
 end
