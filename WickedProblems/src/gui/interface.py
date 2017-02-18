58ffa8367db7e8b936104846b6d189b3fc7a7e90
vdweegen <cas@vdweegen.com>
2017-02-18 19:52:40 +0100

    [WickedProblems] Initial version of GUI drawing (storing variables + basic evalution of ConditionalNode.
    TODO: Write actual evaluation for complex evaluations.

--- WickedProblems/src/gui/interface.py
+++ WickedProblems/src/gui/interface.py
@@ -6,6 +6,7 @@ if cur_version >= req_version:
     from pyparsing import *
     from tkinter import *
     from ast.node import *
+    from parser.ql import QL
 else:
     exit("Did you forget to run it using python >= 3.0 ??")
 
@@ -34,25 +35,40 @@ class Interface(Frame):
     def callback(self):
         print("callback!")
 
+    def print_current_variables(self):
+        __ret = ""
+        for k,v in self.__variables.items():
+            print(k + ":" + str(v.get()))
+
     def refresh(self):
-        self.__root.update()
-        print(self.__variables)
-        print("refresh!")
+        for widget in self.__root.winfo_children():
+            widget.destroy()
+        self.construct_interface()
+        self.print_current_variables()
+        # print("refresh!")
+
+    def variable_is_defined(self, variable):
+        try:
+            self.__variables[variable]
+        except KeyError:
+            return False
+        return True
 
     def construct_from_node(self,node):
         # Get possible displayables
         if(node.__class__ == QuestionNode):
             if(node._field_type == "boolean"):
-                self.__variables[node.get_identifier()] = IntVar()
+                if not self.variable_is_defined(node.get_identifier()):
+                    self.__variables[node.get_identifier()] = IntVar()
                 item = Checkbutton(self.__root,
                                    text=node._text,
                                    variable=self.__variables[node.get_identifier()],
                                    command=self.refresh)
             elif(node._field_type == "string"):
-                self.__variables[node.get_identifier()] = StringVar()
+                if not self.variable_is_defined(node.get_identifier()):
+                    self.__variables[node.get_identifier()] = StringVar()
                 item = Entry(self.__root,
-                             textvariable=self.__variables[node.get_identifier()],
-                             command=self.refresh)
+                             textvariable=self.__variables[node.get_identifier()])
             elif(node._field_type == "integer"):
                 return
             elif(node._field_type == "data"):
@@ -60,7 +76,11 @@ class Interface(Frame):
             elif(node._field_type == "decimal"):
                 return
             elif(node._field_type == "money"):
-                return
+                # TODO: Improve Money Input Check
+                if not self.variable_is_defined(node.get_identifier()):
+                    self.__variables[node.get_identifier()] = StringVar()
+                item = Entry(self.__root,
+                             textvariable=self.__variables[node.get_identifier()])
             elif(node._field_type == "currency"):
                 return
             else:
@@ -70,23 +90,35 @@ class Interface(Frame):
             node.get_interface_item().pack()
         elif(node.__class__ == ConditionalNode):
             # evaluate condition
-            print(node._evaluation)
-            # recursive
-            for child in node.get_children():
-                self.construct_from_node(child)
+            # print(QL.match_evaluation.parseString(node._evaluation))
+            # print(node._evaluation)
+            # TODO: parse the evaluation [LINE BELOW IS TEMPORARY]
+            _var = QL.evaluation_unquoted.parseString(node._evaluation)[0]
+            # print(_var)
+            # print(self.__variables[_var].get())
+            # when evaluated as TRUE
+            if(self.__variables[_var].get() == 1):
+                # recursive
+                for child in node.get_children():
+                    # print(child.__class__.__name__)
+                    self.construct_from_node(child)
         elif(node.__class__ == StatementNode):
             # statement node
             Label(self.__root, text=node._text).pack()
         else:
             pass
 
+    def construct_interface(self):
+        # walk through the nodes
+        for content_node in self.__tree.get_children():
+            self.construct_from_node(content_node)
+
     def build_interface(self):
         # Title (based on identifier of root node)
         self.__root.title(self.__tree.get_identifier())
 
-        # walk through the nodes
-        for content_node in self.__tree.get_children():
-            self.construct_from_node(content_node)
+        # construct widgets (frame content)
+        self.construct_interface()
 
         self.menu = Menu(self.__root)
         self.__root.config(menu=self.menu)
