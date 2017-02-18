fd69bb378a0d312d32bbe6258ace29d978e7693c
Floris Velleman <florisvelleman@hotmail.com>
2017-02-18 20:13:05 +0100

    Added overflow/format tests.

--- WeekendWarriors/DSL/Tests/QL/AST/Literals.cs
+++ WeekendWarriors/DSL/Tests/QL/AST/Literals.cs
@@ -67,14 +67,15 @@ namespace Tests.QL.AST
             {
                 { "123456", "Test failure: 123456 literal" },
                 { "0", "Test failure: 0 literal" },
-                { Int32.MaxValue.ToString(), "Test failure: Int32 max" }
+                { Int32.MaxValue.ToString(), "Test failure: Int32 max" },
+                { "2147483648", "Test failure: Int32 max + 1" }
             };
 
             foreach (var testCase in testCases)
             {
                 Assert.AreEqual(
-                    ExpressionFromTestCase<QLNumber>(testCase.Key).Value,
-                    Int32.Parse(testCase.Key), testCase.Value
+                    ExpressionFromTestCase<QLNumber>(testCase.Key).Value.ToString(),
+                    testCase.Key, testCase.Value
                 );
             }
         }
@@ -86,7 +87,7 @@ namespace Tests.QL.AST
             {
                 { "123.456", "Test failure: 123.456 literal" },
                 { "0.", "Test failure: 0. literal" },
-                { "0.123", "Test failure: 0.123" }
+                { ".123", "Test failure: 0.123" }
             };
 
             foreach (var testCase in testCases)
