dcd8cb2a24fedb0c3c84b88575010f5d42523f48
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-02-20 13:56:59 +0100

    removed instance variable by JComponent superclass

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/StringQuestion.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/StringQuestion.java
@@ -2,24 +2,33 @@ package org.uva.taxfree.model;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Set;
 
 public class StringQuestion extends NamedNode {
-    private String mValue;
-
+    private JTextField mTextField;
     public StringQuestion(String description, String id) {
         super(description, id);
-        mValue = new String("");
+        mTextField = new JTextField("");
     }
 
     @Override
     public void fillPanel(JPanel parentPanel) {
-        JTextField f = new JTextField(mValue);
-        f.setPreferredSize(new Dimension(100,25));
-        parentPanel.add(f);
+        mTextField.setPreferredSize(new Dimension(100,25));
+        setVisible(true);
+        parentPanel.add(mTextField);
+    }
+
+    public void setVisible(boolean isVisible){
+        mTextField.setVisible(isVisible);
     }
 
     @Override
     public String getType() {
         return "string";
+    }
+
+    @Override
+    public void printValue(){
+        System.out.println("Value of string: " + mTextField.getText());
     }
 }
