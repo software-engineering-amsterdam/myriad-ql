5cd450174a467ccc32462589d07b5e88fb6e170a
Rico Lamein <rico_lamein@hotmail.com>
2017-02-20 12:43:35 +0100

    Changed QLFloat so it's compatible with eval visitor

--- TBD/QL/src/ql/ast/literals/QLFloat.java
+++ TBD/QL/src/ql/ast/literals/QLFloat.java
@@ -16,7 +16,117 @@ public class QLFloat extends QLLiteral {
         return qlFloat;
     }
 
+    @Override
+    public String toString() {
+        return String.valueOf(qlFloat);
+    }
+
     public <T> T accept(ASTVisitor<T> visitor) {
         return visitor.visit(this);
+    }
+
+    @Override
+    public QLLiteral add(QLLiteral other) {
+        return other.addEval(this);
+    }
+
+    @Override
+    protected QLLiteral addEval(QLFloat other) {
+        return new QLFloat(this.getValue() + other.getValue());
+    }
+
+    @Override
+    protected QLLiteral addEval(QLInt other) {
+        return new QLFloat(this.getValue() + other.getValue());
+    }
+
+    @Override
+    public QLLiteral div(QLLiteral other) {
+        return other.divEval(this);
+    }
+
+    @Override
+    public QLLiteral divEval(QLFloat other) {
+        return new QLFloat(other.getValue() / this.getValue());
+    }
+
+    @Override
+    public QLLiteral divEval(QLInt other) {
+        return new QLFloat(other.getValue() / this.getValue());
+    }
+
+    @Override
+    public QLLiteral mul(QLLiteral other) {
+        return other.mulEval(this);
+    }
+
+    @Override
+    public QLLiteral mulEval(QLFloat other) {
+        return new QLFloat(this.getValue() * other.getValue());
+    }
+
+    @Override
+    public QLLiteral mulEval(QLInt other) {
+        return new QLFloat(this.getValue() * other.getValue());
+    }
+
+    @Override
+    public QLLiteral gEq(QLLiteral other) {
+        return (new QLBoolean(other.equals(this))).or(gT(other));
+    }
+
+    @Override
+    public QLLiteral gT(QLLiteral other) {
+        return lEq(other).not();
+    }
+
+    @Override
+    public QLLiteral gTEval(QLFloat other) {
+        return  new QLBoolean(this.getValue() > other.getValue());
+    }
+
+    @Override
+    public QLLiteral gTEval(QLInt other) {
+        return  new QLBoolean(this.getValue() > other.getValue());
+    }
+
+    @Override
+    public QLLiteral lEq(QLLiteral other) {
+        return (new QLBoolean(other.equals(this))).or(lT(other));
+    }
+
+    @Override
+    public QLLiteral lT(QLLiteral other) {
+        return other.gTEval(this);
+    }
+
+    @Override
+    public QLLiteral nEq(QLLiteral other) {
+        return new QLBoolean(!other.equals(this));
+    }
+
+    @Override
+    public QLLiteral sub(QLLiteral other) {
+        return other.neg().add(this);
+    }
+
+    @Override
+    public QLLiteral neg() {
+        return new QLFloat(-getValue());
+    }
+
+    @Override
+    public QLLiteral pos() {
+        return new QLFloat(-getValue());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        QLFloat qlFloat1 = (QLFloat) o;
+
+        return qlFloat == qlFloat1.qlFloat;
     }
 }
