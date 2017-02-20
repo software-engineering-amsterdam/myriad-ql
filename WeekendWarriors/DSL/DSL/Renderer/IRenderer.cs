c964b738e43c1b9a3b9a8e5ccd68e6cb8633852d
Bouke Grevelt <b.grevelt@gmail.com>
2017-02-20 15:16:18 +0100

    Added some placeholders for interfaces and some general value implementation.

---
+++ WeekendWarriors/DSL/DSL/Renderer/IRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum Visibility
+{
+    Visible,
+    Hidden
+}
+
+interface IQuestion
+{
+    string Name { get; }
+    string Body { get; }
+    Visibility Visibility { get; }
+};
+
+namespace DSL.Renderer
+{
+    interface IRenderer
+    {
+        void AddQuestion(IQuestion question);
+        void SetValue(string name, DSL.Value.IValue value);
+        void SetVisibility(string name, Visibility visibility);
+    }
+}
---
+++ WeekendWarriors/DSL/DSL/Value/Value.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSL.Value
+{
+    class Value<T> : IValue
+    where T : struct
+    {
+        protected T? Val = null;
+
+        public virtual int? AsInt()
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual decimal? AsDecimal()
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual bool? AsBool()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
