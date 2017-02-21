a6db2bb17a4ba60c8838be4c2ef50409b510316a
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-02-21 16:49:32 +0100

    Pulled evaluator up to ConditionNode

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/ConditionNode.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/ConditionNode.java
@@ -1,5 +1,22 @@
 package org.uva.taxfree.model;
 
+import org.uva.taxfree.util.Evaluator;
+
+import javax.script.ScriptException;
+
 public abstract class ConditionNode extends Node {
 
+    public String evaluate() {
+        try {
+            return tryEvaluate();
+        } catch (ScriptException e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occurred whilst evaluating " + toString());
+        }
+    }
+
+    // Allows the typeChecker to perform a testrun on all expressions.
+    public String tryEvaluate() throws ScriptException {
+        return Evaluator.calculate(toString());
+    }
 }
