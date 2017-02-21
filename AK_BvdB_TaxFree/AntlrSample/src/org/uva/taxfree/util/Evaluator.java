b29d9a40799240fc1b5f0a543b6d5a66f422c01e
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-02-21 15:14:48 +0100

    Specified exception

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/util/Evaluator.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/util/Evaluator.java
@@ -2,9 +2,10 @@ package org.uva.taxfree.util;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class Evaluator {
-    public static String calculate(String calculation) throws Exception {
+    public static String calculate(String calculation) throws ScriptException {
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
         return engine.eval(calculation).toString();
