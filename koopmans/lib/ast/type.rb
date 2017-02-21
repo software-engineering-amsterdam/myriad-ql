e4e156e0206396b0e3feba019c6178416f9a86d2
aijkoopmans <arendkoopmans@gmail.com>
2017-02-21 17:14:53 +0100

    parser clean up

--- koopmans/lib/ast/type.rb
+++ koopmans/lib/ast/type.rb
@@ -6,50 +6,30 @@ class Type
 end
 
 class BooleanType < Type
-  def self.type
-    'boolean'
-  end
-
   def self.accept_types
     [BooleanType]
   end
 end
 
 class IntegerType < Type
-  def self.type
-    'integer'
-  end
-
   def self.accept_types
     [IntegerType, MoneyType]
   end
 end
 
 class DateType < Type
-  def self.type
-    'date'
-  end
-
   def self.accept_types
     [DateType]
   end
 end
 
 class DecimalType < Type
-  def self.type
-    'decimal'
-  end
-
   def self.accept_types
     [DecimalType]
   end
 end
 
 class StringType < Type
-  def self.type
-    'string'
-  end
-
   def self.accept_types
     [StringType]
   end
@@ -57,10 +37,6 @@ end
 
 # TODO make money type allow +-/* operations
 class MoneyType < Type
-  def self.type
-    'money'
-  end
-
   def self.accept_types
     [MoneyType, IntegerType]
   end
