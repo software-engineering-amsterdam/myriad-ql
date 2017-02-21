617cda249998dec3244607196f8efeb7284f32a3
aijkoopmans <arendkoopmans@gmail.com>
2017-02-21 10:31:17 +0100

    ja of nee

--- koopmans/lib/ast/statement.rb
+++ koopmans/lib/ast/statement.rb
@@ -55,14 +55,62 @@ class Parser < Parslet::Parser
 end
 
 class Transformer < Parslet::Transform
-  Type.descendants.each do |type|
-    rule(question: {string: simple(:string), variable: simple(:variable), type: type.type}) do
-      Question.new(string, Variable.new(variable), type.new)
-    end
-    rule(question: {string: simple(:string), variable: simple(:variable), type: type.type, expression: subtree(:expression)}) do
-      Question.new(string, Variable.new(variable), type.new, expression)
-    end
-  end
+  # Type.descendants.each do |type|
+  #   rule(question: {string: simple(:string), variable: simple(:variable), type: type.type}) do
+  #     Question.new(string, Variable.new(variable), type.new)
+  #   end
+  #   rule(question: {string: simple(:string), variable: simple(:variable), type: type.type, expression: subtree(:expression)}) do
+  #     Question.new(string, Variable.new(variable), type.new, expression)
+  #   end
+  # end
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'boolean'}) {
+    Question.new(string, Variable.new(variable), BooleanType.new)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'integer'}) {
+    Question.new(string, Variable.new(variable), IntegerType.new)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'money'}) {
+    Question.new(string, Variable.new(variable), MoneyType.new)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'string'}) {
+    Question.new(string, Variable.new(variable), StringType.new)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'decimal'}) {
+    Question.new(string, Variable.new(variable), DecimalType.new)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'date'}) {
+    Question.new(string, Variable.new(variable), DateType.new)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'boolean', expression: subtree(:expression)}) {
+    Question.new(string, Variable.new(variable), BooleanType.new, expression)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'integer', expression: subtree(:expression)}) {
+    Question.new(string, Variable.new(variable), IntegerType.new, expression)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'money', expression: subtree(:expression)}) {
+    Question.new(string, Variable.new(variable), MoneyType.new, expression)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'string', expression: subtree(:expression)}) {
+    Question.new(string, Variable.new(variable), StringType.new, expression)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'decimal', expression: subtree(:expression)}) {
+    Question.new(string, Variable.new(variable), DecimalType.new, expression)
+  }
+
+  rule(question: {string: simple(:string), variable: simple(:variable), type: 'date', expression: subtree(:expression)}) {
+    Question.new(string, Variable.new(variable), DateType.new, expression)
+  }
 
   rule(if_statement: {expression: subtree(:expression), block: subtree(:block)}) do
     IfStatement.new(expression, block)
