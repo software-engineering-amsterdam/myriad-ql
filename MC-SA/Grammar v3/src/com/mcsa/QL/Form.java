80d912892c74bd00f5c2ca31637aece8350c7b17
Sotiris Alfonsos <sotiris.alfonsos@gmail.com>
2017-02-21 14:09:06 +0100

    classes

--- MC-SA/Grammar v3/src/com/mcsa/QL/Form.java
+++ MC-SA/Grammar v3/src/com/mcsa/QL/Form.java
@@ -5,16 +5,30 @@ import java.util.ArrayList;
 /**
  * Created by matt on 20/02/2017.
  */
-public class Form implements Node{
+public class Form {
 
-    public ArrayList<Question> questionList;
-    public String name;
+    private ArrayList<Question> questionList;
+    private ArrayList<IfStatement> ifStatementList;
+    private String formName;
 
-    public Form(String name, ArrayList<Question> questionList)
-    {
-        this.name = name;
+    public Form (String name) {
+        this.formName = name;
         this.questionList = new ArrayList<>();
+        this.ifStatementList = new ArrayList<>();
+    }
+
+    public void formAddQuestion( ArrayList<Question> questionList)
+    {
         this.questionList = questionList;
     }
+
+    public void formAddIfStatement( ArrayList<IfStatement> IfList)
+    {
+        this.ifStatementList = IfList;
+    }
+
+    public ArrayList<Question> getFormQuestionList() { return questionList; }
+
+    public ArrayList<IfStatement> getFormIfStatementList() { return ifStatementList; }
 
 }
--- MC-SA/Grammar v3/src/com/mcsa/QL/IfStatement.java
+++ MC-SA/Grammar v3/src/com/mcsa/QL/IfStatement.java
@@ -1,7 +1,33 @@
 package com.mcsa.QL;
 
+import java.util.ArrayList;
+
 /**
  * Created by matt on 21/02/2017.
  */
 public class IfStatement implements Node {
+
+    private ArrayList<Question> questionList;
+    private ArrayList<IfStatement> ifStatementList;
+    public String name;
+
+    public IfStatement() {
+        this.questionList = new ArrayList<>();
+        this.ifStatementList = new ArrayList<>();
+    }
+
+    public void ifStatementAddQuestion( ArrayList<Question> quList)
+    {
+        this.questionList = quList;
+    }
+
+    public void ifStatementAddIfStatement( ArrayList<IfStatement> IfList)
+    {
+        this.ifStatementList = IfList;
+    }
+
+    public ArrayList<Question> getQuestionList() { return questionList; }
+
+    public ArrayList<IfStatement> getIfStatementList() { return ifStatementList; }
+
 }
