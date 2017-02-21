bd5f6e9ebabe27617880d7a03794e25f9cd4b698
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-02-21 16:50:00 +0100

    added ParenthesizedExpression

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/ParenthesizedExpressionNode.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/ParenthesizedExpressionNode.java
@@ -1,7 +1,23 @@
 package org.uva.taxfree.model;
 
-public class ParenthesizedExpressionNode extends ExpressionNode {
+public class ParenthesizedExpressionNode extends ConditionNode {
+    private Node mChild;
+
     public ParenthesizedExpressionNode(String label) {
-        super(label);
+        super();
+    }
+
+    @Override
+    public void addChild(Node child) {
+        if (mChild == null) {
+            mChild = child;
+        } else {
+            throw new RuntimeException("Multiple children in parentheses");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "(" + mChild.toString() + ")" ;
     }
 }
