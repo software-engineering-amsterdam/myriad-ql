172575d13ac2f307d59642f30904b470a6d94e11
jetsekoopmans <jetsekoopmans@hotmail.com>
2017-02-21 17:02:07 +0100

    refactor transformer

--- koopmans/lib/parser/transformer.rb
+++ koopmans/lib/parser/transformer.rb
@@ -3,11 +3,6 @@ require 'require_all'
 
 require_rel '../ast'
 
-# tranformer for forms
-class Transformer < Parslet::Transform
-
-end
-
 # variable
 class Transformer < Parslet::Transform
   rule(variable: simple(:variable)) { Variable.new(variable) }
@@ -15,21 +10,13 @@ end
 
 # statement
 class Transformer < Parslet::Transform
-  # Type.descendants.each do |type|
-  #   rule(question: {string: simple(:string), variable: simple(:variable), type: type.type}) do
-  #     Question.new(string, Variable.new(variable), type.new)
-  #   end
-  #   rule(question: {string: simple(:string), variable: simple(:variable), type: type.type, expression: subtree(:expression)}) do
-  #     Question.new(string, Variable.new(variable), type.new, expression)
-  #   end
-  # end
-
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'boolean'}) { Question.new(string, Variable.new(variable), BooleanType.new) }
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'integer'}) { Question.new(string, Variable.new(variable), IntegerType.new) }
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'money'}) { Question.new(string, Variable.new(variable), MoneyType.new) }
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'string'}) { Question.new(string, Variable.new(variable), StringType.new) }
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'decimal'}) { Question.new(string, Variable.new(variable), DecimalType.new) }
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'date'}) { Question.new(string, Variable.new(variable), DateType.new) }
+
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'boolean', expression: subtree(:expression)}) { Question.new(string, Variable.new(variable), BooleanType.new, expression) }
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'integer', expression: subtree(:expression)}) { Question.new(string, Variable.new(variable), IntegerType.new, expression) }
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'money', expression: subtree(:expression)}) { Question.new(string, Variable.new(variable), MoneyType.new, expression) }
@@ -38,16 +25,10 @@ class Transformer < Parslet::Transform
   rule(question: {string: simple(:string), variable: simple(:variable), type: 'date', expression: subtree(:expression)}) { Question.new(string, Variable.new(variable), DateType.new, expression) }
 
   rule(if_statement: {expression: subtree(:expression), block: subtree(:block)}) { IfStatement.new(expression, block) }
-
 end
 
 # literal
 class Transformer < Parslet::Transform
-  # Literal.descendants.each do |literal|
-  #   rule("#{literal.type}": simple(:value)) do
-  #     literal.new(value)
-  #   end
-  # end
   rule(boolean: simple(:value)) { BooleanLiteral.new(value) }
   rule(integer: simple(:value)) { IntegerLiteral.new(value) }
   rule(string: simple(:value)) { StringLiteral.new(value) }
@@ -60,25 +41,12 @@ end
 
 # expression
 class Transformer < Parslet::Transform
-  rule(boolean_negation: simple(:boolean_negation), boolean: simple(:boolean)) { BooleanNegation.new(BooleanLiteral.new(boolean)) }
-  rule(integer_negation: simple(:integer_negation), integer: simple(:integer)) { IntegerNegation.new(IntegerLiteral.new(integer)) }
 
-  # Negation.descendants.each do |singleton_expression|
-  #   rule(negation: singleton_expression.to_operator, variable: simple(:variable)) do
-  #     singleton_expression.new(Variable.new(variable))
-  #   end
-  # end
+  # negation: ! -
   rule(negation: '!', variable: simple(:variable)) { BooleanNegation.new(Variable.new(variable)) }
   rule(negation: '-', variable: simple(:variable)) { IntegerNegation.new(Variable.new(variable)) }
-
-
-  # TODO fix this
-  # descendants = BooleanExpression.descendants + ArithmeticExpression.descendants + ComparisonEqual.descendants + ComparisonOrdering.descendants
-  # descendants.each do |binary_expression|
-  #   rule({left: subtree(:left), operator: binary_expression.to_operator, right: subtree(:right)}) do
-  #     binary_expression.new(left, right)
-  #   end
-  # end
+  rule(boolean_negation: simple(:boolean_negation), boolean: simple(:boolean)) { BooleanNegation.new(BooleanLiteral.new(boolean)) }
+  rule(integer_negation: simple(:integer_negation), integer: simple(:integer)) { IntegerNegation.new(IntegerLiteral.new(integer)) }
 
   # boolean: && ||
   rule({left: subtree(:left), operator: '&&', right: subtree(:right)}) { And.new(left, right) }
