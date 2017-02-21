fc779d3c47b814974448842bd5a80394f600f846
Mats Stijlaart <m.stijlaart@avisi.nl>
2017-02-21 12:25:14 +0100

    [elmos] Add QLS configuration parser

--- elmos/src/QLS/Parser/Configuration.elm
+++ elmos/src/QLS/Parser/Configuration.elm
@@ -1,9 +1,27 @@
 module QLS.Parser.Configuration exposing (..)
 
 import Combine exposing (..)
-import QLS.AST exposing (Configuration)
+import QLS.AST exposing (Configuration(SingleConfig, MultiConfig), ConfigItem(StyleConfig, WidgetConfig))
+import QLS.Parser.Style exposing (style)
+import QLS.Parser.Widget exposing (widget)
+import Combine.Extra exposing (whitespace1)
 
 
 configuration : Parser state Configuration
 configuration =
-    fail "TODO"
+    or
+        (SingleConfig <$> configItem)
+        (MultiConfig <$> configItemBlock)
+
+
+configItemBlock : Parser state (List ConfigItem)
+configItemBlock =
+    braces (sepBy whitespace1 configItem)
+
+
+configItem : Parser state ConfigItem
+configItem =
+    choice
+        [ StyleConfig <$> style
+        , WidgetConfig <$> (string "widget" *> whitespace1 *> widget)
+        ]
