aba4672cb7b860266065727d9425f8b38dacdbdf
Jurgen Baas <jurgen.baas@outlook.com>
2017-02-23 11:21:37 +0100

    Progress in gui updater

--- JurgenPeter/gui/formapp.py
+++ JurgenPeter/gui/formapp.py
@@ -1,14 +1,16 @@
 from appJar import gui
 from ql.visitors.questionfinder import QuestionFinder
-
+from gui.updategui import *
 
 class FormApp:
     LABELPREFIX = "label_"
     ENTRYPREFIX = "entry_"
 
     def __init__(self, ast, symboltable):
+        self.ast = ast
         self.environment = {name: None for name in symboltable}
         self.app = gui(ast.name)
+        self.app.bindKey("<KeyPress>", self.update_gui)
         questions = QuestionFinder().visit(ast)
 
         # TODO how to determine the widget type to be created
@@ -28,3 +30,12 @@ class FormApp:
 
     def create_entry_id(self, name):
         return self.ENTRYPREFIX + name
+
+    def update_gui(self, _):
+        UpdateGUI(self, self.environment).visit(self.ast)
+
+    def hide(self, name):
+        print("hide {}".format(name))
+
+    def show(self, name):
+        print("show {}".format(name))
--- JurgenPeter/gui/updategui.py
+++ JurgenPeter/gui/updategui.py
@@ -1,9 +1,11 @@
 from gui.formapp import *
+from ql.visitors.evaluator import *
 
 class UpdateGUI:
 
-    def __init__(self, app):
-        self.app = app
+    def __init__(self, form_app, environment):
+        self.form_app = form_app
+        self.evaluator = Evaluator(environment)
         self.stack = []
 
     def push(self, element):
@@ -13,7 +15,9 @@ class UpdateGUI:
         self.stack.pop()
 
     def read(self):
-        return self.stack[-1]
+        if self.stack:
+            return self.stack[-1]
+        return True
 
     def visit(self, node):
         node.accept(self)
@@ -23,39 +27,47 @@ class UpdateGUI:
             statement.accept(self)
 
     def visit_if_conditional(self, node):
-        # TODO: evaluate condition?
-        # self.push(node.condition) # TODO: Stack is not needed after all right?
-        if not node.condition:
-            return
+        print(self.read())
+        if self.read():
+            self.push(self.evaluator.visit(node.condition))
+        else:
+            self.push(False)
+
         for statement in node.ifbody:
             statement.accept(self)
-        # self.pop()
+
+        self.pop()
 
     def visit_ifelse_conditional(self, node):
-        # self.push(node.condition)
-        if node.condition:
-            for statement in node.ifbody:
-                statement.accept(self)
+        if self.read():
+            state = self.evaluator.visit(node.condition)
         else:
-            for statement in node.elsebody:
-                statement.accept(self)
-        # self.pop()
+            state = False
+
+        print(state)
+        self.push(state)
+
+        for statement in node.ifbody:
+            statement.accept(self)
+        self.pop()
+        self.push(not state)
+        for statement in node.elsebody:
+            statement.accept(self)
+        self.pop()
 
     def visit_question(self, node):
-        labeltype = 1
-        # TODO: some way to get correct widget type (see comment at the end)
-        questiontype = 2
-
-        self.app.showWidget(FormApp.create_label_id(node.name), labeltype)
-        self.app.showWidget(FormApp.create_entry_id(node.name), questiontype)
-
+        if self.read():
+            self.form_app.show(node.name)
+        else:
+            self.form_app.hide(node.name)
 
     def visit_computed_question(self, node):
-        # Not sure what to do with computed question
-        # Just calculate the outcome and nothing more right?
-        pass
+        if self.read():
+            self.form_app.show(node.name)
+        else:
+            self.form_app.hide(node.name)
 
-# appJar widgets:
+# form_appJar widgets:
 # WINDOW = 0
 # LABEL = 1
 # ENTRY = 2
