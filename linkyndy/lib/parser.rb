3d49a03754c17f25439030bf4e7bbc9a1c9e5949
Andrei Horak <linkyndy@gmail.com>
2017-02-25 16:26:12 +0100

    Annonate grammar

--- linkyndy/lib/parser.rb
+++ linkyndy/lib/parser.rb
@@ -10,34 +10,35 @@ class Parser < Parslet::Parser
   rule(:qmark) { str('?') >> space? }
   rule(:hashrocket) { space? >> str('=>') >> space? }
 
-  rule(:string) { quote >> match['^"'].repeat >> quote }
-  rule(:integer) { match['0-9'].repeat(1) >> space? }
-  rule(:boolean) { (str('true') | str('false')) >> space? }
+  rule(:string) { quote >> match['^"'].repeat.as(:string) >> quote }
+  rule(:integer) { match['0-9'].repeat(1).as(:integer) >> space? }
+  rule(:boolean) { (str('true') | str('false')).as(:boolean) >> space? }
   rule(:literal) { string | integer | boolean }
-  rule(:operator) { match['*/+-'] >> space? }
+  rule(:operator) { match['*/+-'].as(:operator) >> space? }
 
   rule(:comment) { str('#') >> any.repeat >> space? }
 
-  rule(:type) { (str('text') | str('bool') | str('number') | str('money')) >> space? }
+  rule(:type) { (str('text') | str('bool') | str('number') | str('money')).as(:type) >> space? }
 
-  rule(:identifier) { match['a-zA-Z'] >> match['a-zA-z0-9'].repeat >> space? }
+  rule(:identifier) { (match['a-zA-Z'] >> match['a-zA-z0-9'].repeat).as(:identifier) >> space? }
+  # TODO: Fix left recursion and allow nested expressions on left
   rule(:expression) do
-    lparen >> expression >> rparen |
+    lparen >> expression.as(:expression) >> rparen |
     # infix_expression(literal,
     #   [match['*/'], 2, :left],
     #   [match['+-'], 1, :left]
     # ) |
-    literal >> operator >> expression |
+    (literal | identifier).as(:left) >> operator >> expression.as(:right) |
     identifier |
     literal
   end
-  rule(:block) { (if_statement | question).repeat }
+  rule(:block) { (if_statement | question).repeat.as(:block) }
 
-  rule(:if_statement) { str('if') >> space >> expression >> block >> (str('else') >> space >> block).maybe >> str('end') >> space? }
+  rule(:if_statement) { (str('if') >> space >> expression >> block >> (str('else') >> space >> block).maybe >> str('end')).as(:if_statement) >> space? }
 
-  rule(:question) { string >> type >> identifier >> (hashrocket >> expression).maybe }
+  rule(:question) { (string >> type >> identifier >> (hashrocket >> expression).maybe).as(:question) }
 
-  rule(:form) { str('form') >> space >> identifier >> block >> str('end') }
+  rule(:form) { (str('form') >> space >> identifier >> block >> str('end')).as(:form) >> space? }
 
   root :form
 end
