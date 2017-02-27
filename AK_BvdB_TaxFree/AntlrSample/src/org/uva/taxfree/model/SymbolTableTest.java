cd6051e3a3d073a7cfa77677b267cf832121747b
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-02-27 13:32:16 +0100

    Simple test for SymbolTable

---
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/SymbolTableTest.java
@@ -0,0 +1,26 @@
+package org.uva.taxfree.model;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SymbolTableTest {
+    SymbolTable mSymbolTable;
+
+    @BeforeMethod
+    public void setUp() {
+        mSymbolTable = new SymbolTable();
+    }
+
+    @Test
+    public void testEvaluate() throws Exception {
+        BooleanQuestion boolQuestion = new BooleanQuestion("did you sell a house?", "hasSoldHouse");
+        mSymbolTable.addSymbol("hasSoldhouse", boolQuestion);
+        Assert.assertEquals("false", mSymbolTable.resolve("hasSoldHouse"));
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testUnknownVariable() throws Exception {
+        mSymbolTable.resolve("giveMeARuntimeException!");
+    }
+}
