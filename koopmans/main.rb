f87ac6722f335db6829d290706b40adff293100d
jetsekoopmans <jetsekoopmans@hotmail.com>
2017-02-27 15:34:06 +0100

    QLS parser

--- koopmans/main.rb
+++ koopmans/main.rb
@@ -12,8 +12,10 @@ parsed = QL::Parser::Parser.new.parse(contents)
 ast = QL::Parser::Transformer.new.apply(parsed)
 type_checker = QL::TypeChecker::TypeChecker.check(ast)
 pp type_checker
-QL::GUI::GUI.new(ast, type_checker)
+# QL::GUI::GUI.new(ast, type_checker)
 
 
-# contents = File.read('examples/example.qls')
-# p contents
+contents = File.read('examples/example.qls')
+p contents
+parsed = QLSParser.new.parse(contents)
+pp parsed
