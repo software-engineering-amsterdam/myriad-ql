4f7ddfd2ac0277b7107942b777d3508ab8698d63
Alex van Manen <alexvanmanen@hotmail.com>
2017-02-27 14:06:58 +0100

    initial version of QL memory

---
+++ manuelalex/src/memory/MemoryElement.js
@@ -0,0 +1,13 @@
+/**
+ * Created by alexvanmanen on 27-02-17.
+ */
+
+
+export class MemoryElement {
+
+    constructor(options = {}) {
+        this.name = options.name;
+        this.type = options.type;
+    }
+
+}
