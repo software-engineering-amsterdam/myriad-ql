d5b53c6cd7797b37f850026685f767a196c30cdf
Jeffrey Panneman <jpan@live.nl>
2017-02-27 16:04:02 +0100

    -Added identifier checking -Added test to verify duplicate fields are not allowed

--- pql/pql/identifierchecker/identifierchecker.py
+++ pql/pql/identifierchecker/identifierchecker.py
@@ -3,18 +3,14 @@ from collections import defaultdict
 
 
 class IdentifierChecker(object):
-    def __init__(self):
-        self.identifier_map = {}
-        self.messages = []
-
     def visit(self, pql_ast):
-        self.messages = []
-        self.identifier_map = {}
-        [form.apply(self) for form in pql_ast]
-        return self.__compute_result(self.identifier_map)
+        result = [form.apply(self) for form in pql_ast]
+        identifier_dict = self.__flatten_list_of_dictionaries(result)
+        del result
+        return self.__compute_result(identifier_dict)
 
     def form(self, node):
-        return self.__flatten_dictionaries([statement.apply(self) for statement in node.children])
+        return [statement.apply(self) for statement in node.children]
 
     def conditional(self, node):
         pass
@@ -22,17 +18,19 @@ class IdentifierChecker(object):
     def field(self, node):
         return {node.name.name: node.data_type}
 
-    def __flatten_dictionaries(self, dicts):
-        # TODO: Eventueel inner for in map
+    def __flatten_list_of_dictionaries(self, list_of_dicts):
+        # TODO: Mooier maken evt?
         res = defaultdict(list)
-        for d in dicts:
-            for k, v in d.items():
-                res[k].append(v)
+        for l in list_of_dicts:
+            for d in l:
+                for k, v in d.items():
+                    res[k].append(v)
         return res
-        # return {key: value for dic_entry in dicts for key, value in dic_entry.items()}
 
-    def __compute_result(self, dictionaries):
-        return dictionaries, self.__compute_errors(dictionaries)
-
-    def __compute_errors(self, dictionaries):
-        pass
+    def __compute_result(self, dictionary):
+        # TODO: Mooier maken evt?
+        errors = []
+        for key, value in dictionary.items():
+            if len(value) > 1:
+                errors.append('Key: {} contained multiple entries, the following: {}'.format(key, value))
+        return dictionary, errors
