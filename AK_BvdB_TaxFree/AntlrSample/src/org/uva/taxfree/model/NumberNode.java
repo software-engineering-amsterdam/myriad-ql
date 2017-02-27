8e6e5d667cc92594e05dcd6cb757ae3cef67fd42
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-02-27 14:21:32 +0100

    Re-Removed unused nodes

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/NumberNode.java
+++
@@ -1,8 +0,0 @@
-package org.uva.taxfree.model;
-
-public abstract class NumberNode extends NamedNode {
-    public NumberNode(String description, String id) {
-        super(description, id);
-    }
-
-}
