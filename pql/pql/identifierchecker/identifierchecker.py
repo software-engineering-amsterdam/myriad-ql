908761f14505ee35e4ebcb959c8e8ffe5b9cc580
Jeffrey Panneman <jpan@live.nl>
2017-02-27 17:56:19 +0100

    -Removed approach of dynamic dictionary and moved it to instance variable

--- pql/pql/identifierchecker/identifierchecker.py
+++ pql/pql/identifierchecker/identifierchecker.py
@@ -3,29 +3,24 @@ from collections import defaultdict
 
 
 class IdentifierChecker(object):
+    def __init__(self):
+        # TODO: Dictionary niet als instance variable
+        self.identifier_dict = defaultdict(list)
+
     def visit(self, pql_ast):
-        result = [form.apply(self) for form in pql_ast]
-        identifier_dict = self.__flatten_list_of_dictionaries(result)
-        del result
-        return self.__compute_result(identifier_dict)
+        [form.apply(self) for form in pql_ast]
+        return self.__compute_result(self.identifier_dict)
 
     def form(self, node):
-        return [statement.apply(self) for statement in node.children]
+        [statement.apply(self) for statement in node.children]
 
     def conditional(self, node):
-        pass
+        [statement.apply(self) for statement in node.statements]
+        if node.else_statement_list is not None:
+            [statement.apply(self) for statement in node.else_statement_list]
 
     def field(self, node):
-        return {node.name.name: node.data_type}
-
-    def __flatten_list_of_dictionaries(self, list_of_dicts):
-        # TODO: Mooier maken evt?
-        res = defaultdict(list)
-        for l in list_of_dicts:
-            for d in l:
-                for k, v in d.items():
-                    res[k].append(v)
-        return res
+        self.identifier_dict[node.name.name].append(node.data_type)
 
     def __compute_result(self, dictionary):
         # TODO: Mooier maken evt?
