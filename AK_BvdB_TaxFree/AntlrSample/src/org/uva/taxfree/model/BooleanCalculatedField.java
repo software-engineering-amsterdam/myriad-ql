82d59227346fd22a7e70d3e95bf75aa2bf6c6ca5
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-02-27 12:14:30 +0100

    Added BooleanCalculatedField and renamed IntegerField to IntegerCalculatedField

---
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/BooleanCalculatedField.java
@@ -0,0 +1,7 @@
+package org.uva.taxfree.model;
+
+public class BooleanCalculatedField extends CalculatedField {
+    public BooleanCalculatedField(String label, String id) {
+        super(label, id);
+    }
+}
---
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/IntegerCalculatedField.java
@@ -0,0 +1,10 @@
+package org.uva.taxfree.model;
+
+public class IntegerCalculatedField extends CalculatedField {
+    public IntegerCalculatedField(String label, String id) {
+        super(label, id);
+    }
+}
+
+
+
