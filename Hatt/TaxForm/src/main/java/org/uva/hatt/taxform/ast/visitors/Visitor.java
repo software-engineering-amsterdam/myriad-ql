d62ff7a0030e9bf2718fb72239af3431992a059d
Thanus <tthanusijan@gmail.com>
2017-02-27 13:56:47 +0100

    add visit on our ast

--- Hatt/TaxForm/src/main/java/org/uva/hatt/taxform/ast/visitors/Visitor.java
+++ Hatt/TaxForm/src/main/java/org/uva/hatt/taxform/ast/visitors/Visitor.java
@@ -1,6 +1,9 @@
 package org.uva.hatt.taxform.ast.visitors;
 
 import org.uva.hatt.taxform.ast.nodes.*;
+import org.uva.hatt.taxform.ast.nodes.expressions.BooleanExpression;
+import org.uva.hatt.taxform.ast.nodes.expressions.ComputationExpression;
+import org.uva.hatt.taxform.ast.nodes.expressions.GroupedExpression;
 import org.uva.hatt.taxform.ast.nodes.items.*;
 import org.uva.hatt.taxform.ast.nodes.types.*;
 import org.uva.hatt.taxform.ast.nodes.types.Boolean;
@@ -8,13 +11,16 @@ import org.uva.hatt.taxform.ast.nodes.types.Integer;
 import org.uva.hatt.taxform.ast.nodes.types.String;
 
 public interface Visitor {
-    public Form visit(Form node);
-    public FormId visit(FormId node);
-    public Question visit(Question node);
-    public Conditional visit(Conditional node);
-    public Boolean visit(Boolean node);
-    public Integer visit(Integer node);
-    public Money visit(Money node);
-    public String visit(String node);
-    public ValueType visit(ValueType node);
+    Form visit(Form node);
+    FormId visit(FormId node);
+    Question visit(Question node);
+    Conditional visit(Conditional node);
+    Boolean visit(Boolean node);
+    Integer visit(Integer node);
+    Money visit(Money node);
+    String visit(String node);
+    ValueType visit(ValueType node);
+    BooleanExpression visit(BooleanExpression node);
+    ComputationExpression visit(ComputationExpression node);
+    GroupedExpression visit(GroupedExpression node);
 }
