76b4b36b9db8825944bfa60725b4349ad01e4a6f
Jasper Dijt <jasper@jasperdijt.eu>
2017-02-27 13:37:35 +0100

    Beheer :: Bugfixes after bugs found by random tests.

--- Beheer/src/main/scala/parser/ExpressionParser.scala
+++ Beheer/src/main/scala/parser/ExpressionParser.scala
@@ -11,18 +11,20 @@ trait ExpressionParser extends JavaTokenParsers {
   type InfixMatcher = (ExpressionNode, ~[String, ExpressionNode]) => ExpressionNode
 
   //We do one date: yyyy-mm-dd.
-  private val dateFormat = new SimpleDateFormat("yyyy-mm-dd")
+  private val dateFormat = new SimpleDateFormat("""yyyy-mm-dd""")
 
-  def expression: Parser[ExpressionNode] = infixOperationParser(comp, """\&\&|\|\|""".r) {
+  def expression: Parser[ExpressionNode] = logic
+
+  private def logic: Parser[ExpressionNode] = infixOperationParser(comp, """&&|\|\|""".r) {
     case (lhs, "&&" ~ rhs) => AND(lhs, rhs)
     case (lhs, "||" ~ rhs) => OR(lhs, rhs)
   }
 
-  private def comp: Parser[ExpressionNode] = infixOperationParser(subAdd, """>|<|>=|<=|!=|==""".r) {
-    case (lhs, ">" ~ rhs) => GT(lhs, rhs)
-    case (lhs, "<" ~ rhs) => LT(lhs, rhs)
+  private def comp: Parser[ExpressionNode] = infixOperationParser(subAdd, """>=|<=|>|<|\!=|==""".r) {
     case (lhs, ">=" ~ rhs) => GEQ(lhs, rhs)
     case (lhs, "<=" ~ rhs) => LEQ(lhs, rhs)
+    case (lhs, ">" ~ rhs) => GT(lhs, rhs)
+    case (lhs, "<" ~ rhs) => LT(lhs, rhs)
     case (lhs, "!=" ~ rhs) => NEQ(lhs, rhs)
     case (lhs, "==" ~ rhs) => EQ(lhs, rhs)
   }
@@ -37,12 +39,7 @@ trait ExpressionParser extends JavaTokenParsers {
     case (lhs, "/" ~ rhs) => DIV(lhs, rhs)
   }
 
-  private def factor: Parser[ExpressionNode] = (
-    prefix
-    | (bool | integer | decimal | money | date | string)
-    | identifier
-    | "(" ~> expression <~ ")"
-  )
+  private def factor: Parser[ExpressionNode] = literal | prefix | "(" ~> expression <~ ")"
 
   private def prefix: Parser[ExpressionNode] =
     """-|!""".r ~ factor ^^ {
@@ -52,6 +49,8 @@ trait ExpressionParser extends JavaTokenParsers {
       }
     }
 
+  private def literal: Parser[ExpressionNode] = numeric | bool | date | string | identifier
+
   //Ident taken from JavaTokenParsers, equals Java Identifier.
   private def identifier: Parser[Identifier] = ident ^^ (s => Identifier(s))
 
@@ -59,13 +58,16 @@ trait ExpressionParser extends JavaTokenParsers {
 
   private def bool: Parser[BooleanLiteral] = ("True" | "False") ^^ (x => BooleanLiteral(x.toBoolean))
 
+  private def date: Parser[DateLiteral] = "D" ~> """\d\d\d\d-\d\d-\d\d""".r ^^ (x => DateLiteral(dateFormat.parse(x)))
+
+  //Note: ORDER MATTERS HERE.
+  private def numeric: Parser[ExpressionNode] = money | decimal | integer
+
   private def money: Parser[MoneyLiteral] = "€" ~> decimal ^^ (x => MoneyLiteral(x.value))
 
-  private def decimal: Parser[DecimalLiteral] = """\d*.\d+""".r ^^ (x => DecimalLiteral(BigDecimal(x)))
+  private def decimal: Parser[DecimalLiteral] = """\d*\.\d+""".r ^^ (x => DecimalLiteral(BigDecimal(x)))
 
   private def integer: Parser[IntegerLiteral] = """\d+""".r ^^ (x => IntegerLiteral(BigDecimal(x).setScale(0)))
-
-  private def date: Parser[DateLiteral] = """\d\d\d\d-\d\d-\d\d""".r ^^ (x => DateLiteral(dateFormat.parse(x)))
 
   private def infixOperationParser(child: Parser[ExpressionNode], operators: Regex)(f: InfixMatcher): Parser[ExpressionNode] = {
     child ~ rep(operators ~ child) ^^ { case head ~ tail => tail.foldLeft(head)(f) }
