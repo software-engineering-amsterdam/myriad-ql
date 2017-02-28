90c5087b10bec5237b89eea99fbd745bca5040a1
Jasper Dijt <jasper@jasperdijt.eu>
2017-02-28 16:45:20 +0100

    Beheer :: Cleaner type checking.

--- Beheer/src/main/scala/checker/ExpressionChecker.scala
+++ Beheer/src/main/scala/checker/ExpressionChecker.scala
@@ -15,7 +15,6 @@ class ExpressionChecker(db: FormModel, expression: ExpressionNode, expectedType:
   }
 
   def checkExpression(expressionNode: ExpressionNode): (Option[Type], Issues) = expressionNode match {
-    case identifier: Identifier => identifierType(identifier)
     case _: DecimalLiteral => (Some(DecimalType), Nil)
     case _: IntegerLiteral => (Some(IntegerType), Nil)
     case _: BooleanLiteral => (Some(BooleanType), Nil)
@@ -23,6 +22,7 @@ class ExpressionChecker(db: FormModel, expression: ExpressionNode, expectedType:
     case _: MoneyLiteral => (Some(MoneyType), Nil)
     case _: StringLiteral => (Some(StringType), Nil)
 
+    case identifier: Identifier => identifierType(identifier)
     case prefixNode: PrefixNode => checkPrefixExpression(prefixNode)
     case infixNode: InfixNode => checkInfixExpression(infixNode)
   }
@@ -32,33 +32,45 @@ class ExpressionChecker(db: FormModel, expression: ExpressionNode, expectedType:
     val (right, errorsRight) = checkExpression(infixNode.rhs)
     val errors = errorsLeft ++ errorsRight
 
-    (infixNode, left, right) match {
-      case (_, None, _) => (None, errors)
-      case (_, _, None) => (None, errors)
-      case (_: Arithmetic, Some(l: NumericType), Some(r: NumericType)) => (Some(mostGeneric(l, r)), errors)
-      case (_: Logic, Some(BooleanType), Some(BooleanType)) => (Some(BooleanType), errors)
-      case (_: Comparison, Some(_: NumericType), Some(_: NumericType)) => (Some(BooleanType), errors)
-      case (_: Comparison, Some(l: Type), Some(r: Type)) if l == r => (Some(BooleanType), errors)
-      case (_: Relation, Some(_: NumericType), Some(_: NumericType)) => (Some(BooleanType), errors)
-      case (o: Arithmetic, Some(l: Type), Some(r: Type)) => (None, errors :+ Error(s"Invalid types for operator $o: $l, $r"))
-      case (o: Logic, Some(l: Type), Some(r: Type)) => (Some(BooleanType), errors :+ Error(s"Invalid types for operator $o: $l, $r"))
-      case (o: Comparison, Some(l: Type), Some(r: Type)) => (Some(BooleanType), errors :+ Error(s"Invalid types for operator $o: $l, $r"))
-      case (o: Relation, Some(l: Type), Some(r: Type)) => (Some(BooleanType), errors :+ Error(s"Invalid types for operator $o: $l, $r"))
+    (left, right) match {
+      case (_, None) => (None, errors)
+      case (None, _) => (None, errors)
+      case (Some(leftType), Some(rightType)) => (infixNode, leftType, rightType) match {
+        case (_: Add, l: NumericType, r: NumericType) => (Some(mostGeneric(l, r)), errors)
+        case (_: Sub, l: NumericType, r: NumericType) => (Some(mostGeneric(l, r)), errors)
+        case (_: Mul, l: NumericType, r: NumericType) => (Some(mostGeneric(l, r)), errors)
+        case (_: Div, l: NumericType, r: NumericType) => (Some(mostGeneric(l, r)), errors)
+        case (_: Neq, _: NumericType, _: NumericType) => (Some(BooleanType), errors)
+        case (_: Geq, _: NumericType, _: NumericType) => (Some(BooleanType), errors)
+        case (_: Leq, _: NumericType, _: NumericType) => (Some(BooleanType), errors)
+        case (_: Eq, _: NumericType, _: NumericType) => (Some(BooleanType), errors)
+        case (_: Gt, _: NumericType, _: NumericType) => (Some(BooleanType), errors)
+        case (_: Lt, _: NumericType, _: NumericType) => (Some(BooleanType), errors)
+        case (_: And, BooleanType, BooleanType) => (Some(BooleanType), errors)
+        case (_: Or, BooleanType, BooleanType) => (Some(BooleanType), errors)
+        case (n: InfixNode, l: Type, r: Type) => emitError(n, l, r, errors)
+      }
     }
   }
 
-  private def checkPrefixExpression(prefixNode: PrefixNode): (Option[Type], Issues) =
-    (prefixNode, checkExpression(prefixNode.operand)) match {
-      case (_, (None, errors)) => (None, errors)
-      case (_: Neg, (Some(t: NumericType), errors)) => (Some(t), errors)
-      case (_: Not, (Some(BooleanType), errors)) => (Some(BooleanType), errors)
-      case (_: Neg, (Some(t: Type), errors)) => (None, errors :+ Error(s"Wrong operand type $t for operator NEG"))
-      case (_: Not, (Some(t: Type), errors)) => (Some(BooleanType), errors :+ Error(s"Wrong operand type $t for operator NOT"))
+  private def checkPrefixExpression(prefixNode: PrefixNode): (Option[Type], Issues) = {
+    val (operandType, errors) = checkExpression(prefixNode.operand)
+    operandType match {
+      case None => (None, errors)
+      case Some(opType) => (prefixNode, opType) match {
+        case (_: Neg, t: NumericType) => (Some(t), errors)
+        case (_: Not, BooleanType) => (Some(BooleanType), errors)
+        case (n: PrefixNode, t: Type) => emitError(n, t, errors)
+      }
     }
+  }
 
-  private def isSubType(candidate: NumericType, expectedType: NumericType): Boolean =
-    mostGeneric(candidate, expectedType) == expectedType
-
+  private def identifierType(identifier: Identifier): (Option[Type], Issues) = {
+    db.identifiersWithType.filter { case (i, _) => identifier.value == i } match {
+      case (_, identifierType) :: Nil => (Some(identifierType), Nil)
+      case _ => (None, Seq(Error(s"Duplicate identifier found in expression check: ${identifier.value}")))
+    }
+  }
   private def mostGeneric(left: NumericType, right: NumericType): NumericType =
     (left, right) match {
       case (MoneyType, _) => MoneyType
@@ -68,12 +80,15 @@ class ExpressionChecker(db: FormModel, expression: ExpressionNode, expectedType:
       case _ => IntegerType
     }
 
-  private def identifierType(identifier: Identifier): (Option[Type], Issues) = {
-    db.identifiersWithType.filter { case (i, _) => identifier.value == i } match {
-      case (_, identifierType) :: Nil => (Some(identifierType), Nil)
-      case _ => (None, Seq(Error(s"Duplicate identifier found in expression check: ${identifier.value}")))
-    }
-  }
+  private def isSubType(candidate: NumericType, expectedType: NumericType): Boolean =
+    mostGeneric(candidate, expectedType) == expectedType
+
+  private def emitError(expressionNode: ExpressionNode, left: Type, right: Type, errors: Issues) =
+    (None, Error(s"Types $left, $right not supported by operation ${expressionNode.getClass.getName}") +: errors)
+
+  private def emitError(expressionNode: ExpressionNode, operand: Type, errors: Issues) =
+    (None, Error(s"Type $operand not supported by operation ${expressionNode.getClass.getName}") +: errors)
+
 }
 
 object ExpressionChecker {
