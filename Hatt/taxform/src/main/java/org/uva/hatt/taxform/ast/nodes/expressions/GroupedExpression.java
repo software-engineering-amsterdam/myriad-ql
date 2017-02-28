dc6f3422174c77d8755af9af51367b4bd50ad5b0
Thanus <tthanusijan@gmail.com>
2017-02-28 14:05:33 +0100

    grouped expressions evaluates his own expression
    * grouped expression has no left or right expression, so that's why the override on his own expression

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/GroupedExpression.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/GroupedExpression.java
@@ -4,8 +4,23 @@ import org.uva.hatt.taxform.ast.visitors.Visitor;
 
 public class GroupedExpression extends Expression{
 
+    private Expression expression;
+
     public GroupedExpression(int lineNumber) {
         super(lineNumber);
+    }
+
+    public Expression getExpression() {
+        return expression;
+    }
+
+    public void setExpression(Expression expression) {
+        this.expression = expression;
+    }
+
+    @Override
+    public String evaluateExpression() {
+        return expression.evaluateExpression();
     }
 
     @Override
