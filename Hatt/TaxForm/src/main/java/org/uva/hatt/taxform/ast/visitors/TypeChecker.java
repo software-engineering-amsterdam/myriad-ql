98400a0ba670707bde459d9293070fc0a3bd5ea4
Metchu <ahmednassir@hotmail.com>
2017-02-28 14:25:26 +0100

    added type checking for declarations and questions

--- Hatt/TaxForm/src/main/java/org/uva/hatt/taxform/ast/visitors/TypeChecker.java
+++ Hatt/TaxForm/src/main/java/org/uva/hatt/taxform/ast/visitors/TypeChecker.java
@@ -5,7 +5,12 @@ import org.uva.hatt.taxform.ast.nodes.Form;
 import org.uva.hatt.taxform.ast.nodes.FormId;
 import org.uva.hatt.taxform.ast.nodes.expressions.BooleanExpression;
 import org.uva.hatt.taxform.ast.nodes.expressions.ComputationExpression;
+import org.uva.hatt.taxform.ast.nodes.expressions.Expression;
 import org.uva.hatt.taxform.ast.nodes.expressions.GroupedExpression;
+import org.uva.hatt.taxform.ast.nodes.expressions.literals.BooleanLiteral;
+import org.uva.hatt.taxform.ast.nodes.expressions.literals.Identifier;
+import org.uva.hatt.taxform.ast.nodes.expressions.literals.IntegerLiteral;
+import org.uva.hatt.taxform.ast.nodes.expressions.literals.StringerLiteral;
 import org.uva.hatt.taxform.ast.nodes.items.Conditional;
 import org.uva.hatt.taxform.ast.nodes.items.Item;
 import org.uva.hatt.taxform.ast.nodes.items.Question;
@@ -15,13 +20,21 @@ import org.uva.hatt.taxform.ast.nodes.types.Integer;
 import org.uva.hatt.taxform.ast.nodes.types.String;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 
 public class TypeChecker implements Visitor{
 
-    private final Map<java.lang.String, ValueType> declarations = new HashMap<>();
+    private Map<java.lang.String, ValueType> declarations;
+    private List<java.lang.String> questions;
 
+    public TypeChecker()
+    {
+        declarations = new HashMap<>();
+        this.questions = new LinkedList<>();
+    }
     @Override
     public Form visit(Form node){
         for (Item item : node.getQuestions()) {
@@ -39,16 +52,24 @@ public class TypeChecker implements Visitor{
     @Override
     public Question visit(Question node){
         if (declarations.containsKey(node.getValue())) {
-            // to do: error question exists
-        } else {
-            declarations.put(node.getValue(), node.getType());
+            // to do: error -> Duplicate declaration
         }
+
+        if (questions.contains(node.getQuestion().toLowerCase())) {
+            // to do: warning -> duplicate question label
+        }
+
+        declarations.put(node.getValue(), node.getType());
+        questions.add(node.getQuestion().toLowerCase());
+
         return null;
     }
 
     @Override
     public Conditional visit(Conditional node){
 
+        node.getThenStatements().forEach(item -> item.accept(this));
+        node.getElseStatements().forEach(item -> item.accept(this));
         return null;
     }
 
@@ -89,6 +110,31 @@ public class TypeChecker implements Visitor{
 
     @Override
     public GroupedExpression visit(GroupedExpression node) {
+        return null;
+    }
+
+    @Override
+    public Identifier visit(Identifier identifier) {
+        return null;
+    }
+
+    @Override
+    public StringerLiteral visit(StringerLiteral stringerLiteral) {
+        return null;
+    }
+
+    @Override
+    public IntegerLiteral visit(IntegerLiteral integerLiteral) {
+        return null;
+    }
+
+    @Override
+    public BooleanLiteral visit(BooleanLiteral booleanLiteral) {
+        return null;
+    }
+
+    @Override
+    public Expression visit(Expression expression) {
         return null;
     }
 }
