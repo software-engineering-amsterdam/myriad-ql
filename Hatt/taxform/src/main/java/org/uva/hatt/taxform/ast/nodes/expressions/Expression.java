e41a2a6e28eeb164588a8d5ddee1040e6cf1c31d
Thanus <tthanusijan@gmail.com>
2017-02-28 13:59:28 +0100

    evaluate expressions with scriptengine

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/Expression.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/Expression.java
@@ -1,20 +1,59 @@
 package org.uva.hatt.taxform.ast.nodes.expressions;
 
 import org.uva.hatt.taxform.ast.nodes.ASTNode;
+import org.uva.hatt.taxform.ast.visitors.Visitor;
+import org.uva.hatt.taxform.utils.ScriptEngineUtils;
 
-public abstract class Expression extends ASTNode {
+import javax.script.ScriptException;
 
-    private String expression;
+public class Expression extends ASTNode{
+
+    private Expression left;
+    private Expression right;
+    private String operator;
 
     public Expression(int lineNumber) {
         super(lineNumber);
     }
 
-    public String getExpression() {
-        return expression;
+    public Expression getLeft() {
+        return left;
     }
 
-    public void setExpression(String expression) {
-        this.expression = expression;
+    public void setLeft(Expression left) {
+        this.left = left;
+    }
+
+    public Expression getRight() {
+        return right;
+    }
+
+    public void setRight(Expression right) {
+        this.right = right;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String evaluateExpression() {
+        return "(" + this.left.evaluateExpression() + operator + this.right.evaluateExpression() + ")";
+    }
+
+    public String evaluate() throws ScriptException {
+        return evaluate(evaluateExpression());
+    }
+
+    protected String evaluate(String exp) throws ScriptException {
+        return ScriptEngineUtils.evaluate(exp);
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
     }
 }
