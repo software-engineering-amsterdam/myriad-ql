2f3a9b9b52661c65ffe6ad5c9a93f10b3087797d
aijkoopmans <arendkoopmans@gmail.com>
2017-02-28 14:38:35 +0100

    clean up

--- koopmans/lib/ql/gui/widgets.rb
+++ koopmans/lib/ql/gui/widgets.rb
@@ -5,33 +5,29 @@ module QL
 
       def initialize(args)
         @question = args[:question]
-        p @question
       end
     end
 
     class RadioWidget < Widget
       def initialize(args)
         super
-        create_radio_button(args[:true_value], true)
-        create_radio_button(args[:false_value], false)
-      end
-
-      def create_radio_button(text, value)
         radio_button          = TkRadioButton.new(@question.frame).pack
         radio_button.variable = @question.variable
         radio_button.command  = proc { @question.gui.value_changed }
-        radio_button.text     = text
-        radio_button.value    = value
+        radio_button.text     = args[:true_value]
+        radio_button.value    = true
+
+        radio_button          = TkRadioButton.new(@question.frame).pack
+        radio_button.variable = @question.variable
+        radio_button.command  = proc { @question.gui.value_changed }
+        radio_button.text     = args[:false_value]
+        radio_button.value    = false
       end
     end
 
     class CheckboxWidget < Widget
       def initialize(args)
         super
-        create_checkbox_button
-      end
-
-      def create_checkbox_button
         check_button          = TkCheckButton.new(@question.frame).pack
         check_button.variable = @question.variable
         check_button.command  = proc { @question.gui.value_changed }
@@ -41,61 +37,48 @@ module QL
     class DropdownWidget < Widget
       def initialize(args)
         super
-        create_combobox(args[:true_value], args[:false_value])
-      end
-
-      def create_combobox(true_value, false_value)
         combobox        = Tk::Tile::Combobox.new(@question.frame).pack
-        combobox.values = [true_value, false_value]
+        combobox.values = [args[:true_value], args[:false_value]]
         combobox.bind('<ComboboxSelected>') do
-          @question.variable.value = true if combobox.value == true_value
-          @question.variable.value = false if combobox.value == false_value
+          @question.variable.value = true if combobox.value == args[:true_value]
+          @question.variable.value = false if combobox.value == args[:false_value]
           @question.gui.value_changed
         end
-        combobox.value = true_value
+        combobox.value = args[:true_value]
       end
     end
 
     class SpinboxWidget < Widget
       def initialize(args)
         super
-        create_spinbox
-      end
-
-      def create_spinbox
-        spinbox = TkSpinbox.new(@question.frame).pack
+        spinbox              = TkSpinbox.new(@question.frame).pack
         # system's min
-        spinbox.from = -(2**(0.size * 8 -2))
+        spinbox.from         = -(2**(0.size * 8 -2))
         # system's max
-        spinbox.to = (2**(0.size * 8 -2) -1)
-        spinbox.value = 0
+        spinbox.to           = (2**(0.size * 8 -2) -1)
+        spinbox.value        = 0
+        # spinbox.increment = 0.1
+        # spinbox.format = "%.2f"
         spinbox.textvariable = @question.variable
-        spinbox.command = proc { @question.gui.value_changed }
+        spinbox.command      = proc { @question.gui.value_changed }
       end
     end
 
     class SliderWidget < Widget
       def initialize(args)
         super
-        create_scale(args[:minimum], args[:maximum])
-      end
-      def create_scale(minimum, maximum)
         scale = TkScale.new(@question.frame).pack
         scale.orient 'horizontal'
-        scale.from = minimum
-        scale.to = maximum
+        scale.from     = args[:minimum]
+        scale.to       = args[:maximum]
         scale.variable = @question.variable
-        scale.command = proc { @question.gui.value_changed }
+        scale.command  = proc { @question.gui.value_changed }
       end
     end
 
     class TextWidget < Widget
       def initialize(args)
         super
-        create_entry
-      end
-
-      def create_entry
         entry              = TkEntry.new(@question.frame).pack
         entry.textvariable = @question.variable
         entry.bind('KeyRelease') do
