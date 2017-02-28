ee740e13c33bd3295c2031d451e76c533077059b
jetsekoopmans <jetsekoopmans@hotmail.com>
2017-02-28 13:03:11 +0100

    qls type checker #1

--- koopmans/lib/qls/type_checker/question_reference_checker.rb
+++ koopmans/lib/qls/type_checker/question_reference_checker.rb
@@ -3,15 +3,16 @@ module QLS
     class QuestionReferenceChecker
       include QL::Visitor
 
+      # gather all variables from all questions of the QL and from the QLS and check for errors
       def visit_stylesheet(stylesheet, form)
-        ql_variables = form.accept(QL::Visitor::QuestionVisitor.new).map(&:variable).map(&:name)
-        qls_variables = stylesheet.pages.map { |page| page.accept(self) }.flatten.compact
+        ql_variables            = form.accept(QL::Visitor::QuestionVisitor.new).map(&:variable).map(&:name)
+        qls_variables           = stylesheet.pages.map { |page| page.accept(self) }.flatten.compact
         duplicate_qls_variables = qls_variables.select { |variable| qls_variables.count(variable) > 1 }.uniq
 
         errors = []
-        errors.push((ql_variables - qls_variables).map{|error| "#{error} of the QL program is not placed by the QLS program."})
-        errors.push((qls_variables - ql_variables).map{|error| "#{error} is referenced to a question that is not in the QL program"})
-        errors.push((duplicate_qls_variables).map{|error| "#{error} is placed multiple times"})
+        errors.push((ql_variables - qls_variables).map { |error| "#{error} of the QL program is not placed by the QLS program." })
+        errors.push((qls_variables - ql_variables).map { |error| "#{error} is referenced to a question that is not in the QL program" })
+        errors.push((duplicate_qls_variables).map { |error| "#{error} is placed multiple times" })
       end
 
       def visit_page(page)
