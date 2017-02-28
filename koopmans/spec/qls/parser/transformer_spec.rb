9a87ad492bb727db548f096200de4a5c23beb74a
aijkoopmans <arendkoopmans@gmail.com>
2017-02-28 11:10:19 +0100

    qls transformer tests

--- koopmans/spec/qls/parser/transformer_spec.rb
+++ koopmans/spec/qls/parser/transformer_spec.rb
@@ -16,18 +16,51 @@ module QLS
             expect(transformer.apply(widget: 'spinbox')).to be_a(SpinboxWidget)
           end
         end
+
         context 'radio' do
           it 'transforms' do
             expect(transformer.apply(widget: {radio: {first_value: {string: 'Yes'}, second_value: {string: 'No'}}})).to be_a(RadioWidget)
           end
         end
       end
+
       describe 'properties' do
         context 'width' do
           it 'transforms' do
-            expect
-            rule(width: {integer: simple(:value)}) { Width.new(value) }
+            expect(transformer.apply(width: {integer: '400'})).to be_a(Width)
           end
+        end
+      end
+
+      describe 'default properties' do
+        context 'boolean default' do
+          it 'transforms' do
+            expect(transformer.apply(default: {type: 'boolean', properties: [{width: '400'}]})).to be_a (Default)
+          end
+        end
+      end
+
+      describe 'question' do
+        it 'transforms' do
+          expect(transformer.apply(question: {variable: 'hasSoldHouse', properties: [{width: '400'}]})).to be_a (Question)
+        end
+      end
+
+      describe 'section' do
+        it 'transforms' do
+          expect(transformer.apply(section: {string: 'Section name', block: {}})).to be_a (Section)
+        end
+      end
+
+      describe 'page' do
+        it 'transforms' do
+          expect(transformer.apply(page: {variable: 'Section name', block: {}})).to be_a (Page)
+        end
+      end
+
+      describe 'stylesheet' do
+        it 'transforms' do
+          expect(transformer.apply(stylesheet: {variable: 'Section name', pages: {}})).to be_a (StyleSheet)
         end
       end
     end
