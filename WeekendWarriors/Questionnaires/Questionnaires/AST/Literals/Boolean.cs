cab0c4ccc7ba8a3812cc45ccb6023cc1593d33d2
Floris Velleman <florisvelleman@hotmail.com>
2017-03-04 23:20:38 +0100

    Removed unneeded includes.

--- WeekendWarriors/Questionnaires/Questionnaires/AST/Literals/Boolean.cs
+++ WeekendWarriors/Questionnaires/Questionnaires/AST/Literals/Boolean.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Questionnaires.SemanticAnalysis.Messages;
-using System.Diagnostics;
 using Questionnaires.SemanticAnalysis;
 using Questionnaires.Value;
 
