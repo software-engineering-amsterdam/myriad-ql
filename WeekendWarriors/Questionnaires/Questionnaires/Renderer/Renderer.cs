52b44f1b5c3daf28654a87d83aa6d7520de67e93
Floris Velleman <florisvelleman@hotmail.com>
2017-03-05 13:27:37 +0100

    Moved widget creation to a factory.

--- WeekendWarriors/Questionnaires/Questionnaires/Renderer/Renderer.cs
+++ WeekendWarriors/Questionnaires/Questionnaires/Renderer/Renderer.cs
@@ -17,6 +17,7 @@ namespace Questionnaires.Renderer
         private StackPanel QuestionnaireStack = new StackPanel();
         private Dictionary<string, IQuestionWidget> Questions = new Dictionary<string, IQuestionWidget>();
         private VariableStore.VariableStore VariableStore;
+        private WidgetFactory WidgetFactory = new WidgetFactory();
 
         public Renderer(VariableStore.VariableStore variableStore)
         {
@@ -28,44 +29,11 @@ namespace Questionnaires.Renderer
             QuestionnaireWindow.Show();
         }
 
-        public void AddQuestion(BoolValue type, IQuestion question)
+        public void AddQuestion(IQuestion question)
         {
             // Render the question by adding it to the questionnaire stack
             var inputChangedDelegate = new InputChangedCallback(this.InputChanged);
-            var questionWidget = new BooleanQuestionWidget(question.Name);
-            questionWidget.SetLabel(question.Body);
-            questionWidget.SetOnInputChanged(inputChangedDelegate);
-            Questions.Add(question.Name, questionWidget);
-            QuestionnaireStack.Children.Add(questionWidget);
-        }
-
-        public void AddQuestion(DecimalValue type, IQuestion question)
-        {
-            // Render the question by adding it to the questionnaire stack
-            var inputChangedDelegate = new InputChangedCallback(this.InputChanged);
-            var questionWidget = new MoneyQuestionWidget(question.Name);
-            questionWidget.SetLabel(question.Body);
-            questionWidget.SetOnInputChanged(inputChangedDelegate);
-            Questions.Add(question.Name, questionWidget);
-            QuestionnaireStack.Children.Add(questionWidget);
-        }
-
-        public void AddQuestion(IntValue type, IQuestion question)
-        {
-            // Render the question by adding it to the questionnaire stack
-            var inputChangedDelegate = new InputChangedCallback(this.InputChanged);
-            var questionWidget = new NumberQuestionWidget(question.Name);
-            questionWidget.SetLabel(question.Body);
-            questionWidget.SetOnInputChanged(inputChangedDelegate);
-            Questions.Add(question.Name, questionWidget);
-            QuestionnaireStack.Children.Add(questionWidget);
-        }
-
-        public void AddQuestion(StringValue type, IQuestion question)
-        {
-            // Render the question by adding it to the questionnaire stack
-            var inputChangedDelegate = new InputChangedCallback(this.InputChanged);
-            var questionWidget = new StringQuestionWidget(question.Name);
+            var questionWidget = WidgetFactory.BuildWidget(question.Value, question.Name);
             questionWidget.SetLabel(question.Body);
             questionWidget.SetOnInputChanged(inputChangedDelegate);
             Questions.Add(question.Name, questionWidget);
