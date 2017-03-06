4013d97978b4bb59d20e1f47fac9e7ee638e0d7d
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-03-06 15:24:48 +0100

    added tests for sematics analyzer

--- AK_BvdB_TaxFree/AntlrSample/src/test/org/uva/taxfree/ast/SemanticsAnalyzerTest.java
+++ AK_BvdB_TaxFree/AntlrSample/src/test/org/uva/taxfree/ast/SemanticsAnalyzerTest.java
@@ -63,13 +63,24 @@ public class SemanticsAnalyzerTest {
     }
 
     @Test
+    void testCyclicDependencyCalculation() throws Exception {
+        assertSemantics("cyclicDependencyCalculation.txt", 1, "Cyclic dependency in calculation");
+    }
+
+    @Test
+    void testCyclicDependencyCalculations() throws Exception {
+        assertSemantics("cyclicDependencyCalculations.txt", 2, "Cyclic dependency in calculations");
+    }
+
+    @Test
     void testCyclicDependency() throws Exception {
-        assertSemantics("cyclicDependencyCalculations.txt", 1, "Cyclic dependency in calculation");
+        assertSemantics("invalidDelegatedCalculations.txt", 0, "Substitution of variables should not trigger errors");
     }
 
     private void assertSemantics(String fileName, int expectedErrorAmount, String description) throws IOException {
+        boolean expectedValid = 0 == expectedErrorAmount;
         SemanticsAnalyzer semanticsAnalyzer = createAnalyzer(fileName);
-        Assert.assertFalse(semanticsAnalyzer.validSemantics(), "Expecting errors: " + description);
+        Assert.assertEquals(semanticsAnalyzer.validSemantics(), expectedValid, "Expecting errors: " + description);
         Assert.assertEquals(semanticsAnalyzer.getSemanticErrors().size(), expectedErrorAmount, "Invalid error amount");
     }
 
@@ -79,7 +90,7 @@ public class SemanticsAnalyzerTest {
     }
 
     private File testFile(String fileName) {
-        return new File("src\\test\\org\\uva\\taxfree\\ast\\semanticErrors\\" + fileName);
+        return new File("src\\test\\org\\uva\\taxfree\\ast\\semanticsCheckerTestInput\\" + fileName);
     }
 
 }
