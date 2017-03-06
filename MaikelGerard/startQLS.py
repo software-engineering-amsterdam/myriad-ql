15fbf38128ac0d90af1ea221d6da5beb2661ab41
Maikel van der Panne <maikelpanne@outlook.com>
2017-03-06 11:05:04 +0100

    Added start of QLS: initial parser done

---
+++ MaikelGerard/startQLS.py
@@ -0,0 +1,39 @@
+from QLS.Stages.Parser import Parser
+
+if __name__ == '__main__':
+    example = """
+        stylesheet taxOfficeExample {
+            page Housing {
+                section "Buying" {
+                    question hasBoughtHouse widget checkbox
+                }
+                section "Loaning" {
+                    question hasMaintLoan
+                }
+            }
+            page Selling {
+                section "Selling" {
+                    question hasSoldHouse
+                        widget radio
+                    section "You sold a house" {
+                        question sellingPrice
+                            widget spinbox
+                        question privateDebt
+                            widget spinbox
+                        question valueResidue
+                        default money {
+                            width: true
+                            font: "Arial"
+                            fontsize: 14
+                            color: #999999
+                            widget spinbox
+                        }
+                    }
+                }
+                default boolean widget radio
+            }
+        }
+    """
+
+    client = Parser()
+    print client.parse(example)
