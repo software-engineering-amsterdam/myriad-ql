ddc763f10dbd913a47e269cc60401a89979bc3c8
Futsy <florisvelleman@hotmail.com>
2017-03-06 11:49:36 +0100

    resolved crap from merge.

--- WeekendWarriors/Questionnaires/Questionnaires/Renderer/Widgets/WidgetFactory.cs
+++ WeekendWarriors/Questionnaires/Questionnaires/Renderer/Widgets/WidgetFactory.cs
@@ -14,38 +14,22 @@ namespace Questionnaires.Renderer.Widgets
             return BuildWidget((dynamic)value, name);
         }
 
-<<<<<<< HEAD
-        private CheckBoxWidget BuildWidget(BoolValue question, string name)
-=======
-        private BooleanQuestionWidget BuildWidget(BooleanType question, string name)
->>>>>>> origin/WeekendWarriors
+        private CheckBoxWidget BuildWidget(BooleanType question, string name)
         {
             return new CheckBoxWidget(name);
         }
 
-<<<<<<< HEAD
-        private NumberPickerWidget BuildWidget(IntValue question, string name)
-=======
-        private NumberQuestionWidget BuildWidget(IntegerType question, string name)
->>>>>>> origin/WeekendWarriors
+        private NumberPickerWidget BuildWidget(IntegerType question, string name)
         {
             return new NumberPickerWidget(name);
         }
 
-<<<<<<< HEAD
-        private DecimalPickerWidget BuildWidget(DecimalValue question, string name)
-=======
-        private MoneyQuestionWidget BuildWidget(MoneyType question, string name)
->>>>>>> origin/WeekendWarriors
+        private DecimalPickerWidget BuildWidget(MoneyType question, string name)
         {
             return new DecimalPickerWidget(name);
         }
 
-<<<<<<< HEAD
-        private TextBoxWidget BuildWidget(StringValue question, string name)
-=======
-        private StringQuestionWidget BuildWidget(StringType question, string name)
->>>>>>> origin/WeekendWarriors
+        private TextBoxWidget BuildWidget(StringType question, string name)
         {
             return new TextBoxWidget(name);
         }
