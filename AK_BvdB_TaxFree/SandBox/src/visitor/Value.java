7dd463834df2f625938f1ab027ac2a5d99a824ad
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-03-06 12:16:16 +0100

    added specific type calls

--- AK_BvdB_TaxFree/SandBox/src/visitor/Value.java
+++ AK_BvdB_TaxFree/SandBox/src/visitor/Value.java
@@ -4,4 +4,7 @@ public abstract class Value {
     public void accept(VisitorBase v){
         v.visit(this);
     }
+
+    public abstract double asDouble();
+    public abstract int asIntgeger();
 }
