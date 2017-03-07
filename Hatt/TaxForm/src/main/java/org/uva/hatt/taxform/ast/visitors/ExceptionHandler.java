817bc878ab5f3f2342175ff97745b6f94f8f4016
Metchu <ahmednassir@hotmail.com>
2017-03-07 12:45:21 +0100

    Modified type

--- Hatt/TaxForm/src/main/java/org/uva/hatt/taxform/ast/visitors/ExceptionHandler.java
+++ Hatt/TaxForm/src/main/java/org/uva/hatt/taxform/ast/visitors/ExceptionHandler.java
@@ -1,11 +1,11 @@
 package org.uva.hatt.taxform.ast.visitors;
 
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExceptionHandler {
-    private List<Error> errors = new LinkedList<>();
+    private List<Error> errors = new ArrayList<>();
 
     public void addError(Error error) {
         errors.add(error);
