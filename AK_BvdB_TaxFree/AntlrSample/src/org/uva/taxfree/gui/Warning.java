0b07286e19bd21f7cc7958c7e88d184a25015649
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-03-07 11:41:33 +0100

    Added MessageDialog to display errors

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/gui/Warning.java
+++
@@ -1,9 +0,0 @@
-package org.uva.taxfree.gui;
-
-import java.awt.*;
-
-public class Warning extends Message {
-    public Warning(String message) {
-        super(message, Color.YELLOW);
-    }
-}
--- AK_BvdB_TaxFree/AntlrSample/src/test/org/uva/taxfree/ast/MessageWindowTest.java
+++ AK_BvdB_TaxFree/AntlrSample/src/test/org/uva/taxfree/ast/MessageWindowTest.java
@@ -1,5 +1,28 @@
 package test.org.uva.taxfree.ast;
 
-public class MessageWindowTest {
+import org.testng.annotations.Test;
+import org.uva.taxfree.gui.ErrorMessage;
+import org.uva.taxfree.gui.Message;
+import org.uva.taxfree.gui.MessageWindow;
+import org.uva.taxfree.gui.WarningMessage;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageWindowTest {
+    @Test
+    public void testComposedDialog() throws Exception {
+        List<Message> errors = new ArrayList<>();
+        errors.add(new ErrorMessage("Big error!"));
+        errors.add(new WarningMessage("Small warning!"));
+        MessageWindow.showMessageDialog(errors);
+    }
+
+    @Test
+    public void testWarningDialog() throws Exception {
+        List<Message> warnings = new ArrayList<>();
+        warnings.add(new WarningMessage("This is a warning"));
+        warnings.add(new WarningMessage("This is another warning"));
+        MessageWindow.showMessageDialog(warnings);
+    }
 }
