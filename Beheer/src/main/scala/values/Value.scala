043e67261137ff6f8cf95545efe5e29cb458c9f1
Willem Jan Glerum <willem@wjglerum.nl>
2017-03-07 13:29:08 +0100

    Beheer :: remove operations from value hierarchy

--- Beheer/src/main/scala/values/Value.scala
+++ Beheer/src/main/scala/values/Value.scala
@@ -1,37 +1,44 @@
 package values
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import scala.math.BigDecimal.RoundingMode
+
 trait Value {
-  def ==(other: Value): Value = BooleanValue(this.equals(other))
-
-  def !=(other: Value): Value = BooleanValue(!this.equals(other))
-
-  def +(other: Value): Value = UndefinedValue
-
-  def -(other: Value): Value = UndefinedValue
-
-  def *(other: Value): Value = UndefinedValue
-
-  def /(other: Value): Value = UndefinedValue
-
-  def >(other: Value): Value = UndefinedValue
-
-  def <(other: Value): Value = UndefinedValue
-
-  def >=(other: Value): Value = UndefinedValue
-
-  def <=(other: Value): Value = UndefinedValue
-
-  def &&(other: Value): Value = UndefinedValue
-
-  def ||(other: Value): Value = UndefinedValue
-
-  def ! = Not
-
-  private def Not: Value = UndefinedValue
-
-  def - = Neg
-
-  private def Neg: Value = UndefinedValue
-
   def display: String
+}
+
+case class BooleanValue(value: Boolean) extends Value {
+  override def display = value.toString
+}
+
+case class DateValue(value: Date) extends Value {
+  private val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+
+  override def display = dateFormat.format(value)
+}
+
+sealed trait NumericValue extends Value {
+  val value: BigDecimal
+}
+
+case class IntegerValue(value: BigDecimal) extends NumericValue {
+  override def display = value.toString
+}
+
+case class DecimalValue(value: BigDecimal) extends NumericValue {
+  override def display = value.toString
+}
+
+case class MoneyValue(value: BigDecimal) extends NumericValue {
+  override def display = s"€${value.setScale(2, RoundingMode.HALF_EVEN)}"
+}
+
+case class StringValue(value: String) extends Value {
+  override def display = value
+}
+
+case object UndefinedValue extends Value {
+  override def display = "Value not defined"
 }
