aec34159d1fae005a0d1d2625f2de38f013163ae
aijkoopmans <arendkoopmans@gmail.com>
2017-03-07 14:05:49 +0100

    parser with fully working expression

--- koopmans/examples/simple_questionnaire.ql
+++ koopmans/examples/simple_questionnaire.ql
@@ -1,1 +1,19 @@
-4||2&&10>(2==4)+5
+form taxOfficeExample {
+  "Did you sell a house in 2010?"
+    hasSoldHouse: boolean
+  "Did you buy a house in 2010?"
+    hasBoughtHouse: boolean
+  "Did you enter a loan?"
+    hasMaintLoan: boolean
+
+  if (hasSoldHouse) {
+    "What was the selling price?"
+      sellingPrice: money
+    "Private debts for the sold house:"
+      privateDebt: money
+    "Value residue:"
+      valueResidue: money =
+        (5*5)
+  }
+
+}
--- koopmans/lib/ql/parser/parser.rb
+++ koopmans/lib/ql/parser/parser.rb
@@ -6,83 +6,48 @@ module QL
     class Parser < Parslet::Parser
       root(:form)
 
-      root :boolean
-
-      # rule(:root) {
-      #   ((exp | addition)).repeat
-      # }
-
-      rule(:exp) {
-        str('(') >> boolean >> str(')')
-      }
-
-      rule(:boolean) {
-        comparison_order.as(:left) >> (boolean_op >> comparison_order.as(:right)).repeat(1) |
-          comparison_order
-      }
-
-      rule(:comparison_order) {
-        comparison_equals.as(:left) >> (comparison_order_op >> comparison_equals.as(:right)).repeat(1) |
-          comparison_equals
-      }
-
-      rule(:comparison_equals) {
-        addition.as(:left) >> (comparison_equals_op >> addition.as(:right)).repeat(1) |
-          addition
-      }
-
-      rule(:addition) {
-        multiplication.as(:left) >> (add_op >> multiplication.as(:right)).repeat(1) |
-          multiplication
-      }
-
-      rule(:multiplication) {
-        i_or_exp.as(:left) >> (mult_op >> i_or_exp.as(:right)).repeat(1) |
-          i_or_exp }
-
-
-      rule(:i_or_exp) {
-        (integer_literal | exp)
-      }
-
-      rule(:mult_op) { (str('*') | str('/')).as(:operator) >> _ }
-      rule(:add_op) { (str('+') | str('-')).as(:operator) >> _ }
-      rule(:comparison_equals_op) { (str('==') | str('!=')).as(:operator) >> _ }
-      rule(:comparison_order_op) { (str('<=') | str('>=') | str('<') | str('>')).as(:operator) >> _ }
-      rule(:boolean_op) { (str('&&') | str('||')).as(:operator) >> _ }
-
-
-
-      # spaces, breaks
+      # spaces, breaks, tabs
       rule(:_) { match('\s').repeat(1).maybe }
 
-      # expression
-      rule(:negation?) { (str('!') | str('-')).as(:negation).maybe }
-      rule(:variable_or_literal) { negation? >> (boolean_literal | integer_literal | string_literal | variable) >> _ }
-      rule(:calculation) { variable_or_literal.as(:left) >> operator >> expression.as(:right) }
-      rule(:operator) { (str('-') | str('+') | str('*') | str('/') | str('<=') | str('>=') | str('==') | str('!=') | str('<') | str('>') | str('&&') | str('||') | str('!')).as(:operator) >> _ }
-      rule(:expression) { str('(') >> _ >> expression.as(:expression) >> _ >> str(')') >> _ | calculation | variable_or_literal }
-
-      # form
-      rule(:form) { _ >> (str('form') >> _ >> variable.as(:id) >> _ >> str('{') >> body >> str('}')).as(:form) }
-
-      # literal
+      # literals
       rule(:boolean_literal) { (str('true') | str('false')).as(:boolean_literal) >> _ }
       rule(:integer_literal) { match('[0-9]').repeat(1).as(:integer_literal) >> _ }
       rule(:string_literal) { str('"') >> match('[^"]').repeat.as(:string_literal) >> str('"') >> _ }
 
-      # statement
-      rule(:assignment?) { (str('=') >> _ >> expression).maybe >> _ }
-      rule(:question) { (string_literal.as(:label) >> variable_assignment >> type.as(:type) >> assignment?).as(:question) >> _ }
-      rule(:body) { _ >> (question | if_statement).repeat.as(:body) }
-      rule(:if_statement) { (str('if') >> _ >> expression >> str('{') >> body >> str('}')).as(:if_statement) >> _ }
-
-      # type
-      rule(:type) { (str('boolean') | str('integer') | str('integer') | str('decimal') | str('date') | str('money')).as(:type) >> _ }
-
-      # variable.as
+      # variable
       rule(:variable) { match('\w+').repeat(1).as(:variable) }
       rule(:variable_assignment) { variable.as(:id) >> str(':') >> _ }
+
+      # types
+      rule(:type) { (str('boolean') | str('integer') | str('integer') | str('decimal') | str('date') | str('money')).as(:type) >> _ }
+
+      # operators
+      rule(:negation?) { (str('!') | str('-')).as(:negation).maybe }
+      rule(:multiplication_operator) { (str('*') | str('/')).as(:operator) >> _ }
+      rule(:addition_operator) { (str('+') | str('-')).as(:operator) >> _ }
+      rule(:comparison_equals_operator) { (str('==') | str('!=')).as(:operator) >> _ }
+      rule(:comparison_order_operator) { (str('<=') | str('>=') | str('<') | str('>')).as(:operator) >> _ }
+      rule(:boolean_operator) { (str('&&') | str('||')).as(:operator) >> _ }
+
+      # expression
+      # precedence oder: booolean, order, equals, addition, multiplication, literal
+      rule(:expression) { boolean_expression }
+      rule(:expression_with_parenthesis) { str('(') >> boolean_expression >> str(')') }
+      rule(:boolean_expression) { comparison_order_expression.as(:left) >> (boolean_operator >> comparison_order_expression.as(:right)).repeat(1) | comparison_order_expression }
+      rule(:comparison_order_expression) { comparison_equals_expression.as(:left) >> (comparison_order_operator >> comparison_equals_expression.as(:right)).repeat(1) | comparison_equals_expression }
+      rule(:comparison_equals_expression) { addition_expression.as(:left) >> (comparison_equals_operator >> addition_expression.as(:right)).repeat(1) | addition_expression }
+      rule(:addition_expression) { multiplication_expression.as(:left) >> (addition_operator >> multiplication_expression.as(:right)).repeat(1) | multiplication_expression }
+      rule(:multiplication_expression) { literal_or_variable_or_expression.as(:left) >> (multiplication_operator >> literal_or_variable_or_expression.as(:right)).repeat(1) | literal_or_variable_or_expression }
+      rule(:literal_or_variable_or_expression) { negation? >> (boolean_literal | integer_literal | string_literal | variable | expression_with_parenthesis) >> _ }
+
+      # statement
+      rule(:assignment?) { (str('=') >> _ >> expression.as(:assignment)).maybe >> _ }
+      rule(:question) { (string_literal.as(:label) >> variable_assignment >> type.as(:type) >> assignment?).as(:question) >> _ }
+      rule(:body) { _ >> (question | if_statement).repeat.as(:body) }
+      rule(:if_statement) { (str('if') >> _ >> expression.as(:condition) >> str('{') >> body >> str('}')).as(:if_statement) >> _ }
+
+      # form
+      rule(:form) { _ >> (str('form') >> _ >> variable.as(:id) >> _ >> str('{') >> body >> str('}')).as(:form) }
     end
   end
 end
