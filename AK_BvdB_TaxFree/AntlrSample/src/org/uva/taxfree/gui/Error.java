eafffaca8979a44eb5e0af4661566be6c7e458cc
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-03-07 11:09:52 +0100

    Fixed merge issues overwriting our files

---
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/gui/Error.java
@@ -0,0 +1,9 @@
+package org.uva.taxfree.gui;
+
+import java.awt.*;
+
+public class Error extends Message {
+    public Error(String message) {
+        super(message, Color.RED);
+    }
+}
---
+++ AK_BvdB_TaxFree/AntlrSample/src/test/org/uva/taxfree/ast/MessageWindowTest.java
@@ -0,0 +1,5 @@
+package test.org.uva.taxfree.ast;
+
+public class MessageWindowTest {
+
+}
