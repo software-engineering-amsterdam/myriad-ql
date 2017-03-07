1134606d1545868036aeecb0debd1cc55ab53a0f
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-03-07 14:29:01 +0100

    Message popups / errors

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/main/Main.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/main/Main.java
@@ -1,9 +1,7 @@
 package org.uva.taxfree.main;//package main;
 
 import org.uva.taxfree.ast.Ast;
-import org.uva.taxfree.gui.ErrorMessage;
-import org.uva.taxfree.gui.FileSelector;
-import org.uva.taxfree.gui.MessageWindow;
+import org.uva.taxfree.gui.*;
 import org.uva.taxfree.model.environment.Environment;
 
 import java.io.File;
@@ -12,9 +10,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("** Starting our parser **");
-        System.out.println("- Parsing the input");
-
         File inputFile = FileSelector.select();
         if (!inputFile.exists()) {
             if (MessageWindow.retryDialog(new ErrorMessage("No file selected...\nRetry?"))) {
@@ -24,7 +19,16 @@ public class Main {
         }
         Environment environment = Ast.generateAst(inputFile);
         SemanticsAnalyzer semanticsAnalyzer = new SemanticsAnalyzer(environment);
-        semanticsAnalyzer.validSemantics();
+        if (semanticsAnalyzer.hasMessages()) {
+            MessageWindow.showMessages(semanticsAnalyzer.getSemanticErrors());
+        }
+        if (semanticsAnalyzer.validSemantics()) {
+            QuestionForm taxForm = new QuestionForm(environment);
+            taxForm.show();
+            new FormRenderer(taxForm);
+            return;
+        }
+
     }
 
 }
