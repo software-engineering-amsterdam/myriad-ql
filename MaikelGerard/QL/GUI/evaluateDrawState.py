5028f9e552c2d3ab1ddb8586679904d0f1e3f4f0
Gerard Schröder <gerard.schroder@live.com>
2017-03-07 12:27:11 +0100

    Renamed show_state --> widget_visible in evaluateDrawState

--- MaikelGerard/QL/GUI/evaluateDrawState.py
+++ MaikelGerard/QL/GUI/evaluateDrawState.py
@@ -19,22 +19,22 @@ class EvaluateDrawState(object):
     def start_traversal(self):
         self.ast.accept(self, True)
 
-    def if_node(self, if_node, show_state):
+    def if_node(self, if_node, widget_visible):
         condition = if_node.condition.accept(self.evaluator)
         condition = condition if condition != Undefined else False
 
-        if_node.if_block.accept(self, condition and show_state)
+        if_node.if_block.accept(self, condition and widget_visible)
 
-    def if_else_node(self, if_else_node, show_state):
+    def if_else_node(self, if_else_node, widget_visible):
         condition = if_else_node.condition.accept(self.evaluator)
         condition = condition if condition != Undefined else False
 
-        if_else_node.if_block.accept(self, show_state and condition)
-        if_else_node.else_block.accept(self, show_state and not condition)
+        if_else_node.if_block.accept(self, widget_visible and condition)
+        if_else_node.else_block.accept(self, widget_visible and not condition)
 
-    def question_node(self, question_node, show_state):
+    def question_node(self, question_node, widget_visible):
         identifier = question_node.name
-        if not show_state:
+        if not widget_visible:
             self.form_gui.hide_widget(identifier)
         else:
             value = self.env.get_var_value(identifier)
@@ -43,9 +43,9 @@ class EvaluateDrawState(object):
             self.form_gui.show_widget(identifier)
             self.form_gui.set_widget_val(identifier, value)
 
-    def comp_question_node(self, comp_question_node, show_state):
+    def comp_question_node(self, comp_question_node, widget_visible):
         identifier = comp_question_node.name
-        if not show_state:
+        if not widget_visible:
             self.form_gui.hide_widget(identifier)
         else:
             value = self.env.get_var_value(identifier)
