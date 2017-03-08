990769cd55f3eec797210d13c971329efad57e4e
Andrei Horak <linkyndy@gmail.com>
2017-03-08 19:24:30 +0100

    Transform literals to their corresponding AST node

--- linkyndy/lib/transform.rb
+++ linkyndy/lib/transform.rb
@@ -2,9 +2,9 @@ require 'parslet'
 require_relative 'ast'
 
 class Transform < Parslet::Transform
-  rule(string: simple(:x)) { Ast::String.new(x) }
-  rule(integer: simple(:x)) { Ast::Integer.new(x) }
-  rule(boolean: simple(:x)) { Ast::Boolean.new(x) }
+  rule(string: simple(:x)) { Ast::StringLiteral.new(x) }
+  rule(integer: simple(:x)) { Ast::IntegerLiteral.new(x) }
+  rule(boolean: simple(:x)) { Ast::BooleanLiteral.new(x) }
 
   rule(identifier: simple(:identifier)) { Ast::Identifier.new(identifier) }
 
@@ -13,8 +13,8 @@ class Transform < Parslet::Transform
   rule(left: subtree(:left), operator: '+', right: subtree(:right)) { Ast::Addition.new(left, right) }
   rule(left: subtree(:left), operator: '-', right: subtree(:right)) { Ast::Subtraction.new(left, right) }
 
-  rule(question: { string: simple(:string), type: simple(:type), identifier: simple(:identifier) }) { Ast::Question.new(Ast::String.new(string), Ast::Type.new(type), Ast::Identifier.new(identifier)) }
-  rule(question: { string: simple(:string), type: simple(:type), identifier: simple(:identifier), value: subtree(:value) }) { Ast::Question.new(Ast::String.new(string), Ast::Type.new(type), Ast::Identifier.new(identifier), value) }
+  rule(question: { string: simple(:string), type: simple(:type), identifier: simple(:identifier) }) { Ast::Question.new(string, Ast::Type.new(type), Ast::Identifier.new(identifier)) }
+  rule(question: { string: simple(:string), type: simple(:type), identifier: simple(:identifier), value: subtree(:value) }) { Ast::Question.new(string, Ast::Type.new(type), Ast::Identifier.new(identifier), value) }
 
   rule(if_statement: { condition: subtree(:condition), if_true: subtree(:if_true) }) { Ast::IfStatement.new(condition, if_true) }
   rule(if_statement: { condition: subtree(:condition), if_true: subtree(:if_true), if_false: subtree(:if_false) }) { Ast::IfStatement.new(condition, if_true, if_false) }
