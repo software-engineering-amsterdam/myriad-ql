cc2f0c377513587856629c92a50329e6dc517f94
Andrei Horak <linkyndy@gmail.com>
2017-03-08 17:15:59 +0100

    Fix left recursion for expressions and allow nesting them

--- linkyndy/lib/parser.rb
+++ linkyndy/lib/parser.rb
@@ -14,24 +14,22 @@ class Parser < Parslet::Parser
   rule(:integer) { match['0-9'].repeat(1).as(:integer) >> space? }
   rule(:boolean) { (str('true') | str('false')).as(:boolean) >> space? }
   rule(:literal) { string | integer | boolean }
-  rule(:operator) { match['*/+-'].as(:operator) >> space? }
 
   rule(:comment) { str('#') >> any.repeat >> space? }
 
   rule(:type) { (str('text') | str('bool') | str('number') | str('money')).as(:type) >> space? }
 
   rule(:identifier) { (match['a-zA-Z'] >> match['a-zA-z0-9'].repeat).as(:identifier) >> space? }
-  # TODO: Fix left recursion and allow nested expressions on left
-  rule(:expression) do
-    lparen >> expression >> rparen |
-    # infix_expression(literal,
-    #   [match['*/'], 2, :left],
-    #   [match['+-'], 1, :left]
-    # ) |
-    ((literal | identifier).as(:left) >> operator >> expression.as(:right)) |
-    literal |
-    identifier
-  end
+
+  rule(:addition) { str('+').as(:operator) >> space? }
+  rule(:subtraction) { str('-').as(:operator) >> space? }
+  rule(:multiplication) { str('*').as(:operator) >> space? }
+  rule(:division) { str('/').as(:operator) >> space? }
+
+  rule(:expression) { term.as(:left) >> (addition | subtraction) >> expression.as(:right) | term }
+  rule(:term) { factor.as(:left) >> (multiplication | division) >> term.as(:right) | factor }
+  rule(:factor) { lparen >> expression >> rparen | identifier | literal }
+
   rule(:block) { (if_statement | question).repeat }
 
   rule(:if_statement) { (str('if') >> space >> expression.as(:condition) >> block.as(:if_true) >> (str('else') >> space >> block.as(:if_false)).maybe >> str('end')).as(:if_statement) >> space? }
