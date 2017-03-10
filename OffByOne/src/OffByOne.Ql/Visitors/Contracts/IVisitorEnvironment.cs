3057a6ede75c65dce23be4334e6fdfefded400f6
Teodor92 <teodor.ivanov92@gmail.com>
2017-03-10 21:37:23 +0100

    added missing file

---
+++ OffByOne/src/OffByOne.Ql/Visitors/Contracts/IVisitorEnvironment.cs
@@ -0,0 +1,11 @@
+﻿namespace OffByOne.Ql.Visitors.Contracts
+{
+    using OffByOne.Ql.Ast.ValueTypes.Base;
+
+    public interface IVisitorEnvironment : IEnvironment
+    {
+        void AddSymbol(string name, ValueType type);
+
+        ValueType GetTypeOf(string name);
+    }
+}
---
+++ OffByOne/src/OffByOne.Qls/Checker/FormCheckerEnvironment.cs
@@ -0,0 +1,25 @@
+﻿namespace OffByOne.Qls.Checker
+{
+    using System.Collections.Generic;
+
+    using OffByOne.Ql.Visitors.Contracts;
+
+    public class FormCheckerEnvironment : IEnvironment
+    {
+        public FormCheckerEnvironment()
+        {
+            this.StyleSheetNames = new HashSet<string>();
+            this.PageLabels = new HashSet<string>();
+            this.SectionNames = new HashSet<string>();
+            this.QuestionLabels = new HashSet<string>();
+        }
+
+        public ISet<string> StyleSheetNames { get; set; }
+
+        public ISet<string> PageLabels { get; }
+
+        public ISet<string> SectionNames { get; }
+
+        public ISet<string> QuestionLabels { get; }
+    }
+}
