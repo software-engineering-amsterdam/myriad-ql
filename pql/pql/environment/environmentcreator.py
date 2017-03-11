c43edd1f7fb46a96a2936e21677b25ede9346947
Jeffrey Panneman <jpan@live.nl>
2017-03-11 16:02:17 +0100

    -Creator now also does not have instance variable anymore -Added tests for asserting default variable states of environment

--- pql/pql/environment/environmentcreator.py
+++ pql/pql/environment/environmentcreator.py
@@ -1,19 +1,19 @@
 # coding=utf-8
-from collections import defaultdict
-
 from pql.identifierchecker.identifierchecker import IdentifierChecker
-from pql.traversal.FormVisitor import FormVisitor
 
 
 class EnvironmentCreator(IdentifierChecker):
-    def __init__(self):
-        super(EnvironmentCreator, self).__init__()
-        self.__environment_dict = dict()
-
     def visit(self, pql_ast):
-        self.__environment_dict.clear()
-        [form.apply(self) for form in pql_ast]
-        return self.__environment_dict
+        def recursively_build_dictionary(items, dct):
+            for dictionary in items:
+                if isinstance(dictionary, list):
+                    recursively_build_dictionary(dictionary, dct)
+                else:
+                    for d_key, d_value in dictionary.items():
+                        dct[d_key] = d_value
+        flattened_dict = dict()
+        recursively_build_dictionary([form.apply(self) for form in pql_ast], flattened_dict)
+        return flattened_dict
 
     def field(self, node):
-        self.__environment_dict[node.name.name] = (node.data_type.default_value())
+        return {node.name.name: node.data_type.default_value()}
