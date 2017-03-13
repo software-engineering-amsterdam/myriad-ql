5cb38b68f6c7b282c7c4d715c4251b30d6e7669f
aijkoopmans <arendkoopmans@gmail.com>
2017-03-13 13:24:09 +0100

    evaluator spec

--- koopmans/spec/ql/gui/evaluator_spec.rb
+++ koopmans/spec/ql/gui/evaluator_spec.rb
@@ -12,46 +12,54 @@ module QL
       let(:form_parser) { FormParser.new }
       let(:form_transformer) { FormTransformer.new }
 
-      describe 'expressions' do
-        context 'general' do
-          it 'parses' do
-            expression_1 = form_transformer.apply(form_parser.expression.parse('5 + -10 > 0'))
-            expression_2 = form_transformer.apply(form_parser.expression.parse('(5 + -10 > 0) == false'))
-
-            expect(expression_1.accept(evaluator).to_value).to eq(false)
-            expect(expression_2.accept(evaluator).to_value).to eq(true)
-          end
+      context 'general' do
+        it 'evaluates' do
+          expression_1 = form_transformer.apply(form_parser.expression.parse('5 + -10 > 0'))
+          expect(expression_1.accept(evaluator).to_value).to eq(false)
+          expression_2 = form_transformer.apply(form_parser.expression.parse('(5 + -10 > 0) == false'))
+          expect(expression_2.accept(evaluator).to_value).to eq(true)
         end
+      end
 
-        context 'arithmetic' do
-          it 'parses' do
-            expect(form_transformer.apply(form_parser.expression.parse('(5 + 10 * 5)')).eval.value).to eq('55')
-            expect(form_transformer.apply(form_parser.expression.parse('(10 / 2 - 4 * 10)')).eval.value).to eq('-35')
-            expect(form_transformer.apply(form_parser.expression.parse('(10 / (2 - 4) * 10)')).eval.value).to eq('-50')
-          end
+      context 'arithmetic' do
+        it 'evaluates' do
+          expression_1 = form_transformer.apply(form_parser.expression.parse('(5 + 10 * 5)'))
+          expect(expression_1.accept(evaluator).to_value).to eq(55)
+          expression_2 = form_transformer.apply(form_parser.expression.parse('(10 / 2 - 4 * 10)'))
+          expect(expression_2.accept(evaluator).to_value).to eq(-35)
+          expression_3 = form_transformer.apply(form_parser.expression.parse('(10 / (2 - 4) * 10)'))
+          expect(expression_3.accept(evaluator).to_value).to eq(-50)
         end
+      end
 
-        context 'boolean' do
-          it 'parses' do
-            expect(form_transformer.apply(form_parser.expression.parse('(true && false)')).eval.value).to eq('false')
-            expect(form_transformer.apply(form_parser.expression.parse('false || false')).eval.value).to eq('false')
-            expect(form_transformer.apply(form_parser.expression.parse('true && (false || true)')).eval.value).to eq('true')
-          end
+      context 'boolean' do
+        it 'evaluates' do
+          expression_1 = form_transformer.apply(form_parser.expression.parse('(true && false)'))
+          expect(expression_1.accept(evaluator).to_value).to eq(false)
+          expression_2 = form_transformer.apply(form_parser.expression.parse('false || false'))
+          expect(expression_2.accept(evaluator).to_value).to eq(false)
+          expression_3 = form_transformer.apply(form_parser.expression.parse('true && (false || true)'))
+          expect(expression_3.accept(evaluator).to_value).to eq(true)
         end
+      end
 
-        context 'comparison' do
-          it 'parses' do
-            expect(form_transformer.apply(form_parser.expression.parse('(5 < 10)')).eval.value).to eq('true')
-            expect(form_transformer.apply(form_parser.expression.parse('(8 != 10)')).eval.value).to eq('true')
-            expect(form_transformer.apply(form_parser.expression.parse('(8 == 10)')).eval.value).to eq('false')
-          end
+      context 'comparison' do
+        it 'evaluates' do
+          expression_1 = form_transformer.apply(form_parser.expression.parse('(5 < 10)'))
+          expect(expression_1.accept(evaluator).to_value).to eq(true)
+          expression_2 = form_transformer.apply(form_parser.expression.parse('(8 != 10)'))
+          expect(expression_2.accept(evaluator).to_value).to eq(true)
+          expression_3 = form_transformer.apply(form_parser.expression.parse('(8 == 10)'))
+          expect(expression_3.accept(evaluator).to_value).to eq(false)
         end
+      end
 
-        context 'negations' do
-          it 'parses' do
-            expect(form_transformer.apply(form_parser.expression.parse('(!true)')).eval.value).to eq('false')
-            expect(form_transformer.apply(form_parser.expression.parse('(-5)')).eval.value).to eq('-5')
-          end
+      context 'negations' do
+        it 'evaluates' do
+          expression_1 = form_transformer.apply(form_parser.expression.parse('(!false)'))
+          expect(expression_1.accept(evaluator).to_value).to eq(true)
+          expression_2 = form_transformer.apply(form_parser.expression.parse('(-5)'))
+          expect(expression_2.accept(evaluator).to_value).to eq(-5)
         end
       end
     end
