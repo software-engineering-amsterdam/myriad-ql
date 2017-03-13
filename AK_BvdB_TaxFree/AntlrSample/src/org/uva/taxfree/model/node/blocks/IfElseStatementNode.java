4dd49dce62785f5f29b9207fecca2bc3793ee5cc
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-03-13 15:55:01 +0100

    added iterating over children

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/node/blocks/IfElseStatementNode.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/node/blocks/IfElseStatementNode.java
@@ -1,5 +1,8 @@
 package org.uva.taxfree.model.node.blocks;
 
+import org.uva.taxfree.gui.MessageList;
+import org.uva.taxfree.gui.QuestionForm;
+import org.uva.taxfree.model.environment.SymbolTable;
 import org.uva.taxfree.model.node.Node;
 import org.uva.taxfree.model.node.expression.ExpressionNode;
 
@@ -11,5 +14,29 @@ public class IfElseStatementNode extends IfStatementNode {
     public IfElseStatementNode(ExpressionNode expressionNode, List<Node> thenStatementNodes, List<Node> elseStatementNodes) {
         super(expressionNode, thenStatementNodes);
         mElseChildren = elseStatementNodes;
+    }
+
+    @Override
+    public void fillSymbolTable(SymbolTable symbolTable) {
+        super.fillSymbolTable(symbolTable);
+        for (Node n : mElseChildren) {
+            n.fillSymbolTable(symbolTable);
+        }
+    }
+
+    @Override
+    public void checkSemantics(SymbolTable symbolTable, MessageList semanticsMessages) {
+        super.checkSemantics(symbolTable, semanticsMessages);
+        for (Node n : mElseChildren) {
+            n.checkSemantics(symbolTable, semanticsMessages);
+        }
+    }
+
+    @Override
+    public void fillQuestionForm(QuestionForm form) {
+        super.fillQuestionForm(form);
+        for (Node n : mElseChildren) {
+            n.fillSymbolTable(form);
+        }
     }
 }
