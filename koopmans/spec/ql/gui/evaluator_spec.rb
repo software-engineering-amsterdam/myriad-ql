b2086dbbe674a7941a71e4be058d9b953fcf11ea
aijkoopmans <arendkoopmans@gmail.com>
2017-03-13 13:42:30 +0100

    evaluator spec

--- koopmans/spec/ql/gui/evaluator_spec.rb
+++ koopmans/spec/ql/gui/evaluator_spec.rb
@@ -6,59 +6,56 @@ require_all 'lib'
 
 module QL
   module GUI
+    include AST
     include Parser
     describe Evaluator do
       let(:evaluator) { Evaluator.new }
       let(:form_parser) { FormParser.new }
       let(:form_transformer) { FormTransformer.new }
 
-      context 'general' do
-        it 'evaluates' do
-          expression_1 = form_transformer.apply(form_parser.expression.parse('5 + -10 > 0'))
-          expect(expression_1.accept(evaluator).to_value).to eq(false)
-          expression_2 = form_transformer.apply(form_parser.expression.parse('(5 + -10 > 0) == false'))
-          expect(expression_2.accept(evaluator).to_value).to eq(true)
-        end
-      end
-
       context 'arithmetic' do
         it 'evaluates' do
-          expression_1 = form_transformer.apply(form_parser.expression.parse('(5 + 10 * 5)'))
-          expect(expression_1.accept(evaluator).to_value).to eq(55)
-          expression_2 = form_transformer.apply(form_parser.expression.parse('(10 / 2 - 4 * 10)'))
-          expect(expression_2.accept(evaluator).to_value).to eq(-35)
-          expression_3 = form_transformer.apply(form_parser.expression.parse('(10 / (2 - 4) * 10)'))
-          expect(expression_3.accept(evaluator).to_value).to eq(-50)
+          # 5 + 10 -> 15
+          expression_1 = Expression.new([IntegerLiteral.new(5), Add.new(IntegerLiteral.new(10))])
+          expect(expression_1.accept(evaluator).to_value).to eq(15)
+
+          # 5 * 10 -> 50
+          expression_2 = Expression.new([IntegerLiteral.new(5), Multiply.new(IntegerLiteral.new(10))])
+          expect(expression_2.accept(evaluator).to_value).to eq(50)
         end
       end
 
       context 'boolean' do
         it 'evaluates' do
-          expression_1 = form_transformer.apply(form_parser.expression.parse('(true && false)'))
+          # true && false -> false
+          expression_1 = Expression.new([BooleanLiteral.new(true), And.new(BooleanLiteral.new(false))])
           expect(expression_1.accept(evaluator).to_value).to eq(false)
-          expression_2 = form_transformer.apply(form_parser.expression.parse('false || false'))
-          expect(expression_2.accept(evaluator).to_value).to eq(false)
-          expression_3 = form_transformer.apply(form_parser.expression.parse('true && (false || true)'))
-          expect(expression_3.accept(evaluator).to_value).to eq(true)
+
+          # true || false -> true
+          expression_2 = Expression.new([BooleanLiteral.new(true), Or.new(BooleanLiteral.new(false))])
+          expect(expression_2.accept(evaluator).to_value).to eq(true)
         end
       end
 
       context 'comparison' do
         it 'evaluates' do
-          expression_1 = form_transformer.apply(form_parser.expression.parse('(5 < 10)'))
+          # 5 < 10 -> true
+          expression_1 = Expression.new([IntegerLiteral.new(5), Less.new(IntegerLiteral.new(10))])
           expect(expression_1.accept(evaluator).to_value).to eq(true)
-          expression_2 = form_transformer.apply(form_parser.expression.parse('(8 != 10)'))
+
+          # 5 != 10 -> true
+          expression_2 = Expression.new([IntegerLiteral.new(5), NotEqual.new(IntegerLiteral.new(10))])
           expect(expression_2.accept(evaluator).to_value).to eq(true)
-          expression_3 = form_transformer.apply(form_parser.expression.parse('(8 == 10)'))
-          expect(expression_3.accept(evaluator).to_value).to eq(false)
         end
       end
 
       context 'negations' do
         it 'evaluates' do
-          expression_1 = form_transformer.apply(form_parser.expression.parse('(!false)'))
+          # !false -> true
+          expression_1 = Expression.new(BooleanNegation.new(BooleanLiteral.new(false)))
           expect(expression_1.accept(evaluator).to_value).to eq(true)
-          expression_2 = form_transformer.apply(form_parser.expression.parse('(-5)'))
+
+          expression_2 = Expression.new(IntegerNegation.new(IntegerLiteral.new(5)))
           expect(expression_2.accept(evaluator).to_value).to eq(-5)
         end
       end
