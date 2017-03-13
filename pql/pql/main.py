fbebe4ed88fd51e96788b24601d35d19d7c8785f
Jeffrey Panneman <jpan@live.nl>
2017-03-13 17:24:32 +0100

    -Made notes, made TODOs out of notes

--- pql/pql/main.py
+++ pql/pql/main.py
@@ -19,25 +19,24 @@ if __name__ == '__main__':
 # Adrian TODO: Make all fields mandatory
 # Adrian TODO: Highlight betreffende error line
 
+# TODO: Value opsplitsen in aparte objecten voor de literals / value hernoemen naar literal
+# TODO: Field opsplitsen in assignment en question
+# TODO: List comprehension eventueel bekijken / andere manier/  refactoren
+# TODO: Loopen over ast voor form weghalen
+# TODO: Typechecker kijken naar ons check systeem en evt minder generic maken
+# TODO: Identifier_dict naar symbol_table
+
+# TODO: IdentifierChecker weer niet recursief maken
+# TODO: TypeChecker de TypeEnvironment meegeven
+# TODO: Environment een class maken en een update value methode geven
+# TODO: Unary operator rhs hernoemen naar lhs
+
+# TODO: Evaluator regel 92 de is None checks weghalen
+# TODO: Evaluator latere declaraties die worden niet meegeven ( in een loop evaluaten)
+# TODO: Identifier regel 39 check kan weg, identifier bestaat altijd
+# TODO: String support toevoegen
 
 
-
-# NOTES:
-# Value opsplitsen in aparte objecten voor de literals
-# Field opsplitsen in assignment en question
-# value hernoemen naar literal
-# List comprehension eventueel bekijken
-# Loopen over ast voor form weghalen
-# Eventueel If / IfElse logic combineren in typechecker
-# Typechecker kijken naar ons check systeem en evt minder generic maken
-# Identifier_dict naar symbol_table
-# ast.apply(typevisitor) ipv Typenvironment
-# IdentifierChecker weer niet recursief maken
-# TypeChecker de TypeEnvironment meegeven
-# Environment een class maken en een update value methode geven
-# Unary operator rhs hernoemen naar lhs
-# Evaluator regel 92 de is None checks weghalen
-# Evaluator latere declaraties die worden niet meegeven ( in een loop evaluaten)
-# Identifier regel 39 check kan weg, identifier bestaat altijd
-# String support toevoegen
-# Dynamic types in operators ipv python operators
+# TODO: LOW PRIO: Eventueel If / IfElse logic combineren in typechecker
+# TODO: LOWEST PRIO : ast.apply(typevisitor) ipv Typenvironment
+# TODO: NICE TO HAVE Dynamic types in operators ipv python operators
