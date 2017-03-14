4d0be906a5c682b262f21acf6e090f6cec1f5923
Alex Kok <alexkok08@gmail.com>
2017-03-14 17:09:37 +0100

    Fixed uniform expression

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/node/operators/UniformOperator.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/model/node/operators/UniformOperator.java
@@ -1,6 +1,6 @@
 package org.uva.taxfree.model.node.operators;
 
-import org.uva.taxfree.model.types.IntegerType;
+import org.uva.taxfree.model.types.BooleanType;
 import org.uva.taxfree.model.types.Type;
 
 public class UniformOperator extends Operator {
@@ -15,6 +15,6 @@ public class UniformOperator extends Operator {
 
     @Override
     public Type getType() {
-        return new IntegerType();
+        return new BooleanType();
     }
 }
