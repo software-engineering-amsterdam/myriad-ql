ff9540a50a371e24a5eb08bb90c4f8affef857cc
Adrian Zborowski <ak.zborowski@gmail.com>
2017-03-14 16:39:48 +0100

    + added dependency check + change elif into or

--- pql/tests/dependencies/test_dependencieschecker.py
+++ pql/tests/dependencies/test_dependencieschecker.py
@@ -79,3 +79,20 @@ class TestDependenciesChecker(Shared):
         form_node = self.acquire_ast(input_string)
         errors = self.acquire_circular_references(form_node)
         self.assertEqual(len(errors), 1, "There should be exactly 1 error")
+
+    def test_circular_inside_nested_if_2(self):
+        input_string = """
+        form taxOfficeExample {
+            if(hasBought){
+                "Did you buy a house in 2010?" hasCar: boolean
+                if(hasCar){
+                    "Did you buy a house in 2010?" hasBike: boolean
+                }
+                "Did you buy a house in 2010?" hasNewBike: boolean = hasBike
+            }
+            "Did you buy a house in 2010?" hasBought: boolean
+        }
+        """
+        form_node = self.acquire_ast(input_string)
+        errors = self.acquire_circular_references(form_node)
+        self.assertEqual(len(errors), 1, "There should be exactly 1 error")
