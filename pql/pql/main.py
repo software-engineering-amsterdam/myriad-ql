78aa2a388f848cdceb32640e4248941b5e95b5b7
Adrian Zborowski <ak.zborowski@gmail.com>
2017-03-14 15:18:06 +0100

    + completed task

--- pql/pql/main.py
+++ pql/pql/main.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
     exit(app.exec_())
 
 # TODO: Checken of alle properties die in een Expression zitten bestaan (circular reference ding)
-# TODO: Field opsplitsen in assignment en question / Evaluator regel 92 de is None checks weghalen
 # TODO: Evaluator latere declaraties die worden niet meegeven ( in een loop evaluaten tot environment niet veranderd is)
 # TODO: String support toevoegen
 # TODO: Typechecker kijken naar ons check systeem en evt minder generic maken
