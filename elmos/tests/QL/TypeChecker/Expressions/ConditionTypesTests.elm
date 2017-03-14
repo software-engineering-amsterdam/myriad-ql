cfde74a517e7b1da92be5f02d75091a741b3fff2
pacbeckh <pacbeckh@users.noreply.github.com>
2017-03-14 15:31:29 +0100

    [elmos] Added tests for ConditionTypeChecker

--- elmos/tests/QL/TypeChecker/Expressions/ConditionTypesTests.elm
+++ elmos/tests/QL/TypeChecker/Expressions/ConditionTypesTests.elm
@@ -6,13 +6,26 @@ import Dict
 import Expect
 import Test exposing (Test, describe, test)
 import QL.TypeChecker.Messages exposing (..)
-import QL.TypeChecker.Expressions.ComputedFieldTypes exposing (computedFieldTypeErrors)
-
-
--- TODO: Implement these
+import QL.TypeChecker.Expressions.ConditionTypes exposing (conditionTypeErrors)
 
 
 all : Test
 all =
     describe "ConditionTypes.conditionTypeErrors"
-        []
+        [ test "should detect condition with non-boolean type" <|
+            \() ->
+                conditionTypeErrors
+                    (Form ( "form", emptyLoc )
+                        [ IfThen (Str emptyLoc "test") [ Field "Label" ( "t", Location 1 1 ) BooleanType ] ]
+                    )
+                    (Dict.empty)
+                    |> Expect.equal [ Error (InvalidConditionType (Location 0 0) StringType) ]
+        , test "should detect condition with var value with non-boolean type" <|
+            \() ->
+                conditionTypeErrors
+                    (Form ( "form", emptyLoc )
+                        [ IfThen (Var ( "y", Location 0 0 )) [ Field "Label" ( "x", Location 1 1 ) BooleanType ] ]
+                    )
+                    (Dict.singleton "y" IntegerType)
+                    |> Expect.equal [ Error (InvalidConditionType (Location 0 0) IntegerType) ]
+        ]
