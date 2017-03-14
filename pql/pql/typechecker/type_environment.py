f8ad610feb5f96c67f7acbe98eec3dfaab1fc91d
Jeffrey Panneman <jpan@live.nl>
2017-03-14 10:49:15 +0100

    -Refactored TypeEnvironment/ EnvironmentCreator and IdentifierChecker

--- pql/pql/typechecker/type_environment.py
+++ pql/pql/typechecker/type_environment.py
@@ -1,10 +1,10 @@
 # coding=utf-8
 from collections import defaultdict
 
-from pql.identifierchecker.identifierchecker import IdentifierChecker
+from pql.traversal.FormVisitor import FormVisitor
 
 
-class TypeEnvironment(IdentifierChecker):
+class TypeEnvironment(FormVisitor):
     def __init__(self):
         super(TypeEnvironment, self).__init__()
         self.__symbol_table = defaultdict()
@@ -13,6 +13,19 @@ class TypeEnvironment(IdentifierChecker):
         self.__symbol_table.clear()
         [form.apply(self) for form in pql_ast]
         return self.__symbol_table
+
+    def form(self, node):
+        for statement in node.statements:
+            statement.apply(self)
+
+    def conditional_if_else(self, node):
+        self.conditional_if(node)
+        for statement in node.else_statement_list:
+            statement.apply(self)
+
+    def conditional_if(self, node):
+        for statement in node.statements:
+            statement.apply(self)
 
     def field(self, node):
         self.__symbol_table[node.name.name] = node.data_type
