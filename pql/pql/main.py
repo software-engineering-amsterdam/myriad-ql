f234ef7b29c70c23907984f0a1b57f8e6848d3db
Jeffrey Panneman <jpan@live.nl>
2017-03-14 10:39:45 +0100

    -Adjusted TODO priorities

--- pql/pql/main.py
+++ pql/pql/main.py
@@ -14,27 +14,23 @@ if __name__ == '__main__':
 
 # Jeffrey TODO: Checken of alle properties die in een Expression zitten bestaan (circular reference ding)
 # Adrian TODO: Line numbers tonen in error messages van TypeChecker
-
-# Adrian TODO: Export of values ?
-# Adrian TODO: Make all fields mandatory
-# Adrian TODO: Highlight betreffende error line
-
 # TODO: Value opsplitsen in aparte objecten voor de literals / value hernoemen naar literal
 # TODO: Field opsplitsen in assignment en question
+# TODO: String support toevoegen
 # TODO: List comprehension eventueel bekijken / andere manier/  refactoren
 # TODO: Loopen over ast voor form weghalen
 # TODO: Typechecker kijken naar ons check systeem en evt minder generic maken
 # TODO: Identifier_dict naar symbol_table
-
 # TODO: TypeChecker de TypeEnvironment meegeven
 # TODO: Environment een class maken en een update value methode geven
-
 # TODO: Evaluator regel 92 de is None checks weghalen
 # TODO: Evaluator latere declaraties die worden niet meegeven ( in een loop evaluaten)
 # TODO: Identifier regel 39 check kan weg, identifier bestaat altijd
-# TODO: String support toevoegen
 
-
+# NICE TO HAVES ---------------------------------
 # TODO: LOW PRIO: Eventueel If / IfElse logic combineren in typechecker
 # TODO: LOWEST PRIO : ast.apply(typevisitor) ipv Typenvironment
 # TODO: NICE TO HAVE Dynamic types in operators ipv python operators
+# TODO: Export of values ?
+# TODO: Make all fields mandatory
+# TODO: NICE TO HAVE Highlight betreffende error line
