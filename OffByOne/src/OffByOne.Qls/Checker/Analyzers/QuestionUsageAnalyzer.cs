4c1f1ac0f84b3ec697b9cda599c0ebd7400d06cb
Teodor92 <teodor.ivanov92@gmail.com>
2017-03-17 22:53:28 +0100

    Added usage analyzer.

--- OffByOne/src/OffByOne.Qls/Checker/Analyzers/QuestionUsageAnalyzer.cs
+++ OffByOne/src/OffByOne.Qls/Checker/Analyzers/QuestionUsageAnalyzer.cs
@@ -1,6 +1,9 @@
 ﻿namespace OffByOne.Qls.Checker.Analyzers
 {
     using System.Collections.Generic;
+    using System.Linq;
+
+    using MoreDotNet.Extensions.Collections;
 
     using OffByOne.Ql.Checker;
     using OffByOne.Ql.Checker.Contracts;
@@ -8,12 +11,15 @@
     using OffByOne.Qls.Ast.Style.Statements;
     using OffByOne.Qls.Checker.Analyzers.Contracts;
     using OffByOne.Qls.Checker.Analyzers.Environment;
+    using OffByOne.Qls.Checker.Messages;
     using OffByOne.Qls.Visitors.Base;
 
     using ValueType = OffByOne.Ql.Ast.ValueTypes.Base.ValueType;
 
     public class QuestionUsageAnalyzer : BaseQlsVisitor<object, FormAnalyzerEnvironment>, IAnalyzer
     {
+        private IDictionary<string, bool> usageMap;
+
         public QuestionUsageAnalyzer()
             : this(new CheckerReport())
         {
@@ -22,17 +28,41 @@
         public QuestionUsageAnalyzer(ICheckerReport report)
         {
             this.Report = report;
+            this.usageMap = new Dictionary<string, bool>();
         }
 
         public ICheckerReport Report { get; }
 
         public void Analyze(StyleSheet root, IDictionary<string, ValueType> qlQuestionMappings)
         {
+            this.usageMap = qlQuestionMappings
+                .Select(x => x.Key)
+                .ToDictionary(x => x, y => false);
+            this.Visit(root, new FormAnalyzerEnvironment());
+
+            var hasUnusedQuestions = this.usageMap.Any(x => !x.Value);
+            if (hasUnusedQuestions)
+            {
+                var unusedQuestions = this.usageMap
+                    .Where(x => !x.Value)
+                    .Select(x => x.Key);
+
+                unusedQuestions.ForEach(x => this.Report.Add(new UnusedQuestionMessage(x)));
+            }
         }
 
         public override object Visit(QuestionRule rule, FormAnalyzerEnvironment environment)
         {
-            return null;
+            if (this.usageMap.ContainsKey(rule.Identifier))
+            {
+                this.usageMap[rule.Identifier] = true;
+            }
+            else
+            {
+                this.Report.Add(new UndeclaredQuestionMessage(rule.Identifier));
+            }
+
+            return base.Visit(rule, environment);
         }
     }
 }
---
+++ OffByOne/src/OffByOne.Qls/Checker/Messages/UndeclaredQuestionMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace OffByOne.Qls.Checker.Messages
+{
+    using OffByOne.Ql.Checker.Messages.Base;
+
+    public class UndeclaredQuestionMessage : ErrorMessage
+    {
+        public UndeclaredQuestionMessage(string questionIdentifier, string description = "")
+            : base($"Question {questionIdentifier} is not declared in the QL structure.", description)
+        {
+        }
+    }
+}
