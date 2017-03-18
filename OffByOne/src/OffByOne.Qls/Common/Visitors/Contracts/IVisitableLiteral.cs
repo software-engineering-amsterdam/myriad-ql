bd3966fd427004809f7034f8327931bc2785c5f4
Teodor92 <teodor.ivanov92@gmail.com>
2017-03-18 15:09:43 +0100

    Structure change to the QLS project.

--- OffByOne/src/OffByOne.Qls/Common/Visitors/Contracts/IVisitableLiteral.cs
+++ OffByOne/src/OffByOne.Qls/Common/Visitors/Contracts/IVisitableLiteral.cs
@@ -1,6 +1,7 @@
 ﻿namespace OffByOne.Qls.Visitors.Contracts
 {
     using OffByOne.Ql.Common.Visitors.Contracts;
+    using OffByOne.Qls.Common.Visitors.Contracts;
 
     public interface IVisitableLiteral : IVisitable
     {
