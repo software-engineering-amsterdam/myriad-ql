4f4a9636d2f068df12198a337f5c6745cb0fcfa6
Teodor92 <teodor.ivanov92@gmail.com>
2017-03-18 17:40:27 +0100

    Started adding QLS AST tests.

---
+++ OffByOne/test/OffByOne.Qls.Tests/Ast/Style/Properties/FontNamePropertyTests.cs
@@ -0,0 +1,26 @@
+﻿namespace OffByOne.Qls.Tests.Ast.Style.Properties
+{
+    using System;
+
+    using OffByOne.Qls.Ast.Style.Literals;
+    using OffByOne.Qls.Ast.Style.Properties;
+
+    using Xunit;
+
+    public class FontNamePropertyTests
+    {
+        [Fact]
+        public void CreatingNewObject_ShouldHaveCorrectValueIfCorectDataGiven()
+        {
+            var actual = new FontNameProperty(new StringLiteral("HelloWorld!"));
+
+            Assert.Equal("HelloWorld!", actual.Value.Value);
+        }
+
+        [Fact]
+        public void CreatingNewObject_ShouldThrowExceptionWhenIncorrectDataGiven()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FontNameProperty(null));
+        }
+    }
+}
---
+++ OffByOne/test/OffByOne.Qls.Tests/Ast/Style/Properties/WidthPropertyTests.cs
@@ -0,0 +1,26 @@
+﻿namespace OffByOne.Qls.Tests.Ast.Style.Properties
+{
+    using System;
+
+    using OffByOne.Qls.Ast.Style.Literals;
+    using OffByOne.Qls.Ast.Style.Properties;
+
+    using Xunit;
+
+    public class WidthPropertyTests
+    {
+        [Fact]
+        public void CreatingNewObject_ShouldHaveCorrectValueIfCorectDataGiven()
+        {
+            var actual = new WidthProperty(new IntegerLiteral(42));
+
+            Assert.Equal(42, actual.Value.Value);
+        }
+
+        [Fact]
+        public void CreatingNewObject_ShouldThrowExceptionWhenIncorrectDataGiven()
+        {
+            Assert.Throws<ArgumentNullException>(() => new WidthProperty(null));
+        }
+    }
+}
