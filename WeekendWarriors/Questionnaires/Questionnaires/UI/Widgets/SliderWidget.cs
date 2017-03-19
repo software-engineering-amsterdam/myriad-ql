ffe314a87f7a306ffd975e4dd0751f106c29bd73
Floris Velleman <florisvelleman@hotmail.com>
2017-03-19 16:51:05 +0100

    Added inheritance for slider widgets

---
+++ WeekendWarriors/Questionnaires/Questionnaires/UI/Widgets/SliderWidget.cs
@@ -0,0 +1,22 @@
+﻿using Questionnaires.QL.AST.Types;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Questionnaires.UI.Widgets
+{
+    class SliderWidget : QuestionWidget
+    {
+        protected Slider Slider;
+
+        public SliderWidget(Slider control) : base(control)
+        {
+            Slider = control;
+            Slider.Maximum = 1E6;
+            Slider.Minimum = 0;
+            Slider.Width = 100;
+        }
+
+        public override void SetQuestionValue(IType value)
+        { }
+    }
+}
