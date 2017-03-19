e1fd4c51ad48d0841ad3d663708dd60c02632713
Jasper Dijt <jasper@jasperdijt.eu>
2017-03-19 14:05:13 +0100

    Beheer :: Spinbox.

--- Beheer/src/main/scala/view/NumericQuestion.scala
+++ Beheer/src/main/scala/view/NumericQuestion.scala
@@ -2,14 +2,14 @@ package view
 
 import ast._
 import model.{ ComputedQuestion, DisplayQuestion }
-import view.widgets.{ NumericTextWidget, SliderWidget }
+import view.widgets.{ NumericTextWidget, SliderWidget, SpinboxWidget }
 
 class NumericQuestion(val question: DisplayQuestion, val questionStyle: Option[QuestionStyle] = None) extends GUIQuestion {
   private def questionType = question.`type` match {
     case n: NumericType => n
     case _ => sys.error(s"Constructing numericQuestion for non numeric question $question")
   }
-  private def defaultWidget = new SliderWidget(0, 1000.0, questionType)
+  private def defaultWidget = new SpinboxWidget(questionType)
   private val widget = questionStyle match {
     case Some(q) => q.widget match {
       case Some(w) => defaultWidget
--- Beheer/src/main/scala/view/widgets/SpinboxWidget.scala
+++ Beheer/src/main/scala/view/widgets/SpinboxWidget.scala
@@ -1,5 +1,29 @@
 package view.widgets
 
-class SpinboxWidget {
+import ast.{ DecimalType, IntegerType, MoneyType, NumericType }
+import values.{ NumericValue, UndefinedValue, Value }
+
+import scalafx.scene.control.{ Spinner }
+
+class SpinboxWidget(numType: NumericType)(implicit val changeHandler: Value => Unit) extends QLWidget {
+  private val spinbox: Spinner[Double] = numType match {
+    case IntegerType => new Spinner(Double.MinValue, Double.MaxValue, 0, 1)
+    case DecimalType => new Spinner(Double.MinValue, Double.MaxValue, 0, 0.01)
+    case MoneyType => new Spinner(Double.MinValue, Double.MaxValue, 0, 0.01)
+  }
+
+  spinbox.value.onChange {
+    val qlValue = NumericValue.doubleToNumericValue(spinbox.value.value, numType)
+    this.setValue(qlValue)
+    changeHandler(qlValue)
+  }
+
+  override def setValue(newVal: Value): Unit = newVal match {
+    case n: NumericValue => spinbox.valueFactory.value.setValue(n.value.doubleValue)
+    case UndefinedValue => spinbox.valueFactory.value.setValue(0)
+    case v => sys.error(s"Incompatible value $v for spinbox widget")
+  }
+
+  override def getSFXNode = spinbox
 
 }
