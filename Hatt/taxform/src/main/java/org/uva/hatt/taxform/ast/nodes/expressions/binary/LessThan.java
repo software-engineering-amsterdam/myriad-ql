583ea2a55f86b613a7063bcfe7193e79d56ae5dd
Thanus <tthanusijan@gmail.com>
2017-03-20 13:11:01 +0100

    rename expressions, remove is and to

---
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/binary/LessThan.java
@@ -0,0 +1,16 @@
+package org.uva.hatt.taxform.ast.nodes.expressions.binary;
+
+import org.uva.hatt.taxform.ast.nodes.expressions.Expression;
+//import org.uva.hatt.taxform.ast.visitors.ExpressionVisitor;
+
+public class LessThan extends Binary{
+
+    public LessThan(int lineNumber, Expression lhs, Expression rhs){
+        super(lineNumber, lhs, rhs);
+    }
+
+    //@Override
+//    public <T> T accept(ExpressionVisitor<T> expressionVisitor){
+//        return expressionVisitor.visit(this);
+//    }
+}
