4a4783c6dd8282ba57a922cee3d4ba3b66233336
Sander Ginn <sanderginn@users.noreply.github.com>
2017-03-20 13:17:11 +0100

    Refactored GUI to more OO style

---
+++ Lemonade/src/main/java/org/lemonade/gui/QLApp.java
@@ -0,0 +1,97 @@
+package org.lemonade.gui;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.lemonade.QLLexer;
+import org.lemonade.QLParser;
+import org.lemonade.QLParserErrorListener;
+import org.lemonade.nodes.Form;
+import org.lemonade.visitors.EvaluateVisitor;
+import org.lemonade.visitors.FormVisitor;
+import org.lemonade.visitors.GuiVisitor;
+import org.lemonade.visitors.TypeCheckVisitor;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class QLApp extends Application {
+
+    private QLGui qlGui;
+
+    private boolean isSubmitted = false;
+
+    // JavaFX initialisation method
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+
+        qlGui = new QLGui(primaryStage);
+        qlGui.getSubmitFileButton().setOnAction(e -> {
+            if (qlGui.getFile() != null) {
+                goToQuestionnaire();
+            }
+        });
+
+        primaryStage.setScene(qlGui.getSelectionScene());
+        primaryStage.setWidth(1000);
+        primaryStage.setHeight(800);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    // Called on file submit
+    private void goToQuestionnaire() {
+        GuiForm guiRoot;
+
+        try {
+            Form root = parseFileAndValidateForm(qlGui.getFile());
+            GuiVisitor guiVisitor = new GuiVisitor(qlGui);
+            guiRoot = (GuiForm) root.accept(guiVisitor);
+
+            qlGui.setUpQuestionnaireScene();
+            qlGui.getSubmitQuestionnaireButton().setOnAction(e -> {
+                isSubmitted = true;
+                submitForm(guiRoot);
+            });
+
+            EvaluateVisitor evaluateVisitor = new EvaluateVisitor();
+            qlGui.addUserInputListeners(guiRoot, evaluateVisitor);
+
+            qlGui.goToQuestionnaire();
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+    // Something will happen here (evaluate visitor)
+    private void submitForm(GuiForm guiForm) {
+        System.err.println("In submit");
+    }
+
+    private Form parseFileAndValidateForm(final File file) throws IOException {
+        final String contents = String.join("\n", Files.readAllLines(Paths.get(file.getPath())));
+
+        ANTLRInputStream input = new ANTLRInputStream(new StringReader(contents));
+        QLLexer lexer = new QLLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        QLParser parser = new QLParser(tokens);
+        QLParserErrorListener errorListener = new QLParserErrorListener();
+        parser.removeErrorListeners();
+        parser.addErrorListener(errorListener);
+        ParseTree tree = parser.form();
+
+        FormVisitor visitor = new FormVisitor();
+        Form root = (Form) tree.accept(visitor);
+
+        TypeCheckVisitor typeCheckVisitor = new TypeCheckVisitor();
+        root.accept(typeCheckVisitor);
+
+        return root;
+    }
+}
---
+++ Lemonade/src/main/java/org/lemonade/gui/QLGui.java
@@ -0,0 +1,152 @@
+package org.lemonade.gui;
+
+import java.io.File;
+
+import org.lemonade.gui.elements.GuiElement;
+import org.lemonade.gui.elements.GuiLabelElement;
+import org.lemonade.visitors.EvaluateVisitor;
+import org.lemonade.visitors.GuiVisitor;
+
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class QLGui {
+
+    // Gui wide elements
+    private Stage stage;
+    private File file;
+
+    // File selection elements
+    private Scene selectionScene;
+    private Button submitFileButton;
+
+    // Questionnaire elements
+    private Scene questionnaireScene;
+    private Button submitQuestionnaireButton;
+    private GridPane questionsGridPane;
+    private int questionsRowCount;
+
+    public QLGui(Stage stage) {
+        this.stage = stage;
+        setUpFileSelectionScene();
+        setUpQuestionsPane();
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public Scene getSelectionScene() {
+        return selectionScene;
+    }
+
+    public Button getSubmitFileButton() {
+        return submitFileButton;
+    }
+
+    public Button getSubmitQuestionnaireButton() {
+        return submitQuestionnaireButton;
+    }
+
+    private void setUpFileSelectionScene() {
+        stage.setTitle("Form");
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add((new FileChooser.ExtensionFilter("Select a .ql file", "*.ql")));
+
+        Label fileLabel = new Label();
+
+        final Button openButton = new Button("Select a questionnaire");
+        openButton.setOnAction(e -> {
+            file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                fileLabel.setText(file.getPath());
+            }
+        });
+
+        submitFileButton = new Button("Submit");
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.getChildren().addAll(openButton, submitFileButton, fileLabel);
+
+        AnchorPane anchorPane = new AnchorPane();
+        AnchorPane.setBottomAnchor(hBox, 10.0);
+        AnchorPane.setLeftAnchor(hBox, 5.0);
+        anchorPane.getChildren().add(hBox);
+        anchorPane.setPadding(new Insets(10, 10, 10, 10));
+
+        selectionScene = new Scene(anchorPane);
+    }
+
+    public void setUpQuestionnaireScene() {
+        submitQuestionnaireButton = new Button("Submit form");
+
+        final Button backButton = new Button("Select new questionnaire");
+        backButton.setOnAction(e -> stage.setScene(selectionScene));
+
+        final AnchorPane rootGroup = new AnchorPane();
+
+        AnchorPane.setBottomAnchor(submitQuestionnaireButton, 10.0);
+        AnchorPane.setLeftAnchor(submitQuestionnaireButton, 5.0);
+        AnchorPane.setBottomAnchor(backButton, 10.0);
+        AnchorPane.setRightAnchor(backButton, 5.0);
+        AnchorPane.setTopAnchor(questionsGridPane, 10.0);
+        AnchorPane.setLeftAnchor(questionsGridPane, 5.0);
+        AnchorPane.setRightAnchor(questionsGridPane, 5.0);
+
+        rootGroup.getChildren().addAll(questionsGridPane, backButton, submitQuestionnaireButton);
+        rootGroup.setPadding(new Insets(10, 10, 10, 10));
+
+        questionnaireScene = new Scene(rootGroup);
+    }
+
+    public void setUpQuestionsPane() {
+        questionsGridPane = new GridPane();
+        ColumnConstraints constraints = new ColumnConstraints();
+        constraints.setPercentWidth(100);
+        questionsGridPane.getColumnConstraints().addAll(constraints);
+
+        this.questionsRowCount = 0;
+    }
+
+    public void addQuestion(GuiLabelElement labelElement, GuiElement element) {
+        final GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(3, 6, 3, 6));
+
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(50);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(50);
+        gridPane.getColumnConstraints().addAll(col1, col2);
+
+        GridPane.setConstraints(labelElement.getWidget(), 0, 0);
+        GridPane.setConstraints(element.getWidget(), 1, 0);
+        gridPane.getChildren().addAll(labelElement.getWidget(), element.getWidget());
+        gridPane.managedProperty().bind(gridPane.visibleProperty());
+
+        // Increment rowCount to place question on next row
+        questionsGridPane.addRow(questionsRowCount++, gridPane);
+    }
+
+    public void addUserInputListeners(GuiForm guiRoot, EvaluateVisitor evaluateVisitor) {
+        Parent rootGroup = questionnaireScene.getRoot();
+        rootGroup.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> guiRoot.accept(evaluateVisitor));
+        rootGroup.addEventFilter(KeyEvent.KEY_RELEASED, e -> guiRoot.accept(evaluateVisitor));
+    }
+
+    public void goToQuestionnaire() {
+        this.stage.setScene(questionnaireScene);
+    }
+}
