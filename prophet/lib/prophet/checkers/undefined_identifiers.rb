2c039792cd280a9ff91a1ed9557db6eadb98cb24
Andrei Horak <linkyndy@gmail.com>
2017-03-20 11:46:16 +0100

    Redefine used identifiers checker to use visitor pattern

--- prophet/lib/prophet/checkers/undefined_identifiers.rb
+++ prophet/lib/prophet/checkers/undefined_identifiers.rb
@@ -2,30 +2,23 @@ module Prophet
   module Checkers
     class UndefinedIdentifiers < Base
       def check
-        ast.select do |node|
-          Ast::Identifier === node
-        end.reject do |identifier|
-          defined_identifiers.include? identifier.name.to_s
-        end.each do |identifier|
-          puts error_formatter(identifier)
+        defined_identifier_names = ast.visit(Collectors::DefinedIdentifiers.new).map(&:name).map(&:to_s)
+        used_identifier_names = ast.visit(Collectors::UsedIdentifiers.new).flatten.compact.map(&:name)
+
+        used_identifier_names.reject do |name|
+          defined_identifier_names.include? name
+        end.each do |name|
+          puts error_formatter(name)
         end
       end
 
-      def error_formatter(item)
-        "Identifier `#{item.name}` is not defined (used on #{item.name.line_and_column.join(':')})"
+      def error_formatter(name)
+        "Identifier `#{name}` is not defined (used on #{name.line_and_column.join(':')})"
       end
 
       private
 
       attr_reader :ast
-
-      def defined_identifiers
-        ast.select do |node|
-          Ast::Question === node || Ast::Form === node
-        end.map do |question|
-          question.identifier.name.to_s
-        end
-      end
     end
   end
 end
