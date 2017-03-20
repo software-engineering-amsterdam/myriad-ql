8047d3c674e6b98babd9db7f8bba45ed872b6747
jetsekoopmans <jetsekoopmans@hotmail.com>
2017-03-20 13:02:11 +0100

    refactor big time

--- koopmans/lib/ql/ast/expression/binary_expression/arithmetic_expression/divide.rb
+++
@@ -1,9 +0,0 @@
-# module QL
-#   module AST
-#     class Divide < ArithmeticExpression
-#       def eval(left, right)
-#         IntegerLiteral.new(left / right)
-#       end
-#     end
-#   end
-# end
