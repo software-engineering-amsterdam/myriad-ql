4d6afa467d7a2ebfa6fcc7fdbb91df0fa9a00d99
Alberto Mtnz <alberto@threkk.com>
2017-03-20 01:33:02 +0100

    Adds a launch script for the application.

--- threkk/qlworkbench/main.py
+++ threkk/qlworkbench/main.py
@@ -1,3 +1,8 @@
+"""
+This module is used to launch the QL Workbench. It is a CLI application which
+reads a path from the command line. If the file contains errors, they will be
+printed in the screen. If not, the application will be launched.
+"""
 import click
 from ql.lexer import QLLexer
 from ql.parser import QLParser
@@ -9,20 +14,22 @@ from ui.visitor import UIVisitor
 def cli(file):
     """
     This program generates a formulary based on QL files. It parses the given
-    <file> and generates a graphical interface based on the content.
+    <file> and generates a graphical interface based on the content. In case it
+    contains errors, they will be displayed in the screen.
     """
     data = file.read()
     lex = QLLexer()
     par = QLParser(lex.tokens)
     ast = par.parse(data, lexer=lex.lexer)
+    ast.typechecker.check()
 
     if ast.get_warnings():
         for warn in ast.get_warnings():
-            print(warn)
+            click.secho(warn.__str__(), fg='yellow')
 
     if ast.get_errors():
         for error in ast.get_errors():
-            print(error)
+            click.secho(error.__str__(), fg='red')
     else:
         ui_generator = UIVisitor(ast)
         ui = ui_generator.execute()
