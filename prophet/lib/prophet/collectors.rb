fc3af16cdfff504b6558decbe5e9ce6db913d752
Andrei Horak <linkyndy@gmail.com>
2017-03-20 13:01:18 +0100

    Add collectors for questions with values and for expression identifiers

--- prophet/lib/prophet/collectors.rb
+++ prophet/lib/prophet/collectors.rb
@@ -1,4 +1,6 @@
 require 'prophet/collectors/defined_identifiers'
+require 'prophet/collectors/expression_identifiers'
+require 'prophet/collectors/questions_with_value'
 require 'prophet/collectors/used_identifiers'
 
 module Prophet
