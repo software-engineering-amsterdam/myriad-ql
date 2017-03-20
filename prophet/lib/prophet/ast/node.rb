125f93efba88e7b7cdb44d3bffafe0144264a322
Andrei Horak <linkyndy@gmail.com>
2017-03-21 00:06:10 +0100

    Add syntactic sugar for line/column and for selecting certain node types

--- prophet/lib/prophet/ast/node.rb
+++ prophet/lib/prophet/ast/node.rb
@@ -10,18 +10,18 @@ module Prophet
         visitor.send(method_name, self)
       end
 
-      # def walk(&block)
-      #   initial = block_given? && !block.call(self) ? [] : [self]
-      #   children.inject(initial) do |memo, child|
-      #     (memo << child.walk(&block)).flatten
-      #   end
-      # end
-
       def each(&block)
         block.call(self)
         children.each { |child| child.each(&block) }
       end
       alias walk each
+
+      def select_by_type(*types)
+        qualified_types = types.map do |type|
+          ActiveSupport::Inflector.constantize("Prophet::Ast::#{ActiveSupport::Inflector.camelize(type)}")
+        end
+        select { |node| qualified_types.any? { |type| node.is_a? type }}
+      end
 
       def children
         values.each_with_object([]) do |child, memo|
@@ -30,6 +30,18 @@ module Prophet
           elsif child.kind_of? Array
             child.each { |c| memo << c if c.kind_of? Node }
           end
+        end
+      end
+
+      def line_and_column
+        first_value = values.first
+        case first_value
+        when Parslet::Slice, Node
+          first_value.line_and_column
+        when Array
+          first_value.first.line_and_column
+        else
+          []
         end
       end
 
