91907bf6f1ba6b118b272eb85b90bf7bbd0592ab
aijkoopmans <arendkoopmans@gmail.com>
2017-03-20 13:14:38 +0100

    refactorrrrrrr

--- koopmans/lib/ql/ast/expression/expression.rb
+++ koopmans/lib/ql/ast/expression/expression.rb
@@ -10,18 +10,6 @@ module QL
       def accept(visitor)
         visitor.visit_expression(self)
       end
-
-      # def eval_type(left_type, right_type)
-      #   # return the left type if there are no errors and else return an error
-      #   if self.is_compatible_with.include?(left_type) and
-      #     self.is_compatible_with.include?(right_type) and
-      #     left_type == right_type
-      #     self.return_type(left_type)
-      #   else
-      #     NotificationTable.store(Notification::Error.new("incompatible types at #{self}"))
-      #     ErrorType.new
-      #   end
-      # end
     end
   end
 end
--- koopmans/lib/ql/ast/literal/boolean_literal.rb
+++ koopmans/lib/ql/ast/literal/boolean_literal.rb
@@ -7,13 +7,13 @@ module QL
         @value = to_boolean(value.to_s)
       end
 
+      def accept(visitor)
+        visitor.visit_boolean_literal(self)
+      end
+
       def to_boolean(value)
         return true if value == 'true'
         return false if value == 'false'
-      end
-
-      def accept(visitor)
-        visitor.visit_boolean_literal(self)
       end
     end
   end
