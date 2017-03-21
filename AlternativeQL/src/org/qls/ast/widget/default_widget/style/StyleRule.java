83deee30259123aabe69ddfab7d4047218db84be
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2017-03-21 12:05:02 +0100

    Using stylerule class instead of class name

--- AlternativeQL/src/org/qls/ast/widget/default_widget/style/StyleRule.java
+++ AlternativeQL/src/org/qls/ast/widget/default_widget/style/StyleRule.java
@@ -3,16 +3,16 @@ package org.qls.ast.widget.default_widget.style;
 import org.ql.ast.Node;
 
 /**
- * hashCode() and equals() use instance's class name because style rules cannot be duplicated for a widget
+ * hashCode() and equals() use instance's class because style rules cannot be duplicated for a widget
  */
 public abstract class StyleRule extends Node {
     @Override
     public int hashCode() {
-        return this.getClass().getName().hashCode();
+        return this.getClass().hashCode();
     }
 
     @Override
     public boolean equals(Object obj) {
-        return this.getClass().getName().equals(obj.getClass().getName());
+        return this.getClass().equals(obj.getClass());
     }
 }
