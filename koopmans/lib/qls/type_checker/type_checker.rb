ebab85a8bc3c16abf235ed0c6cabd559dce5e1c8
jetsekoopmans <jetsekoopmans@hotmail.com>
2017-03-21 16:54:54 +0100

    QLS

--- koopmans/lib/qls/type_checker/type_checker.rb
+++ koopmans/lib/qls/type_checker/type_checker.rb
@@ -2,9 +2,14 @@ module QLS
   module TypeChecker
     class TypeChecker
       def check(qls_ast, ql_ast)
-        ql_questions = ql_ast.accept(QL::TypeChecker::QuestionCollector.new).flatten
+        ql_question_collector = QL::TypeChecker::QuestionCollector.new
+        ql_ast.accept(ql_question_collector)
+        ql_questions = ql_question_collector.questions
         ql_variable_names = ql_questions.map(&:variable).map(&:name)
-        qls_variable_names = qls_ast.accept(QuestionVariableCollector.new).flatten.compact.map(&:name)
+
+        qls_variables_collector = QuestionVariableCollector.new
+        qls_ast.accept(qls_variables_collector)
+        qls_variable_names = qls_variables_collector.variables.map(&:name)
 
         questions_placed_checker(qls_variable_names, ql_variable_names)
         questions_reference_checker(qls_variable_names, ql_variable_names)
@@ -13,15 +18,21 @@ module QLS
       end
 
       def questions_placed_checker(qls_variable_names, ql_variable_names)
-        (ql_variable_names - qls_variable_names).map { |error| NotificationTable.store(Notification::Error.new("#{error} of the QL program is not placed by the QLS program.")) }
+        (ql_variable_names - qls_variable_names).each do |error|
+          NotificationTable.store(Notification::Error.new("#{error} of the QL program is not placed by the QLS program."))
+        end
       end
 
       def questions_reference_checker(qls_variable_names, ql_variable_names)
-        (qls_variable_names - ql_variable_names).map { |error| NotificationTable.store(Notification::Error.new("#{error} is referenced to a question that is not in the QL program")) }
+        (qls_variable_names - ql_variable_names).each do |error|
+          NotificationTable.store(Notification::Error.new("#{error} is referenced to a question that is not in the QL program"))
+        end
       end
 
       def questions_uniqueness_checker(qls_variable_names)
-        select_duplicates(qls_variable_names).map { |error| NotificationTable.store(Notification::Error.new("#{error} is placed multiple times")) }
+        select_duplicates(qls_variable_names).each do
+          |error| NotificationTable.store(Notification::Error.new("#{error} is placed multiple times"))
+        end
       end
 
       def widget_type_checker(qls_ast, ql_questions)
@@ -29,7 +40,7 @@ module QLS
         ql_questions.each do |ql_question|
           ql_variable_type_map[ql_question.variable.name] = ql_question.type
         end
-        qls_ast.accept(WidgetTypeChecker.new, ql_variable_type_map).flatten.compact
+        qls_ast.accept(WidgetTypeChecker.new, ql_variable_type_map)
       end
 
       def select_duplicates(elements)
