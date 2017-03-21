911fd97afa13824b731622d105bab134b88d6b12
Sander Ginn <sanderginn@users.noreply.github.com>
2017-03-21 14:32:42 +0100

    Some json updates

--- Lemonade/src/main/java/org/lemonade/visitors/WriteToJsonVisitor.java
+++ Lemonade/src/main/java/org/lemonade/visitors/WriteToJsonVisitor.java
@@ -1,5 +1,7 @@
 package org.lemonade.visitors;
 
+import com.cedarsoftware.util.io.JsonWriter;
+
 import org.json.simple.JSONObject;
 import org.lemonade.gui.GuiConditional;
 import org.lemonade.gui.GuiForm;
@@ -8,17 +10,18 @@ import org.lemonade.visitors.interfaces.GuiBaseElementsVisitor;
 
 public class WriteToJsonVisitor implements GuiBaseElementsVisitor {
 
+    private JSONObject rootJSON;
     private JSONObject formContents;
 
     public WriteToJsonVisitor() {
+        rootJSON = new JSONObject();
         formContents = new JSONObject();
     }
 
     @Override
     public void visit(final GuiForm form) {
-        JSONObject json = new JSONObject();
         form.getBodies().forEach(body -> body.accept(this));
-        json.put(form.getIdentifier(), formContents);
+        rootJSON.put(form.getIdentifier(), formContents);
     }
 
     @Override
@@ -30,4 +33,9 @@ public class WriteToJsonVisitor implements GuiBaseElementsVisitor {
     public void visit(final GuiConditional conditional) {
 
     }
+
+    public String getJSONString() {
+        return JsonWriter.formatJson(rootJSON.toString());
+    }
+
 }
