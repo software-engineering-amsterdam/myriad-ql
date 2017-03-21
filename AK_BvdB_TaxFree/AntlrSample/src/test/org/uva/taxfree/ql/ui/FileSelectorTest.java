1e30dd500d63b7c531a1c1124cc8fe37145c917d
Alex Kok <alexkok08@gmail.com>
2017-03-21 15:12:01 +0100

    Ignore tests that opens ui

--- AK_BvdB_TaxFree/AntlrSample/src/test/org/uva/taxfree/ql/ui/FileSelectorTest.java
+++ AK_BvdB_TaxFree/AntlrSample/src/test/org/uva/taxfree/ql/ui/FileSelectorTest.java
@@ -1,13 +1,13 @@
 package test.org.uva.taxfree.ql.ui;
 
-import org.testng.annotations.Test;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.uva.taxfree.ql.gui.FileSelector;
 
 import java.io.File;
 
 public class FileSelectorTest {
 
-    @Test
+    @Ignore
     public void testFileDialog() throws Exception {
         File inputFile = FileSelector.select();
         System.out.println(inputFile);
