70651528c908ebdd00aa1c3cfc1a4a38aeb902d9
Thanus <tthanusijan@gmail.com>
2017-03-21 18:35:15 +0100

    add accept on logical and and or
    * otherwise it will cause a stackoverflow in the evaluation

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/binary/LogicalOr.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/binary/LogicalOr.java
@@ -2,9 +2,15 @@ package org.uva.hatt.taxform.ast.nodes.expressions.binary;
 
 import org.uva.hatt.taxform.ast.nodes.expressions.BooleanExpression;
 import org.uva.hatt.taxform.ast.nodes.expressions.Expression;
+import org.uva.hatt.taxform.ast.visitors.Visitor;
 
 public class LogicalOr extends BooleanExpression {
     public LogicalOr(int lineNumber, Expression lhs, Expression rhs) {
         super(lineNumber, lhs, rhs);
+    }
+
+    @Override
+    public <T> T accept(Visitor<T> visitor) {
+        return visitor.visit(this);
     }
 }
