9c63ec100bc65e95f18446f1c65583efce62d0d8
Ahmed <ahmedmusaafir@gmail.com>
2017-03-21 16:45:31 +0100

    Setting next or previous page button active

--- AlternativeQL/src/org/qls/gui/Pagination.java
+++ AlternativeQL/src/org/qls/gui/Pagination.java
@@ -1,7 +1,5 @@
 package org.qls.gui;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
@@ -9,13 +7,18 @@ public class Pagination {
     private int currentPage;
     private Button previousButton = new Button();
     private Button nextButton = new Button();
+    private int maxPage;
 
     public Pagination(QLSGUIFormBuilder qlsGUIFormBuilder, int maxPage) {
+        this.maxPage = maxPage;
+        checkButtonActivity();
+
         nextButton.setText("Next page");
         nextButton.setOnAction(event -> {
             if(currentPage < (maxPage-1)) {
                 nextPage();
                 qlsGUIFormBuilder.constructFormPage(currentPage);
+                checkButtonActivity();
             }
         });
 
@@ -24,8 +27,14 @@ public class Pagination {
             if(!(currentPage <= 0)) {
                 previousPage();
                 qlsGUIFormBuilder.constructFormPage(currentPage);
+                checkButtonActivity();
             }
         });
+    }
+
+    private void checkButtonActivity() {
+        nextButton.setDisable(currentPage == (maxPage-1));
+        previousButton.setDisable(currentPage == 0);
     }
 
     public void nextPage() {
