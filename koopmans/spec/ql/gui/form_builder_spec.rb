8d2a2e10f40b51b4a7dcd97dcbe569beb888054f
jetsekoopmans <jetsekoopmans@hotmail.com>
2017-03-21 18:04:43 +0100

    fixed some tests

--- koopmans/spec/ql/gui/form_builder_spec.rb
+++ koopmans/spec/ql/gui/form_builder_spec.rb
@@ -18,23 +18,29 @@ module QL
       context 'ast with one question' do
         it 'has only one question frame' do
           form = Form.new('_', [question])
-          expect(form.accept(form_builder).first).to be_a(QuestionFrame)
-          expect(form.accept(form_builder).size).to be(1)
+          form.accept(form_builder)
+          question_frames = form_builder.question_frames
+          expect(question_frames.first).to be_a(QuestionFrame)
+          expect(question_frames.size).to be(1)
         end
       end
 
       context 'ast with one computed question' do
         it 'has only one computed question frame' do
           form = Form.new('_', [computed_question])
-          expect(form.accept(form_builder).first).to be_a(ComputedQuestionFrame)
-          expect(form.accept(form_builder).size).to be(1)
+          form.accept(form_builder)
+          question_frames = form_builder.question_frames
+          expect(question_frames.first).to be_a(ComputedQuestionFrame)
+          expect(question_frames.size).to be(1)
         end
       end
 
       context 'ast with if statement and one question' do
         it 'assigns correct condition' do
           form = Form.new('_', [if_statement])
-          expect(form.accept(form_builder).first.condition).to be_a(BooleanLiteral)
+          form.accept(form_builder)
+          question_frames = form_builder.question_frames
+          expect(question_frames.first.condition).to be_a(BooleanLiteral)
         end
       end
 
@@ -42,20 +48,22 @@ module QL
         it 'assigns correct condition' do
           outer_if_statement = IfStatement.new(condition, [if_statement])
           form = Form.new('_', [outer_if_statement])
-
+          form.accept(form_builder)
+          question_frames = form_builder.question_frames
           # true && true
-          expect(form.accept(form_builder).first.condition.expressions[0]).to be_a(BooleanLiteral)
-          expect(form.accept(form_builder).last.condition.expressions[0]).to be_a(BooleanLiteral)
+          expect(question_frames.first.condition.expressions[0]).to be_a(BooleanLiteral)
+          expect(question_frames.last.condition.expressions[0]).to be_a(BooleanLiteral)
         end
       end
 
       context 'ast with if else statement and one question each' do
         it 'assigns correct conditions' do
           form = Form.new('_', [if_else_statement])
-
+          form.accept(form_builder)
+          question_frames = form_builder.question_frames
           # true && false
-          expect(form.accept(form_builder).first.condition).to be_a(BooleanLiteral)
-          expect(form.accept(form_builder).last.condition).to be_a(BooleanNegation)
+          expect(question_frames.first.condition).to be_a(BooleanLiteral)
+          expect(question_frames.last.condition).to be_a(BooleanNegation)
         end
       end
 
@@ -63,22 +71,24 @@ module QL
         it 'assigns correct conditions' do
           outer_if_else_statement = IfElseStatement.new(condition, [if_else_statement], [if_else_statement])
           form = Form.new('_', [outer_if_else_statement])
+          form.accept(form_builder)
+          question_frames = form_builder.question_frames
 
           # true && true
-          expect(form.accept(form_builder)[0].condition.expressions[0]).to be_a(BooleanLiteral)
-          expect(form.accept(form_builder)[0].condition.expressions[1].expression).to be_a(BooleanLiteral)
+          expect(question_frames[0].condition.expressions[0]).to be_a(BooleanLiteral)
+          expect(question_frames[0].condition.expressions[1].expression).to be_a(BooleanLiteral)
 
           # true && false
-          expect(form.accept(form_builder)[1].condition.expressions[0]).to be_a(BooleanLiteral)
-          expect(form.accept(form_builder)[1].condition.expressions[1].expression).to be_a(BooleanNegation)
+          expect(question_frames[1].condition.expressions[0]).to be_a(BooleanLiteral)
+          expect(question_frames[1].condition.expressions[1].expression).to be_a(BooleanNegation)
 
           # false && true
-          expect(form.accept(form_builder)[2].condition.expressions[0]).to be_a(BooleanNegation)
-          expect(form.accept(form_builder)[2].condition.expressions[1].expression).to be_a(BooleanLiteral)
+          expect(question_frames[2].condition.expressions[0]).to be_a(BooleanNegation)
+          expect(question_frames[2].condition.expressions[1].expression).to be_a(BooleanLiteral)
 
           # false && false
-          expect(form.accept(form_builder)[3].condition.expressions[0]).to be_a(BooleanNegation)
-          expect(form.accept(form_builder)[3].condition.expressions[1].expression).to be_a(BooleanNegation)
+          expect(question_frames[3].condition.expressions[0]).to be_a(BooleanNegation)
+          expect(question_frames[3].condition.expressions[1].expression).to be_a(BooleanNegation)
         end
       end
     end
