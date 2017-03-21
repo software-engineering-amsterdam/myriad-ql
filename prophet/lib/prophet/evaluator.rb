a08d01cabf80f53de3fdca0199a0527cc465021b
Andrei Horak <linkyndy@gmail.com>
2017-03-21 12:25:49 +0100

    Use dependency sorting to properly resolve context while evaluating AST

--- prophet/lib/prophet/evaluator.rb
+++ prophet/lib/prophet/evaluator.rb
@@ -8,9 +8,7 @@ module Prophet
     end
 
     def evaluate
-      ast.select do |node|
-        Ast::Question === node && node.value
-      end.each do |question|
+      sorted_questions.each do |question|
         context[question.identifier.name.to_s] = question.value.visit(Visitors::ExpressionEvaluator.new(context))
       end
     end
@@ -18,5 +16,21 @@ module Prophet
     private
 
     attr_reader :ast
+
+    def dependency_hash
+      @dependency_hash ||= DependencyHash[
+        ast.select_by_type(:question_with_value).map do |question|
+          [question.identifier.name.to_s,
+           question.select_by_type(:identifier).map(&:name).map(&:to_s)]
+        end
+      ]
+    end
+
+    def sorted_questions
+      sorted_identifier_names = dependency_hash.tsort
+      ast.select_by_type(:question_with_value).sort_by do |question|
+        sorted_identifier_names.index question.identifier.name.to_s
+      end
+    end
   end
 end
