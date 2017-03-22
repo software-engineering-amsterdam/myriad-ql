0525c13495b3049def89a018a79a5031a4fd34a9
Matt Chapman <matt@matthewchapman.co.uk>
2017-03-22 12:00:28 +0100

    add dependency

--- supercoolQL/supercoolQL/src/main/com/matthewchapman/ql/visitors/TypeVisitor.java
+++ supercoolQL/supercoolQL/src/main/com/matthewchapman/ql/visitors/TypeVisitor.java
@@ -9,7 +9,7 @@ import com.matthewchapman.ql.ast.atomic.type.StringType;
  * Created by matt on 18/03/2017.
  */
 public interface TypeVisitor<T, C> {
-    
+
     T visit(BooleanType booleanType, C context);
 
     T visit(IntegerType integerType, C context);
