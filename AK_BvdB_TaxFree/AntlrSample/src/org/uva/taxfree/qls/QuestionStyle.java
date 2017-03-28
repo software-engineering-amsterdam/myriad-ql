8e11e79a1256b16340ecc026b7501fa44a43ad0b
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-03-28 14:34:01 +0200

    QuestionStyle and Sections

---
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/qls/QuestionStyle.java
@@ -0,0 +1,12 @@
+package org.uva.taxfree.qls;
+
+import org.uva.taxfree.ql.model.SourceInfo;
+import org.uva.taxfree.qls.styleoption.StyleOption;
+
+import java.util.List;
+
+public class QuestionStyle extends StyleDeclaration {
+    public QuestionStyle(List<StyleOption> styleOptions, SourceInfo sourceInfo) {
+        super(styleOptions, sourceInfo);
+    }
+}
---
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/qls/Section.java
@@ -0,0 +1,24 @@
+package org.uva.taxfree.qls;
+
+import org.uva.taxfree.ql.gui.MessageList;
+import org.uva.taxfree.ql.model.environment.SymbolTable;
+
+import java.util.List;
+
+public class Section {
+    private final String mName;
+    private final List<String> mQuestions;
+
+    public Section(String name, List<String> questions) {
+        mName = name;
+        mQuestions = questions;
+    }
+
+    public void checkSemantics(SymbolTable symbolTable, MessageList semanticsMessages) {
+        for (String question : mQuestions) {
+            if (!symbolTable.contains(question)) {
+                semanticsMessages.addWarning("Undeclared identifier, section: " + mName + ", question: " + question);
+            }
+        }
+    }
+}
