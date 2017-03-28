8c19735e803a6fa2976c27f67d94f1220d48f537
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-03-28 17:29:37 +0200

    Fixed compile errors due to changed structure

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/qls/QlsStyleBuilder.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/qls/QlsStyleBuilder.java
@@ -2,7 +2,6 @@ package org.uva.taxfree.qls;
 
 import org.uva.taxfree.ql.model.SourceInfo;
 import org.uva.taxfree.ql.model.types.BooleanType;
-import org.uva.taxfree.ql.model.types.StringType;
 import org.uva.taxfree.qls.styleoption.*;
 
 import java.io.File;
@@ -17,22 +16,28 @@ public class QlsStyleBuilder {
     }
 
     public QlsStyle generateStyle() {
+
         // TODO parse and generate the styles.
         SourceInfo tmpSourceInfo = new SourceInfo(0, 0, 0, 0);
-        QlsStyle qlsStyle = new QlsStyle();
         List<StyleOption> booleanStyleOptions = new ArrayList<>();
         booleanStyleOptions.add(new FontStyleOption("Arial", tmpSourceInfo));
         booleanStyleOptions.add(new FontSizeStyleOption(20, tmpSourceInfo));
         booleanStyleOptions.add(new ColorStyleOption("#FFFF00", tmpSourceInfo));
         booleanStyleOptions.add(new BackgroundColorStyleOption("#CCCC", tmpSourceInfo));
-        qlsStyle.addVariableStyleDeclaration(new BooleanType(), booleanStyleOptions, new SourceInfo(0, 0, 0, 0));
 
         List<StyleOption> stringStyleOptions = new ArrayList<>();
         stringStyleOptions.add(new FontStyleOption("Arial", tmpSourceInfo));
         stringStyleOptions.add(new FontSizeStyleOption(14, tmpSourceInfo));
         stringStyleOptions.add(new ColorStyleOption("#00FFFF", tmpSourceInfo));
         stringStyleOptions.add(new BackgroundColorStyleOption("#FFAA00", tmpSourceInfo));
-        qlsStyle.addVariableStyleDeclaration(new StringType(), stringStyleOptions, new SourceInfo(0, 0, 0, 0));
-        return qlsStyle;
+        List<Page> pages = new ArrayList<>();
+        List<Section> sections = new ArrayList<>();
+
+        List<QuestionStyle> questionStyles = new ArrayList<>();
+        sections.add(new Section("TaxSection", questionStyles, tmpSourceInfo));
+        List<DefaultStyle> defaultStyles = new ArrayList<>();
+        defaultStyles.add(new DefaultStyle(new BooleanType(), stringStyleOptions, tmpSourceInfo));
+        pages.add(new Page("TaxPage", sections, defaultStyles, tmpSourceInfo));
+        return new QlsStyle(pages);
     }
 }
