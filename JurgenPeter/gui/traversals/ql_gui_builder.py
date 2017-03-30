35891fab938aaef33e76d6592fca5a5b848f745c
Jurgen Baas <jurgen.baas@outlook.com>
2017-03-30 14:05:41 +0200

    rename of directories somehow untracked the files.

---
+++ JurgenPeter/gui/traversals/ql_gui_builder.py
@@ -0,0 +1,33 @@
+from gui.traversals.default_widget_getter import DefaultWidgetGetter
+from gui.traversals.gui_builder import GuiBuilder
+
+
+class QlGuiBuilder(GuiBuilder):
+
+    def build(self, node):
+        self.visit(node)
+
+    def visit_form(self, node):
+        for element in node.body:
+            self.visit(element)
+        self.create_exit_button()
+
+    def visit_question(self, node):
+        constructor = DefaultWidgetGetter().get(node.datatype)
+        widget = constructor(self.app, node)
+        widget.set_listener(self.listener)
+        self.widgets[node.name] = widget
+
+    def visit_computed_question(self, node):
+        self.visit_question(node)
+        self.widgets[node.name].disable()
+
+    def visit_if_conditional(self, node):
+        for element in node.ifbody:
+            self.visit(element)
+
+    def visit_ifelse_conditional(self, node):
+        for element in node.ifbody:
+            self.visit(element)
+        for element in node.elsebody:
+            self.visit(element)
---
+++ JurgenPeter/qls/traversals/type_checker.py
@@ -0,0 +1,55 @@
+from misc.visitor import CheckerVisitor
+from qls.traversals.styling_widget_type_getter import StylingWidgetTypeGetter
+
+
+class TypeChecker(CheckerVisitor):
+    def __init__(self, symbol_table, errors=[]):
+        self.symboltable = symbol_table
+        self.errors = errors
+
+    def check(self, node):
+        self.visit(node, [])
+
+    def visit_layout(self, node, stylings):
+        for element in node.body:
+            self.visit(element, stylings)
+
+    def visit_styled_layout(self, node, stylings):
+        self.visit_layout(node, stylings + node.stylings)
+
+    def visit_page(self, node, stylings):
+        for element in node.body:
+            self.visit(element, stylings)
+
+    def visit_styled_page(self, node, stylings):
+        self.visit_page(node, stylings + node.stylings)
+
+    def visit_section(self, node, stylings):
+        for element in node.body:
+            self.visit(element, stylings)
+
+    def visit_styled_section(self, node, stylings):
+        self.visit_section(node, stylings + node.stylings)
+
+    def visit_question_anchor(self, node, stylings):
+        question_type = self.symboltable[node.name]
+        widget_type = self.get_widget_type(question_type, stylings)
+
+        if widget_type != question_type:
+            self.error("widget datatype {} for question anchor "
+                       "\"{}\" does not match question "
+                       "datatype {}".format(widget_type, node.name,
+                                            question_type))
+
+    def visit_styled_question_anchor(self, node, stylings):
+        self.visit_question_anchor(node, stylings + [node.styling])
+
+    def get_widget_type(self, datatype, stylings):
+        widget_type = datatype
+        styling_getter = StylingWidgetTypeGetter(datatype)
+
+        for styling in stylings:
+            if styling_getter.get(styling):
+                widget_type = styling_getter.get(styling)
+
+        return widget_type
