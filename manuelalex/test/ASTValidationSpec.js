3127eb68aeda75a372cfbbc90702db2b79010802
Alex van Manen <alexvanmanen@hotmail.com>
2017-04-03 13:23:01 +0200

    test cases refactored

--- manuelalex/test/ASTValidationSpec.js
+++ manuelalex/test/ASTValidationSpec.js
@@ -59,22 +59,24 @@ describe('Test ASTValidator', () => {
 
     describe('Validate expressions', () => {
         it('QLBoolean && QLBoolean -> OK', (done) => {
-            let astValidationVisitor = new imports.ASTValidationVisitor();
-            let expression = new imports.Expression();
-            let p1 = new imports.Property();
-            p1.name = "p1";
-            let p2 = new imports.Property();
-            p2.name = "p2";
-            expression.leftHand = p1;
-            expression.rightHand = p2;
-            expression.operator = "&&";
-            let memoryState = new imports.MemoryState();
-            memoryState.set(p1.name, new imports.QLBoolean());
-            memoryState.set(p2.name, new imports.QLBoolean());
-            astValidationVisitor.memoryState = memoryState;
+            let astValidationVisitor = validate(new imports.QLBoolean(), new imports.QLBoolean(), "&&")
+            expect(astValidationVisitor.hasDetectedErrors()).to.equal(false);
+            done();
+        })
+    });
 
-            astValidationVisitor.visitExpression(expression);
+    describe('Check expressions', () => {
+        it('QLBoolean && QLMoney -> Detects Error', (done) => {
+            let astValidationVisitor = validate(new imports.QLBoolean(), new imports.QLMoney(), "&&")
+            expect(astValidationVisitor.hasDetectedErrors()).to.equal(true);
+            done();
+        })
+    });
 
+
+    describe('Check expressions', () => {
+        it('QLBoolean && QLBoolean || QLMoney < QLMoney -> OK', (done) => {
+            let astValidationVisitor = validate(new imports.QLBoolean(), new imports.QLBoolean(), "&&", new imports.QLMoney(), new imports.QLMoney, "<", "||");
             expect(astValidationVisitor.hasDetectedErrors()).to.equal(false);
             done();
         })
@@ -82,27 +84,86 @@ describe('Test ASTValidator', () => {
 
 
     describe('Check expressions', () => {
-        it('QLBoolean && QLMoney -> Detects Error', (done) => {
-            let astValidationVisitor = new imports.ASTValidationVisitor();
-            let expression = new imports.Expression();
-            let p1 = new imports.Property();
-            p1.name = "p1";
-            let p2 = new imports.Property();
-            p2.name = "p2";
-            expression.leftHand = p1;
-            expression.rightHand = p2;
-            expression.operator = "&&";
-            let memoryState = new imports.MemoryState();
-            memoryState.set(p1.name, new imports.QLBoolean());
-            memoryState.set(p2.name, new imports.QLMoney());
-            astValidationVisitor.memoryState = memoryState;
-
-            astValidationVisitor.visitExpression(expression);
-
+        it('QLBoolean && QLBoolean + QLNumber < QLNumber -> Detects Error', (done) => {
+            let astValidationVisitor = validate(new imports.QLBoolean(), new imports.QLBoolean(), "&&", new imports.QLMoney(), new imports.QLMoney, "<", "+");
             expect(astValidationVisitor.hasDetectedErrors()).to.equal(true);
             done();
         })
     });
+
+
+    function validate(left1, right1, operator1, left2, right2, operator2, operator){
+        let astValidationVisitor = new imports.ASTValidationVisitor();
+        let memoryState = new imports.MemoryState();
+
+        let p1 = new imports.Property();
+        p1.name = "p1";
+        let p2 = new imports.Property();
+        p2.name = "p2";
+        let p3 = new imports.Property();
+        p3.name = "p3";
+        let p4 = new imports.Property();
+        p4.name = "p4";
+        memoryState.set(p1.name, left1);
+        memoryState.set(p2.name, right1);
+        memoryState.set(p3.name, left2);
+        memoryState.set(p4.name, right2);
+
+        let leftExpression = new imports.Expression();
+        let rightExpression = new imports.Expression();
+
+
+        let mainExpression = new imports.Expression();
+
+        leftExpression.leftHand = p1;
+        leftExpression.rightHand = p2;
+        leftExpression.operator = operator1;
+
+        rightExpression.leftHand = p3;
+        rightExpression.rightHand = p4;
+        rightExpression.operator = operator2;
+
+        mainExpression.leftHand = leftExpression;
+        mainExpression.rightHand = rightExpression;
+        leftExpression.operator = operator;
+
+        astValidationVisitor.memoryState = memoryState;
+        astValidationVisitor.visitExpression(mainExpression);
+    }
+
+
+    function validate(propType1, propType2, operator) {
+        var prop1 = {name: "p1", type: propType1};
+        var prop2 = {name: "p2", type: propType2};
+
+        var memoryState = new imports.MemoryState();
+        memoryState.set(prop1.name, prop1.type);
+        memoryState.set(prop2.name, prop2.type);
+        var expression = getExpressionProperty(prop1, operator, prop2);
+
+        let astValidationVisitor = new imports.ASTValidationVisitor();
+        astValidationVisitor.memoryState = memoryState;
+        astValidationVisitor.visitExpression(expression);
+        return astValidationVisitor;
+    }
+
+
+    function getExpressionProperty(prop1, operator, prop2) {
+        let p1 = new imports.Property();
+        p1.name = prop1.name;
+        let p2 = new imports.Property();
+        p2.name = prop2.name;
+
+
+        let expression = new imports.Expression();
+
+        expression.leftHand = p1;
+        expression.rightHand = p2;
+        expression.operator = operator;
+
+
+        return expression;
+    }
 
 
 });
