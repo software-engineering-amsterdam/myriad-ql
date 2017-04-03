8ce55b21e121ca552cae3110058382f127972480
Manuel Overdijk <manuel.overdijk@gmail.com>
2017-04-03 14:29:02 +0200

    Added ParserSpec with unit tests for valid/invalid parse strings

--- manuelalex/test/ParserSpec.js
+++ manuelalex/test/ParserSpec.js
@@ -1,25 +1,55 @@
 /**
- * Created by alexvanmanen on 06-02-17.
+ * Created by Manuel on 06-02-17.
  */
 import chai from 'chai';
-// import Parser from '../src/Parser.js';
-import JspmImport       from './JspmImport.js';
-import {
-    loadDependencies,
-    mockDependency
-}                               from './Bootstrap.js';
+import transpiledRequire from '../src/utils/TranspiledRequire.js';
+import JspmImport from './JspmImport.js';
+import {testStrings} from './testStrings.js';
 
 const expect = chai.expect;
+const assert = chai.assert;
 
 describe('Parser', () => {
 
-    describe('Test', ()=>{
-       it('returns valid', (done)=>{
-           JspmImport('./src/ASTDependencyVisitor.js').then(({astDependencyVisitor})=>{
-                console.log(astDependencyVisitor);
-               astDependencyVisitor.visitAST({});
-                done();
-           });
-       })
+    let nearleyParser;
+    let parser;
+
+    beforeEach('Rebuild the parser grammar', () => {
+        let exec = require('child_process').exec;
+        exec('nearleyc src/grammar/grammar.ne -o src/grammar/grammar.js');
+    });
+
+    beforeEach('Rebuild the parser generator', (done)=>{
+        Promise.all([JspmImport('./src/ast/ASTBuilder.js')]).then(([{ ASTBuilder }]) => {
+            let { Parser } = transpiledRequire('src/Parser.js');
+            let nearley = require('nearley');
+            global.ASTBuilder = ASTBuilder;
+            let window = global;
+            let grammar = require('../src/grammar/grammar.js');
+            parser = new Parser();
+            nearleyParser = parser.createParser(nearley, grammar);
+            done();
+        });
+    });
+
+    describe('Parser', () => {
+        let validStrings = testStrings.valid;
+        let invalidStrings = testStrings.invalid;
+
+        Object.keys(validStrings).forEach((key)=>{
+            let testString = validStrings[key];
+            it('Parses the valid string: '+ key + ' without errors ', function() {
+                let {errors} = parser.parse(testString, nearleyParser);
+                expect(errors).to.be.empty;
+            });
+        });
+
+        Object.keys(invalidStrings).forEach((key)=>{
+            let testString = invalidStrings[key];
+            it('Parses the invalid string: '+ key + ' with errors ', function() {
+                let {errors} = parser.parse(testString, nearleyParser);
+                expect(errors).to.not.be.empty;
+            });
+        });
     });
 });
---
+++ manuelalex/test/TestStrings.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Manuel on 03/04/2017.
+ */
+
+
+export let testStrings = {
+    valid: {
+        1: 'form y{}',
+        2: 'form z{}',
+        3: 'form zz{}'
+    },
+    invalid: {
+        1: 'forn y{}'
+    }
+};
