4080856de78ec91bad71988e66709c1c78a23c60
Alex van Manen <alexvanmanen@hotmail.com>
2017-04-08 11:21:26 +0200

    ASTValidationSpec refactored

--- manuelalex/test/ASTValidationSpec.js
+++ manuelalex/test/ASTValidationSpec.js
@@ -32,32 +32,32 @@ describe('Test ASTValidator', () => {
         });
     });
 
-    describe('General tests', () => {
-        it('returns valid', (done) => {
-
-            let astValidationVisitor = new imports.ASTValidationVisitor();
-            let form = new imports.Form();
-            astValidationVisitor.visitForm(form);
-            done();
-
-            // let form = new Form();
-            // //let ast = new AST();
-            // //ast.program = '{program: '+ form +' }';dd
-            //
-            //d
-            // form.name = "taxOfficeExample";
-            // //form.statements =
-            // let ql = new QLMoney();
-            // let question = new Question();
-
-            //let jsonAST = '{"program":{"name":"taxOfficeExample","statements":[{"location":26,"label":{"value":"Did you sell a house in 2010?"},"propertyName":{"name":"hasSoldHouse","location":70},"propertyType":{"location":84}},{"location":95,"label":{"value":"Did you buy a house in 2010?"},"propertyName":{"name":"hasBoughtHouse","location":138},"propertyType":{"location":154}},{"location":163,"label":{"value":"Did you enter a loan?"},"propertyName":{"name":"hasMaintLoan","location":199},"propertyType":{"location":213}},{"location":223,"condition":{"leftHand":{"leftHand":{"name":"hasSoldHouse","location":228},"operator":"&&","rightHand":{"name":"hasSoldHouse","location":244},"location":228},"operator":"||","rightHand":{"leftHand":{"name":"hasBoughtHouse","location":262},"operator":"<","rightHand":{"name":"hasBoughtHouse","location":279},"location":262},"location":227},"ifBody":[{"location":305,"label":{"value":"What was the selling price?"},"propertyName":{"name":"sellingPrice","location":351},"propertyType":{"location":365}},{"location":378,"label":{"value":"What was the private debts for the sold house?"},"propertyName":{"name":"privateDebt","location":443},"propertyType":{"location":456}},{"location":468,"label":{"value":"Value residue:"},"allocation":{"propertyName":{"name":"valueResidue","location":499},"type":{"location":513},"expression":{"leftHand":{"name":"hasSoldHouse","location":522},"operator":"-","rightHand":{"name":"hasMaintLoan","location":535},"location":522},"location":499}}]}],"location":0}}';
-            //let ast = JSON.parse(form);
-
-            //astValidationVisitor.visitForm(form);
-            // done();
-
-        })
-    });
+    // describe('General tests', () => {
+    //     it('returns valid', (done) => {
+    //
+    //         let astValidationVisitor = new imports.ASTValidationVisitor();
+    //         let form = new imports.Form();
+    //         astValidationVisitor.visitForm(form);
+    //         done();
+    //
+    //         // let form = new Form();
+    //         // //let ast = new AST();
+    //         // //ast.program = '{program: '+ form +' }';dd
+    //         //
+    //         //d
+    //         // form.name = "taxOfficeExample";
+    //         // //form.statements =
+    //         // let ql = new QLMoney();
+    //         // let question = new Question();
+    //
+    //         //let jsonAST = '{"program":{"name":"taxOfficeExample","statements":[{"location":26,"label":{"value":"Did you sell a house in 2010?"},"propertyName":{"name":"hasSoldHouse","location":70},"propertyType":{"location":84}},{"location":95,"label":{"value":"Did you buy a house in 2010?"},"propertyName":{"name":"hasBoughtHouse","location":138},"propertyType":{"location":154}},{"location":163,"label":{"value":"Did you enter a loan?"},"propertyName":{"name":"hasMaintLoan","location":199},"propertyType":{"location":213}},{"location":223,"condition":{"leftHand":{"leftHand":{"name":"hasSoldHouse","location":228},"operator":"&&","rightHand":{"name":"hasSoldHouse","location":244},"location":228},"operator":"||","rightHand":{"leftHand":{"name":"hasBoughtHouse","location":262},"operator":"<","rightHand":{"name":"hasBoughtHouse","location":279},"location":262},"location":227},"ifBody":[{"location":305,"label":{"value":"What was the selling price?"},"propertyName":{"name":"sellingPrice","location":351},"propertyType":{"location":365}},{"location":378,"label":{"value":"What was the private debts for the sold house?"},"propertyName":{"name":"privateDebt","location":443},"propertyType":{"location":456}},{"location":468,"label":{"value":"Value residue:"},"allocation":{"propertyName":{"name":"valueResidue","location":499},"type":{"location":513},"expression":{"leftHand":{"name":"hasSoldHouse","location":522},"operator":"-","rightHand":{"name":"hasMaintLoan","location":535},"location":522},"location":499}}]}],"location":0}}';
+    //         //let ast = JSON.parse(form);
+    //
+    //         //astValidationVisitor.visitForm(form);
+    //         // done();
+    //
+    //     })
+    // });
 
 
     describe('Validate expressions', () => {
@@ -123,40 +123,23 @@ describe('Test ASTValidator', () => {
     });
 
 
-    function validateExpression(left1, right1, operator1, left2, right2, operator2, operator) {
+    function validateExpression(propertyTypeLeft1, propertyTypeRight1, operator1, propertyTypeLeft2, propertyTypeRight2, operator2, operator) {
         let astValidationVisitor = new imports.ASTValidationVisitor();
         let memoryState = new imports.MemoryState();
 
-        let p1 = new imports.Property();
-        p1.name = "p1";
-        let p2 = new imports.Property();
-        p2.name = "p2";
-        let p3 = new imports.Property();
-        p3.name = "p3";
-        let p4 = new imports.Property();
-        p4.name = "p4";
-        memoryState.set(p1.name, left1);
-        memoryState.set(p2.name, right1);
-        memoryState.set(p3.name, left2);
-        memoryState.set(p4.name, right2);
+        let property1 = new imports.Property("p1");
+        let property2 = new imports.Property("p2");
+        let property3 = new imports.Property("p3");
+        let property4 = new imports.Property("p4");
 
-        let leftExpression = new imports.Expression();
-        let rightExpression = new imports.Expression();
+        memoryState.set(property1.name, propertyTypeLeft1);
+        memoryState.set(property2.name, propertyTypeRight1);
+        memoryState.set(property3.name, propertyTypeLeft2);
+        memoryState.set(property4.name, propertyTypeRight2);
 
-
-        let mainExpression = new imports.Expression();
-
-        leftExpression.leftHand = p1;
-        leftExpression.rightHand = p2;
-        leftExpression.operator = operator1;
-
-        rightExpression.leftHand = p3;
-        rightExpression.rightHand = p4;
-        rightExpression.operator = operator2;
-
-        mainExpression.leftHand = leftExpression;
-        mainExpression.rightHand = rightExpression;
-        mainExpression.operator = operator;
+        let leftExpression = new imports.Expression(property1, operator1, property2);
+        let rightExpression = new imports.Expression(property3, operator2, property4);
+        let mainExpression = new imports.Expression(leftExpression, operator, rightExpression);
 
         astValidationVisitor.memoryState = memoryState;
         astValidationVisitor.visitExpression(mainExpression);
@@ -164,35 +147,19 @@ describe('Test ASTValidator', () => {
     }
 
 
-    function validate(propType1, propType2, operator) {
-        var prop1 = {name: "p1", type: propType1};
-        var prop2 = {name: "p2", type: propType2};
+    function validate(propertyTypeLeft, propertyTypeRight, operator) {
+        let property1 = new imports.Property("p1");
+        let property2 = new imports.Property("p2");
 
-        var memoryState = new imports.MemoryState();
-        memoryState.set(prop1.name, prop1.type);
-        memoryState.set(prop2.name, prop2.type);
-        var expression = getExpressionProperty(prop1, operator, prop2);
+        let memoryState = new imports.MemoryState();
+        memoryState.set(property1.name, propertyTypeLeft);
+        memoryState.set(property2.name, propertyTypeRight);
+
+        let expression = new imports.Expression(property1, operator, property2);
 
         let astValidationVisitor = new imports.ASTValidationVisitor();
         astValidationVisitor.memoryState = memoryState;
         astValidationVisitor.visitExpression(expression);
         return astValidationVisitor;
     }
-
-
-    function getExpressionProperty(prop1, operator, prop2) {
-        let p1 = new imports.Property();
-        p1.name = prop1.name;
-        let p2 = new imports.Property();
-        p2.name = prop2.name;
-
-        let expression = new imports.Expression();
-        expression.leftHand = p1;
-        expression.rightHand = p2;
-        expression.operator = operator;
-
-        return expression;
-    }
-
-
 });
