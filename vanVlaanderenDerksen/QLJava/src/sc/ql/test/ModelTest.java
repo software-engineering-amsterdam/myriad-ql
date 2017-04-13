c585c46f22f163b852ff1b6cdcec7a79e9d1cdbd
Jona Derksen <jonaderksen@gmail.com>
2017-04-13 10:36:13 +0200

    clean up comments + unused method

--- vanVlaanderenDerksen/QLJava/src/sc/ql/test/ModelTest.java
+++ vanVlaanderenDerksen/QLJava/src/sc/ql/test/ModelTest.java
@@ -26,16 +26,6 @@ import sc.ql.checkform.Message;
 import sc.ql.checkform.QuestionTypeMap;
 
 public class ModelTest {
-
-	
-	public void checkParseQuestion(){
-		
-		
-		Type testType = new Type();
-		IdLiteral id = new IdLiteral("testIdLitteral");
-		Question question = new Question("Test question 1", id, testType);
-
-	}
 	@Test
 	public void testAssingment() throws Exception{
 		
@@ -69,59 +59,3 @@ public class ModelTest {
 	}
 
 }
-	/*public void FormTest() throws Exception {
-		
-		String oneQuestionForm = "form \"dit is een test\" buyingPrice MONEY endform";
-		StringReader input = new StringReader(oneQuestionForm);
-        QLLexer lexer = new QLLexer(new ANTLRInputStream(input));
-        QLParser parser = new QLParser(new CommonTokenStream(lexer));
-
-        QLParser.FormContext context = parser.form();
-	}
-	
-	@Test
-	public void FormQuestionTest() throws Exception{
-		//setup
-		Form form = getModel("form \"This is a question\" buyingPrice MONEY endform");
-		//excution
-		List<FormElement> form_elements = form.getFormElements();
-		List<Question> questions = new ArrayList<Question>();
-		for (Node form_element : form_elements) {
-			questions.addAll(form_element.accept(new QuestionsVisitor()));
-        }
-		//result
-		//System.out.println("vraag: "+ questions.get(0).getQuestion());
-		Assert.assertEquals("This is a question", questions.get(0).getQuestion());
-		Assert.assertEquals("buyingPrice", questions.get(0).getId().getValue());
-		Assert.assertEquals(Question.Type.MONEY, questions.get(0).getType());
-		Assert.assertEquals((Integer) 1, questions.get(0).getLineNumber());
-	}
-	@Test
-	public void SingleQuestionTest() throws Exception{
-		String vraag			= "This is a question";
-		IdLiteral id 				= new IdLiteral("buyingPrice", 0);
-		Question.Type type 		= Question.Type.MONEY;
-		Expression expression	= (Expression) null;
-		Integer getal			= 1;
-		Question questoin = new Question(vraag, id, type, expression, getal);
-		
-		
-		Assert.assertEquals("This is a question", questoin.getQuestion());
-		Assert.assertEquals("buyingPrice", questoin.getId().getValue());
-		Assert.assertEquals(Question.Type.MONEY, questoin.getType());
-		Assert.assertEquals((Integer) 1, questoin.getLineNumber());
-	}
-
-		
-	private Form getModel(String input) throws IOException{
-		StringReader redstring = new StringReader(input);
-        QLLexer lexer = new QLLexer(new ANTLRInputStream(redstring));
-        QLParser parser = new QLParser(new CommonTokenStream(lexer));
-        ParseTree tree = parser.form();
-        
-        AstVisitor visitor = new AstVisitor();
-        Form form = (Form) visitor.visit(tree);
-        
-        return form;
-	}*/
-
