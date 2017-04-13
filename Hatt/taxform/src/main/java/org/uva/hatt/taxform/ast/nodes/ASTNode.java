f8720afa94254d5a5298d5434de970b4e80c2173
Metchu <ahmednassir@hotmail.com>
2017-04-13 21:24:58 +0200

    removed abstract accept method

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/ASTNode.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/ASTNode.java
@@ -1,11 +1,6 @@
 package org.uva.hatt.taxform.ast.nodes;
 
-import org.uva.hatt.taxform.ast.visitors.Visitor;
-
 public abstract class ASTNode {
-
-    public abstract <T> T accept(Visitor<T> visitor);
-
     private final int lineNumber;
 
     public ASTNode(int lineNumber) {
