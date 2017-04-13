cd1c37ec2c16abf2e50f7ac526192606079dcc15
Metchu <ahmednassir@hotmail.com>
2017-04-13 21:37:45 +0200

    added accept method

--- Hatt/TaxForm/src/main/java/org/uva/hatt/taxform/ast/nodes/types/Unknown.java
+++ Hatt/TaxForm/src/main/java/org/uva/hatt/taxform/ast/nodes/types/Unknown.java
@@ -1,5 +1,7 @@
 package org.uva.hatt.taxform.ast.nodes.types;
 
+import org.uva.hatt.taxform.ast.visitors.Visitor;
+
 public class Unknown extends ValueType{
 
     public Unknown() {
@@ -9,5 +11,9 @@ public class Unknown extends ValueType{
     @Override
     public java.lang.String name(){
         return "Unknown";
+    }
+
+    public <T> T accept(Visitor<T> visitor){
+        return visitor.visit(this);
     }
 }
