290d7013a08ce099ef87b11ef9e94f2e833658f2
Metchu <ahmednassir@hotmail.com>
2017-04-13 21:36:26 +0200

    added abstract accept method

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/Expression.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/expressions/Expression.java
@@ -1,8 +1,11 @@
 package org.uva.hatt.taxform.ast.nodes.expressions;
 
 import org.uva.hatt.taxform.ast.nodes.ASTNode;
+import org.uva.hatt.taxform.ast.visitors.ExpressionVisitor;
 
 public abstract class Expression extends ASTNode{
+
+    public abstract <T> T accept(ExpressionVisitor<T> expressionVisitor);
 
     public Expression(int lineNumber) {
         super(lineNumber);
