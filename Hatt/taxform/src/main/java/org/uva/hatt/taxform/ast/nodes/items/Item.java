1563fca25a0021bbc8f99fdc79e743ab339d432c
Metchu <ahmednassir@hotmail.com>
2017-04-13 21:25:31 +0200

    dded abstract accept method

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/items/Item.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/items/Item.java
@@ -1,8 +1,11 @@
 package org.uva.hatt.taxform.ast.nodes.items;
 
 import org.uva.hatt.taxform.ast.nodes.ASTNode;
+import org.uva.hatt.taxform.ast.visitors.Visitor;
 
 public abstract class Item extends ASTNode{
+    public abstract <T> T accept(Visitor<T> visitor);
+
     public Item(int lineNumber) {
         super(lineNumber);
     }
