be94eea3c9a23f710d23b443132371967f65ee1c
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-04-17 21:12:57 +0200

    formatted testField is generated based on the Widget returntype

--- AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/qls/styleoption/widget/TextWidget.java
+++ AK_BvdB_TaxFree/AntlrSample/src/org/uva/taxfree/qls/styleoption/widget/TextWidget.java
@@ -1,7 +1,9 @@
 package org.uva.taxfree.qls.styleoption.widget;
 
 import org.uva.taxfree.ql.gui.FormListener;
+import org.uva.taxfree.ql.gui.widgets.Widget;
 import org.uva.taxfree.ql.model.SourceInfo;
+import org.uva.taxfree.ql.model.values.IntValue;
 import org.uva.taxfree.ql.model.values.StringValue;
 import org.uva.taxfree.ql.model.values.Value;
 
@@ -9,16 +11,28 @@ import javax.swing.*;
 
 public class TextWidget extends WidgetStyleOption {
 
-    private final JFormattedTextField mTextField;
+    private JFormattedTextField mTextField;
 
     public TextWidget(SourceInfo sourceInfo) {
         super(sourceInfo);
-        mTextField = new JFormattedTextField("");
+    }
+
+    @Override
+    public void applyStyle(Widget widget) {
+        mTextField = generateTextField(widget.resolveValue());
+        super.applyStyle(widget);
     }
 
     @Override
     public Value resolve() {
         return new StringValue(String.valueOf(mTextField.getValue()));
+    }
+
+    protected JFormattedTextField generateTextField(Value value) {
+        if (value.equals(new IntValue(0))) {
+            return new JFormattedTextField(0);
+        }
+        return new JFormattedTextField("");
     }
 
     @Override
