f8aee161ee2b8bbc01dfb080667dbe7b97e2e20c
Teodor92 <teodor.ivanov92@gmail.com>
2017-04-18 10:45:20 +0200

    Removed old projects and XML comments.

--- OffByOne/src/OffByOne.Ql/Checker/Messages/DuplicateQuestionLabelMessage.cs
+++ OffByOne/src/OffByOne.Ql/Checker/Messages/DuplicateQuestionLabelMessage.cs
@@ -3,16 +3,8 @@
     using OffByOne.Ql.Ast.Statements;
     using OffByOne.Ql.Checker.Messages.Base;
 
-    /// <summary>
-    /// A message representing a duplicate question label found during the static code analysis.
-    /// </summary>
-    /// <seealso cref="WarningMessage" />
     public class DuplicateQuestionLabelMessage : WarningMessage
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DuplicateQuestionLabelMessage"/> class.
-        /// </summary>
-        /// <param name="question">The question.</param>
         public DuplicateQuestionLabelMessage(QuestionStatement question)
             : base($"Duplicate question label \"{question.Label}\" at: {question.SourceCode}")
         {
--- OffByOne/src/OffByOne.Ql/Checker/SyntaxChecker.cs
+++ OffByOne/src/OffByOne.Ql/Checker/SyntaxChecker.cs
@@ -11,16 +11,10 @@
     using OffByOne.Ql.Checker.Analyzers.Contracts;
     using OffByOne.Ql.Checker.Contracts;
 
-    /// <summary>
-    /// Checks a QL program for adherence to certain typing and structure rules.
-    /// </summary>
     public class SyntaxChecker
     {
         private readonly IEnumerable<IAnalyzer> analyzers;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SyntaxChecker"/> class with the default set of <see cref="IAnalyzer"/>.
-        /// </summary>
         public SyntaxChecker()
         {
             this.analyzers = new List<IAnalyzer>
@@ -31,20 +25,11 @@
             };
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SyntaxChecker"/> class with custom set of <see cref="IAnalyzer"/>,
-        /// </summary>
-        /// <param name="analyzers">List of <see cref="IAnalyzer"/> to be used for code analysis.</param>
         public SyntaxChecker(IEnumerable<IAnalyzer> analyzers)
         {
             this.analyzers = analyzers ?? throw new ArgumentNullException(nameof(analyzers), "A valid Collection of analyzers must provided.");
         }
 
-        /// <summary>
-        /// Checks a QL program and returns a <see cref="ICheckerReport"/> with all the errors, warnings and information messages.
-        /// </summary>
-        /// <param name="node">The starting node of the QL AST tree.</param>
-        /// <returns>A <see cref="ICheckerReport"/> with all the errors, warnings and information messages</returns>
         public ICheckerReport Check(FormStatement node)
         {
             var finalReport = new CheckerReport();
