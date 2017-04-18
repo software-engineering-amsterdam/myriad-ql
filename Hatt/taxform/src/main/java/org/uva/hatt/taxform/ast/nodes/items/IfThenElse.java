7e5ade00444c62c59c8dcf6a5b6a8c9c3d466c20
Thanus <tthanusijan@gmail.com>
2017-04-18 16:57:18 +0200

    extends IfThen since it contains the same logic
    * only addition is the else statements, so the little bit of duplication
    is removed

--- Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/items/IfThenElse.java
+++ Hatt/taxform/src/main/java/org/uva/hatt/taxform/ast/nodes/items/IfThenElse.java
@@ -5,25 +5,13 @@ import org.uva.hatt.taxform.ast.nodes.FormVisitor;
 
 import java.util.List;
 
-public class IfThenElse extends Item{
+public class IfThenElse extends IfThen {
 
-    private final Expression condition;
-    private final List<Item> thenStatements;
     private final List<Item> elseStatements;
 
     public IfThenElse(int lineNumber, Expression condition, List<Item> thenStatements, List<Item> elseStatements) {
-        super(lineNumber);
-        this.condition = condition;
-        this.thenStatements = thenStatements;
+        super(lineNumber, condition, thenStatements);
         this.elseStatements = elseStatements;
-    }
-
-    public Expression getCondition() {
-        return condition;
-    }
-
-    public List<Item> getThenStatements() {
-        return thenStatements;
     }
 
     public List<Item> getElseStatements() {
