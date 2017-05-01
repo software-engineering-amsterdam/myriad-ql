3465f993719728b78f42ef54ed8700f2539527ce
aijkoopmans <arendkoopmans@gmail.com>
2017-05-01 09:47:51 +0200

    move style merge function to class

--- koopmans/lib/qls/gui/question_frame_style_builder.rb
+++ koopmans/lib/qls/gui/question_frame_style_builder.rb
@@ -18,25 +18,27 @@ module QLS
       end
 
       def visit_section(section, parent_style)
-        style = merge_styles(@styles[section.object_id], parent_style)
+        style = @styles[section.object_id]
+        style.merge_with(parent_style)
         section.body.map { |element| element.accept(self, style) }
       end
 
       def visit_question(question, parent_style)
-        style = merge_styles(@styles[question.object_id], parent_style)
+        style = @styles[question.object_id]
+        style.merge_with(parent_style)
         @question_frame_styles[question.variable.name] = style if style
       end
 
       def visit_default_properties(_, _) end
 
-      def merge_styles(style, parent_style)
-        style.widget = style.widget || parent_style.widget
-        style.width = style.width || parent_style.width
-        style.font = style.font || parent_style.font
-        style.fontsize = style.fontsize || parent_style.fontsize
-        style.color = style.color || parent_style.color
-        style
-      end
+      # def merge_styles(style, parent_style)
+      #   style.widget = style.widget || parent_style.widget
+      #   style.width = style.width || parent_style.width
+      #   style.font = style.font || parent_style.font
+      #   style.fontsize = style.fontsize || parent_style.fontsize
+      #   style.color = style.color || parent_style.color
+      #   style
+      # end
     end
   end
 end
--- koopmans/lib/qls/gui/style.rb
+++ koopmans/lib/qls/gui/style.rb
@@ -2,6 +2,14 @@ module QLS
   module GUI
     class Style
       attr_accessor :widget, :width, :font, :fontsize, :color
+
+      def merge_with(style)
+        @widget = widget || style.widget
+        @width = width || style.width
+        @font = font || style.font
+        @fontsize = fontsize || style.fontsize
+        @color = color || style.color
+      end
     end
   end
 end
