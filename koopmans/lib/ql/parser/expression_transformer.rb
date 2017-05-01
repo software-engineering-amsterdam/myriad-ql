d1d3c9374282afba5ddf205b446e822a6a6327cf
aijkoopmans <arendkoopmans@gmail.com>
2017-05-01 12:50:33 +0200

    joe

--- koopmans/lib/ql/parser/expression_transformer.rb
+++ koopmans/lib/ql/parser/expression_transformer.rb
@@ -5,16 +5,20 @@ module QL
     class ExpressionTransformer < Parslet::Transform
       include AST
 
+      # negation
       rule([negation_operator: '-', single: simple(:single)]) { IntegerNegation.new('-', single) }
       rule([negation_operator: '!', single: simple(:single)]) { BooleanNegation.new('!', single) }
 
+      # binary expression
       rule(arithmetic_operator: simple(:operator), right: simple(:right))       { ArithmeticExpression.new(operator, right) }
       rule(comparison_equal_operator: simple(:operator), right: simple(:right)) { ComparisonEqualExpression.new(operator, right) }
       rule(comparison_order_operator: simple(:operator), right: simple(:right)) { ComparisonOrderExpression.new(operator, right) }
       rule(boolean_operator: simple(:operator), right: simple(:right))          { BooleanExpression.new(operator, right) }
 
-      rule(left: simple(:integer_literal)) { integer_literal }
+      # literal
+      rule(left: simple(:literal)) { literal }
 
+      # expression
       rule(sequence(:expressions)) { ExpressionSequence.new(expressions) }
     end
   end
